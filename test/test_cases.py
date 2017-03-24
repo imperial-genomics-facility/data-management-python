c6603e3d78b9ec6417afa272f4883da6cdca4bca
Avik Datta <avik.datta@imperial.ac.uk>
2017-03-24 13:33:20 +0000

    samplesheet parsing for hiseq4000

--- test/test_cases.py
+++ test/test_cases.py
@@ -1,3 +1,10 @@
 import unittest
 from igf_data.illumina.samplesheet import SampleSheet
 
+class Hiseq4000SampleSheet(unittest.TestCase):
+  def setUp(self):
+    file='doc/data/SampleSheet/HiSeq4000/SampleSheet.csv'
+    self.file=file
+  def test_samplesheet(self):
+    s1=SampleSheet(infile=self.file)
+
