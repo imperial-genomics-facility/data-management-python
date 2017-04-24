e91193b923ea92bd1dc3f5ecd8f9f6ec4434b77c
Avik Datta <avik.datta@imperial.ac.uk>
2017-04-24 10:16:33 +0100

    script for moving files

---
+++ scripts/file_copy/moveFilesForDemultiplexing.py
@@ -0,0 +1,17 @@
+import argparse
+from igf_data.process.moveBclFilesForDemultiplexing import moveBclFilesForDemultiplexing
+
+parser=argparse.ArgumentParser()
+parser.add_argument('-i','--input_dir',  required=True, help='Input files  directory')
+parser.add_argument('-o','--output_dir', required=True, help='Output files directory')
+parser.add_argument('-s','--samplesheet_file', required=True, help='Illumina format samplesheet file')
+parser.add_argument('-r','--runinfo_file', required=True, help='Illumina format RunInfo.xml file')
+args=parser.parse_args()
+
+input_dir    = args.input_dir
+output_dir   = args.output_dir
+samplesheet  = args.samplesheet_file
+runinfo_file = args.runinfo_file
+
+move_file=moveBclFilesForDemultiplexing(input_dir=self.input_dir, output_dir=self.output_dir, samplesheet=samplesheet, run_info_xml=runinfo_file)
+move_file.copy_bcl_files()
