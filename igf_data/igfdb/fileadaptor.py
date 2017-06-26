8800e90ad662bdb9069d4a33ccf4005c9c817e46
Avik Datta <reach4avik@yahoo.com>
2017-06-26 15:36:03 +0100

    divide data to file and attribute tables

--- igf_data/igfdb/fileadaptor.py
+++ igf_data/igfdb/fileadaptor.py
@@ -8,14 +8,43 @@ class FileAdaptor(BaseAdaptor):
   '''
   An adaptor class for File tables
   '''
-  def get_file_columns(self):
+  def store_file_and_attribute_data(self, data):
     '''
-    A method for fetching the columns for table file
+    A method for dividing and storing data to file and attribute table
     '''
-    file_column=[column.key for column in File.__table__.columns]
-    return user_column
+    (file_data, file_attr_data)=self.divide_data_to_table_and_attribute(data=data)
+    try:
+      self.store_file_data(data=file_data) 
+      self.store_run_attributes(data=file_attr_data) 
+      self.commit_session()
+    except:
+      self.rollback_session()
+      raise
 
-  
+
+  def divide_data_to_table_and_attribute(self, data, required_column='file_path', attribute_name_column='attribute_name', attribute_value_column='attribute_value'):
+    '''
+    A method for separating data for File and File_attribute tables
+    required params:
+    required_column: column name to add to the attribute data
+    attribute_name_column: label for attribute name column
+    attribute_value_column: label for attribute value column
+    It returns two pandas dataframes, one for File and another for File_attribute table
+    '''
+    if not isinstance(data, pd.DataFrame):
+      data=pd.DataFrame(data)
+ 
+    file_columns=self.get_table_columns(table_name=File, excluded_columns=['file_id'])                             # get required columns for file table
+    (file_df, file_attr_df)=super(FileAdaptor, self).divide_data_to_table_and_attribute( \
+                                                                     data=data, \
+    	                                                             required_column=required_column, \
+    	                                                             table_columns=file_columns,  \
+                                                                     attribute_name_column=attribute_name_column, \
+                                                                     attribute_value_column=attribute_value_column
+                                                                    )                                               # divide dataframe
+    return (file_df, file_attr_df)
+
+
   def store_file_data(self, data):
     '''
     Load data to file table
@@ -31,6 +60,16 @@ class FileAdaptor(BaseAdaptor):
       raise
 
 
+  def store_file_attributes(self, data, file_id=''):
+    '''
+    A method for storing data to File_attribute table
+    '''
+    try:
+      self.store_attributes(attribute_table=File_attribute, linked_column='file_id', db_id=file_id, data=data)
+    except:
+      raise
+
+
   def fetch_file_records_file_path(self, file_path):
     '''
     A method for fetching data for file table
@@ -42,3 +81,5 @@ class FileAdaptor(BaseAdaptor):
       return file_obj
     except:
       raise
+
+
