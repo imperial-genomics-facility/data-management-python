24f8347855deb37f7f4de3ca074692f96087158a
Avik Datta <reach4avik@yahoo.com>
2017-06-27 11:34:10 +0100

    linking foreign ids to exp table

--- igf_data/igfdb/experimentadaptor.py
+++ igf_data/igfdb/experimentadaptor.py
@@ -19,21 +19,32 @@ class ExperimentAdaptor(BaseAdaptor):
     (experiment_data, experiment_attr_data)=self.divide_data_to_table_and_attribute(data=data)
     
     try:
-      project_map_function=lambda x: self.map_foreign_table_and_store_attribute(data=x, \
-                                                                        lookup_table=Project, \
-                                                                        lookup_column_name='project_igf_id', \
-                                                                        target_column_name='project_id')        # prepare the function for Project id
-      sample_map_function=lambda x: self.map_foreign_table_and_store_attribute(data=x, \
-                                                                        lookup_table=Sample, \
-                                                                        lookup_column_name='sample_igf_id', \
-                                                                        target_column_name='sample_id')         # prepare the function for Sample id
-      platform_map_function=lambda x: self.map_foreign_table_and_store_attribute(data=x, \
-                                                                        lookup_table=Platform, \
-                                                                        lookup_column_name='platform_igf_id', \
-                                                                        target_column_name='platform_id')       # prepare the function for Platform id
-      new_experiment_attr_data=experiment_attr_data.apply(project_map_function, axis=1)                         # map project id foreign key id
-      new_experiment_attr_data=new_experiment_attr_data.apply(sample_map_function, axis=1)                      # map sample id foreign key id
-      new_experiment_attr_data=new_experiment_attr_data.apply(platform_map_function, axis=1)                    # map platform id foreign key id
+      project_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
+                                              data=x, \
+                                              lookup_table=Project, \
+                                              lookup_column_name='project_igf_id', \
+                                              target_column_name='project_id')                  # prepare the function for Project id
+      sample_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
+                                              data=x, \
+                                              lookup_table=Sample, \
+                                              lookup_column_name='sample_igf_id', \
+                                              target_column_name='sample_id')                   # prepare the function for Sample id
+      platform_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
+                                              data=x, \
+                                              lookup_table=Platform, \
+                                              lookup_column_name='platform_igf_id', \
+                                              target_column_name='platform_id')                 # prepare the function for Platform id
+      new_experiment_data=experiment_data.apply(project_map_function, axis=1)                   # map project id foreign key id
+      new_experiment_data=new_experiment_data.apply(sample_map_function, axis=1)                # map sample id foreign key id
+      new_experiment_data=new_experiment_data.apply(platform_map_function, axis=1)              # map platform id foreign key id
+      self.store_experiment_data(data=new_experiment_data)                                      # store experiment data
+      exp_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
+                                              data=x, \
+                                              lookup_table=Experiment, \
+                                              lookup_column_name='experiment_igf_id', \
+                                              target_column_name='experiment_id')               # prepare the function 
+      new_exp_attr_data=exp_attr_data.apply(exp_map_function, axis=1)                           # map foreign key id                                       
+      self.store_exp_attributes(data=new_exp_attr_data)                                         # store run attributes
       self.commit_session() 
     except:
       self.rollback_session()
@@ -54,14 +65,14 @@ class ExperimentAdaptor(BaseAdaptor):
     if not isinstance(data, pd.DataFrame):
       data=pd.DataFrame(data)
 
-    experiment_columns=self.get_table_columns(table_name=Experiment, excluded_columns=['experiment_id'])           # get required columns for experiment table
+    experiment_columns=self.get_table_columns(table_name=Experiment, excluded_columns=['experiment_id', 'project_id', 'sample_id', 'platform_id'])       # get required columns for experiment table
     (experiment_df, experiment_attr_df)=super(ExperimentAdaptor, self).divide_data_to_table_and_attribute( \
-                                                                     data=data, \
-    	                                                             required_column=required_column, \
-    	                                                             table_columns=experiment_columns,  \
-                                                                     attribute_name_column=attribute_name_column, \
-                                                                     attribute_value_column=attribute_value_column
-                                                                   )
+                                                                           data=data, \
+    	                                                                   required_column=required_column, \
+    	                                                                   table_columns=experiment_columns,  \
+                                                                           attribute_name_column=attribute_name_column, \
+                                                                           attribute_value_column=attribute_value_column \
+                                                                         )                                                                               # divide data to experiment and adatpor
     return (experiment_df, experiment_attr_df)
 
 
