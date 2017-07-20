3248d7ca864b77a31cc76943b9e7be727038acf1
Avik Datta <reach4avik@yahoo.com>
2017-07-20 15:03:36 +0100

    seqrun adaptor to store data and attribute

--- igf_data/igfdb/seqrunadaptor.py
+++ igf_data/igfdb/seqrunadaptor.py
@@ -2,12 +2,54 @@ import json
 import pandas as pd
 from sqlalchemy.sql import table, column
 from igf_data.igfdb.baseadaptor import BaseAdaptor
-from igf_data.igfdb.igfTables import Seqrun, Run, Platform
+from igf_data.igfdb.igfTables import Seqrun, Run, Platform, Seqrun_attribute
 
 class SeqrunAdaptor(BaseAdaptor):
   '''
   An adaptor class for table Seqrun
   '''
+
+  def store_seqrun_and_attribute_data(self, data):
+    '''
+    A method for dividing and storing data to seqrun and attribute table
+    '''
+    (seqrun_data, seqrun_attr_data)=self.divide_data_to_table_and_attribute(data=data)
+
+    try:                                                                                 
+      self.store_seqrun_data(data=seqrun_data)                                                # store run
+      if len(seqrun_attr_data.columns)>0:                                                     # check if any attribute exists
+        self.store_seqrun_attributes(data=seqrun_attr_data)                                   # store run attributes
+
+      self.commit_session()                                                                   # save changes to database
+    except:
+      self.rollback_session()
+      raise
+
+
+  def divide_data_to_table_and_attribute(self, data, required_column='seqrun_igf_id', attribute_name_column='attribute_name', attribute_value_column='attribute_value'):
+    '''
+    A method for separating data for Seqrun and Seqrun_attribute tables
+    required params:
+    required_column: column name to add to the attribute data
+    attribute_name_column: label for attribute name column
+    attribute_value_column: label for attribute value column
+    It returns two pandas dataframes, one for Seqrun and another for Run_attribute table
+    '''
+    if not isinstance(data, pd.DataFrame):
+      data=pd.DataFrame(data)
+
+    seqrun_columns=self.get_table_columns(table_name=Seqrun, excluded_columns=['seqrun_id', 'platform_id'])              # get required columns for run table
+    seqrun_columns.extend(['platform_igf_id'])
+    (seqrun_df, seqrun_attr_df)=super(SeqrunAdaptor, self).divide_data_to_table_and_attribute( \
+                                                      data=data, \
+    	                                              required_column=required_column, \
+    	                                              table_columns=seqrun_columns,  \
+                                                      attribute_name_column=attribute_name_column, \
+                                                      attribute_value_column=attribute_value_column \
+                                                    )                                                                    # divide data to run and attribute table
+    return (seqrun_df, seqrun_attr_df)
+
+
   def store_seqrun_data(self, data):
     '''
     Load data to Seqrun table
@@ -16,16 +58,39 @@ class SeqrunAdaptor(BaseAdaptor):
       if not isinstance(data, pd.DataFrame):
         data=pd.DataFrame(data)
 
-      platform_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
-                                              data=x, \
-                                              lookup_table=Platform, \
-                                              lookup_column_name='platform_igf_id', \
-                                              target_column_name='platform_id')       # prepare the function for Platform id
-      new_data=data.apply(platform_map_function, axis=1)                              # map platform id foreign key id
-      self.store_records(table=Seqrun, data=new_data)
-      self.commit_session()
+      if 'platform_igf_id' in data.columns:
+        platform_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
+                                                data=x, \
+                                                lookup_table=Platform, \
+                                                lookup_column_name='platform_igf_id', \
+                                                target_column_name='platform_id')       # prepare the function for Platform id
+        new_data=data.apply(platform_map_function, axis=1)                              # map platform id foreign key id
+        data=new_data                                                                   # overwrite data
+
+      self.store_records(table=Seqrun, data=data)                                       # store without autocommit
     except:
-      self.rollback_session()
+      raise
+
+
+  def store_seqrun_attributes(self, data, seqrun_id=''):
+    '''
+    A method for storing data to Seqrun_attribute table
+    '''
+    try:
+      if not isinstance(data, pd.DataFrame):
+        data=pd.DataFrame(data)                                                             # convert data to dataframe
+
+      if 'seqrun_igf_id' in data.columns:
+        seqrun_map_function=lambda x: self.map_foreign_table_and_store_attribute(\
+                                               data=x, \
+                                               lookup_table=Seqrun, \
+                                               lookup_column_name='seqrun_igf_id', \
+                                               target_column_name='seqrun_id')              # prepare run mapping function
+        new_data=data.apply(seqrun_map_function, axis=1)
+        data=new_data                                                                       # overwrite data    
+
+      self.store_attributes(attribute_table=Seqrun_attribute, linked_column='seqrun_id', db_id=seqrun_id, data=data) # store without autocommit
+    except:
       raise
 
 
