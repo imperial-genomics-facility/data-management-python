8e8609e7b89a7f604a69a967690670847442229c
Avik Datta <reach4avik@yahoo.com>
2017-07-31 18:16:42 +0100

    fixed bug for non-hpc user account

--- igf_data/igfdb/useradaptor.py
+++ igf_data/igfdb/useradaptor.py
@@ -33,7 +33,7 @@ class UserAdaptor(BaseAdaptor):
     if not isinstance(password, str):
       password=str(series.password_column).encode('utf-8')                                          # encode password if its not a string
 
-    if not len(password)==128:                                                                      # horrible hack for checking already hashed passward
+    if password:                                                                                    # always encrypt password
       key=salt+password                                                                             # construct key using salt and password
       password=hashlib.sha512(str(key).encode('utf-8')).hexdigest()                                 # create password hash
       series[password_column]=password                                                              # set hash to data series
@@ -41,8 +41,23 @@ class UserAdaptor(BaseAdaptor):
     return series
 
 
-  def _preprocess_data(self,data, password_column='password', categoty_column='category', \
-                       email_column='email_id', hpc_user_column='hpc_username', hpc_user='HPC_USER',
+  def _map_missing_user_status(self,data_series,categoty_column,hpc_user_column,hpc_user,non_hpc_user):
+    '''
+    An internal function for assigning user status
+    '''
+    if not isinstance(data_series, pd.Series):
+      data_series=pd.DataFrame(data_series)
+
+    if (data_series[categoty_column] is None or not data_series[categoty_column] ) and \
+       data_series[hpc_user_column] and data_series[hpc_user_column] is not None:
+      data_series[categoty_column]=hpc_user
+    elif (data_series[categoty_column] is None or not data_series[categoty_column]) and \
+         (data_series[hpc_user_column] is None or not data_series[hpc_user_column]):
+      data_series[categoty_column]=non_hpc_user
+    return data_series
+
+  def _preprocess_data(self,data, password_column='password', categoty_column='category',  \
+                       email_column='email_id', hpc_user_column='hpc_username', hpc_user='HPC_USER',non_hpc_user='NON_HPC_USER', \
                        user_igf_id_column='user_igf_id', username_column='username', salt_column='encryption_salt'):
     '''
     An internal function for preprocess data before loading
@@ -57,14 +72,19 @@ class UserAdaptor(BaseAdaptor):
       # check email id, it should contail '@'
       new_data[email_column].map(lambda x: self._email_check(email=x))
 
+      new_data=new_data.fillna('')
       # assign categoty, if user has hpc_username, then its 'HPC_USER'
-      new_data[categoty_column]=new_data[hpc_user_column].notnull().map(lambda x: hpc_user if x else None)
+      if categoty_column not in new_data.columns:         
+        new_data[categoty_column]=None                 # add category column if it doesn't exists
+      new_data.apply(lambda x: self._map_missing_user_status(data_series=x, categoty_column=categoty_column, \
+                                                             hpc_user_column=hpc_user_column, hpc_user=hpc_user, \
+                                                             non_hpc_user=non_hpc_user ), \
+                                                             axis=1) 
 
       # check for username, user with igf id should have the username
       if new_data[new_data[user_igf_id_column].notnull() & new_data[username_column].isnull()][email_column].count() > 0 :
         raise ValueError('Missing username for a registered user {0}'.format(new_data[user_igf_id_column].astype('str')))
 
-      new_data=new_data.fillna('')
       return new_data
     except:
       raise     
