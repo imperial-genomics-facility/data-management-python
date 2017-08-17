680d2d9b1ecdad2496712f291b74a21bba599532
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-17 17:34:07 +0100

    replaced xml parsing by bs4 and updated the tests

--- igf_data/illumina/runinfo_xml.py
+++ igf_data/illumina/runinfo_xml.py
@@ -1,64 +1,55 @@
 import re, os, warnings
 from collections import defaultdict
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 class RunInfo_xml:
   def __init__(self, xml_file):
     self.xml_file=xml_file
     self._read_xml()
 
-  def get_reads_stats(self, root_tag='Read', number_tag='Number'):
+  def get_reads_stats(self, root_tag='read', number_tag='number', tags=['isindexedread','numcycles']):
     '''
     Function for getting read and index stats from the RunInfo.xml file
     Output is a dictionary with the read number as the key
     '''
-    tree = self._tree
-    root = tree.getroot()
     reads_stats=defaultdict(lambda: defaultdict(dict))
     pattern=re.compile(number_tag, re.IGNORECASE)
+    soup=self._soup
 
     match_count=0
 
-    for reads in root.iter(root_tag):
-      match_count += 1
-      read_numbers=[reads.attrib[attrib] for attrib in reads.attrib.keys() if re.search(pattern, attrib)]
-      if len(read_numbers) != 1:
-        raise ValueError('couldn\'t identify readnumber for file {0} with root tag {1} and number tag {2}'.format(self.xml_file, root_tag, number_tag))
+    try:
+      for r in soup.find_all(root_tag):
+        for tag_name in tags:
+          reads_stats[r[number_tag]][tag_name]=r[tag_name]
+          match_count+=1
 
-      if read_numbers[0]:
-        for k,v in reads.items():
-          reads_stats[read_numbers[0]][k]=v
-    
-    if not match_count:
-      raise ValueError('no record found for {0} in file {1}'.format(root_tag, self.xml_file))
-  
-    return reads_stats
-    
-  def get_platform_number(self, tag='Instrument'):
+      if match_count==0:
+        raise ValueError('no record found for {0} in file {1}'.format(root_tag, self.xml_file))
+
+      return reads_stats
+    except:
+      raise 
+
+
+  def get_platform_number(self):
     '''
     Function for fetching the instrument series number
     '''           
-    tree = self._tree
-    root = tree.getroot()
-    match_count=0
-    series_number=''
+    series_number=None
+    soup=self._soup
+    series_number=soup.instrument.contents[0]
 
-    for series in root.iter(tag):
-      match_count += 1  
-      series_number=series.text
-   
     if not series_number:
-      raise ValueError('couldn\'t find tag {0}'.format(tag))
-
-    if match_count > 1:
-      raise ValueError('tag {0} present more than once'.format(tag))
+      raise ValueError('couldn\'t find tag {0}'.format('instrument'))
 
     return series_number
     
   def _read_xml(self):
     '''
-    Internal function for reading the xml file is a tree
+    Internal function for reading the xml file using BS4
     '''
     xml_file=self.xml_file
-    tree = ET.parse(xml_file)
-    self._tree = tree
+    with open(xml_file, 'r') as fp:
+      soup = BeautifulSoup(fp)
+    self._soup = soup
--- test/runinfo_xml_test.py
+++ test/runinfo_xml_test.py
@@ -4,23 +4,13 @@ from igf_data.illumina.runinfo_xml import RunInfo_xml
 class Hiseq4000RunInfo(unittest.TestCase):
 
   def setUp(self):
-    r_file='doc/data/Illumina/RunInfo.xml'
+    r_file='/home/vmuser/git_code/data-management-python/doc/data/Illumina/RunInfo.xml'
     self.runinfo_data=RunInfo_xml(xml_file=r_file)
 
   def test_runInfo_wrong_root_tag(self):
     runinfo_data=self.runinfo_data
     with self.assertRaises(ValueError):
       runinfo_data.get_reads_stats(root_tag='NO_Read')
-
-  def test_runInfo_wrong_number_tag(self):
-    runinfo_data=self.runinfo_data
-    with self.assertRaises(ValueError):
-      runinfo_data.get_reads_stats(number_tag='NO_Number')
-
-  def test_runInfo_wrong_root_and_number_tag(self):
-    runinfo_data=self.runinfo_data
-    with self.assertRaises(ValueError):
-      runinfo_data.get_reads_stats(root_tag='NO_Read', number_tag='NO_Number')
 
   def test_get_platform_number(self):
     runinfo_data=self.runinfo_data
@@ -38,14 +28,14 @@ class Hiseq4000RunInfo(unittest.TestCase):
     read_len=151
 
     for read_id in reads_stats.keys():
-       if reads_stats[read_id]['IsIndexedRead'] == 'Y':
+       if reads_stats[read_id]['isindexedread'] == 'Y':
          index_count += 1
-         index_cycle=reads_stats[read_id]['NumCycles']
+         index_cycle=reads_stats[read_id]['numcycles']
          self.assertEqual(int(index_cycle),index_len)
 
-       elif reads_stats[read_id]['IsIndexedRead'] == 'N':
+       elif reads_stats[read_id]['isindexedread'] == 'N':
          read_count += 1
-         read_cycle=reads_stats[read_id]['NumCycles']
+         read_cycle=reads_stats[read_id]['numcycles']
          self.assertEqual(int(read_cycle), read_len)
      
        else:
