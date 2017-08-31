806aa7e1bfdf37a602688f855d399d4cc4a71345
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-31 11:15:54 +0100

    added utils script for database operations

---
+++ igf_data/utils/dbutils.py
@@ -0,0 +1,40 @@
+import json
+from igf_data.igfdb.igfTables import Base
+from sqlalchemy import create_engine
+from igf_data.igfdb.baseadaptor import BaseAdaptor
+
+
+def read_dbconf_json(dbconfig):
+  '''
+  A method for reading dbconfig json file
+  required params:
+  dbconfig: A json file containing the database connection info
+  e.g. {"dbhost":"DBHOST","dbport": PORT,"dbuser":"USER","dbpass":"DBPASS","dbname":"DBNAME","driver":"mysql","connector":"pymysql"}
+
+  returns: a dictionary containing dbparms
+  '''
+  dbparam=None
+  with open(dbconfig, 'r') as json_data:
+    dbparam=json.load(json_data)
+
+  if dbparam in None:
+    raise ValueError('No dbconfig paramaters found in file {0}'.format(dbconfig))
+  return dbparam
+
+
+def clean_and_rebuild_database(dbconfig):
+  '''
+  A method for deleting data in database and create empty tables
+  required params:
+  dbconfig: A json file containing the database connection info
+  '''
+  try:
+    dbparam=read_dbconf_json(dbconfig)
+    base=BaseAdaptor(**dbparams)
+    base.start_session()
+    Base.metadata.drop_all(base.engine)
+    Base.metadata.create_all(base.engine)
+    base.close_session()
+  except:
+    raise
+ 
