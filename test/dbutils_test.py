8b115bcf2833d5d4f0bb6f85407e8631694c0a79
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-31 11:53:07 +0100

    unittest for checking sqlalchemy operation

--- test/dbutils_test.py
+++ test/dbutils_test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, sqlalchemy
 from igf_data.utils.dbutils import read_dbconf_json, clean_and_rebuild_database
 
 class Dbutils_test1(unittest.TestCase):
@@ -9,6 +9,13 @@ class Dbutils_test1(unittest.TestCase):
     with self.assertRaises(ValueError):
       read_dbconf_json(dbconfig=self.dbconfig)
     
+class Dbutils_test2(unittest.TestCase):
+  def setUp(self):
+    self.dbconfig='data/incorrect_dbconfig2.json'
+
+  def test_clean_and_rebuild_database(self):
+    with self.assertRaises(sqlalchemy.exc.OperationalError):
+      clean_and_rebuild_database(dbconfig=self.dbconfig)
 
 if __name__ == '__main__':
   unittest.main()
