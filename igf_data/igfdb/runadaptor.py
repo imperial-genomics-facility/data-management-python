df68ea6ab407282b81811454fe27fdb34b40968c
Avik Datta <avik.datta@imperial.ac.uk>
2017-09-01 15:38:34 +0100

    removed unused packaged from run adaptor

--- igf_data/igfdb/runadaptor.py
+++ igf_data/igfdb/runadaptor.py
@@ -1,6 +1,5 @@
-import json
 import pandas as pd
-from sqlalchemy.sql import table, column
+from sqlalchemy.sql import column
 from igf_data.igfdb.baseadaptor import BaseAdaptor
 from igf_data.igfdb.igfTables import Experiment, Run, Run_attribute, Seqrun
 
