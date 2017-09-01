608ccbd48208f233dac32ef2d524cc2910c31bb9
Avik Datta <avik.datta@imperial.ac.uk>
2017-09-01 17:16:57 +0100

    modified db for longer name and type in collection table

--- igf_data/igfdb/igfTables.py
+++ igf_data/igfdb/igfTables.py
@@ -310,8 +310,8 @@ class Collection(Base):
     { 'mysql_engine':'InnoDB', 'mysql_charset':'utf8' })
 
   collection_id = Column(INTEGER(unsigned=True), primary_key=True, nullable=False)
-  name          = Column(String(30), nullable=False)
-  type          = Column(String(30), nullable=False)
+  name          = Column(String(50), nullable=False)
+  type          = Column(String(50), nullable=False)
   table         = Column(Enum('sample', 'experiment', 'run', 'file', 'project', 'seqrun','unknown'), nullable=False, server_default='unknown')
   date_stamp    = Column(TIMESTAMP(), nullable=False, server_default=current_timestamp(), onupdate=datetime.datetime.now) 
   collection_group      = relationship('Collection_group', backref='collection')
