30467c551b76daa5e8201dc03d6ff93b263ecf60
Avik Datta <avik.datta@imperial.ac.uk>
2017-09-01 15:46:17 +0100

    removed unused UserAdaptor from project adaptor

--- igf_data/igfdb/projectadaptor.py
+++ igf_data/igfdb/projectadaptor.py
@@ -1,7 +1,6 @@
 import pandas as pd
-from sqlalchemy.sql import table, column
+from sqlalchemy.sql import column
 from igf_data.igfdb.baseadaptor import BaseAdaptor
-from igf_data.igfdb.useradaptor import UserAdaptor
 from igf_data.igfdb.igfTables import Project, ProjectUser, Project_attribute, User
 
 class ProjectAdaptor(BaseAdaptor):
