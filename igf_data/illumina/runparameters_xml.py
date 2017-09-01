0eecb3049bc9df1157fb20d3b60f4e767c9453e5
Avik Datta <avik.datta@imperial.ac.uk>
2017-09-01 14:53:47 +0100

    added new parser class for illumina runparameter xml file

---
+++ igf_data/illumina/runparameters_xml.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+class RunParameter_xml:
+  def __init__(self, xml_file):
+    self.xml_file=xml_file
+    self._read_xml()
+
+
+  def _read_xml(self):
+    '''
+    Internal function for reading the xml file using BS4
+    '''
+    xml_file=self.xml_file
+    with open(xml_file, 'r') as fp:
+      soup = BeautifulSoup(fp, "html5lib")
+    self._soup = soup
+
+
+   def get_hiseq_flowcell(self):
+     '''
+     A method for fetching flowcell details for hiseq run
+     It returns None of MiSeq and NextSeq runs
+     '''
+     soup=self._soup
+     try:
+       if soup.flowcell:
+         flowcell=soup.flowcell.contents[0]
+       else:
+         flowcell=None
+       return flowcell
+     except:
+       raise
+
+
+
