a91e4ae1ab6a478ce8018bd73275ba656bcec3b4
Avik Datta <reach4avik@yahoo.com>
2017-09-09 22:25:34 +0100

    added local and remote file copy method

--- igf_data/utils/fileutils.py
+++ igf_data/utils/fileutils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, subprocess
 from tempfile import mkdtemp
-from shutil import rmtree, move,copyfile,copy2
+from shutil import rmtree, move, copy2
 
 def move_file(source_path,destinationa_path, force=False):
     '''
@@ -17,6 +17,55 @@ def move_file(source_path,destinationa_path, force=False):
             raise IOError('source file {0} not found'.format(source_path))
         if os.path.exists(destinationa_path) and not force:
             raise IOError('destination file {0} already present. set option "force" as True to overwrite it'.format(destinationa_path))
-        move(, dst, copy_function)
+        move(source_path, destinationa_path, copy_function=copy2)
+    except:
+        raise
+
+
+def copy_local_file(source_path,destinationa_path, force=False):
+    '''
+    A method for copy files to local disk
+    required params:
+    source_path: A source file path
+    destination_path: A destination file path
+    force: Optional, set True to overwrite existing
+           destination file, default is False
+    '''
+    try:
+        if not os.path.exists(source_path):
+            raise IOError('source file {0} not found'.format(source_path))
+        if os.path.exists(destinationa_path) and not force:
+            raise IOError('destination file {0} already present. set option "force" as True to overwrite it'.format(destinationa_path))
+        copy2(source_path, destinationa_path, follow_symlinks=True)
+    except:
+        raise
+
+
+def copy_remote_file(source_path,destinationa_path, source_address=None, destination_address=None, copy_method='rsync'):
+    '''
+    A method for copy files from or to remote location
+    required params:
+    source_path: A source file path
+    destination_path: A destination file path
+    source_address: Address of the source server
+    destination_address: Address of the destination server
+    copy_method: A nethod for copy files, default is 'rsync'
+    '''
+    try:
+        if source_address is None and destination_address is None:
+            raise ValueError('required a remote source address or a destination address')
+        
+        if source_address is not None:
+            source_path='{0}@{1}'.format(source_address,source_path)
+        
+        if destination_address is not None:
+            destinationa_path='{0}@{1}'.format(destination_address,destinationa_path)
+        
+        if copy_method == 'rsync':
+            cmd=['rsync','-e','ssh',source_path,destinationa_path]
+        else:
+            raise ValueError('copy method {0} is not supported'.format(copy_method))
+        
+        subprocess.check_call(cmd)
     except:
         raise
