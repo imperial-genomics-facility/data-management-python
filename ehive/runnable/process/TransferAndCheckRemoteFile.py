93fb7d94ce63b57d7fcf73415b9f1424083507bc
Avik Datta <reach4avik@yahoo.com>
2017-09-13 14:13:17 +0100

    extending base class default parameters

--- ehive/runnable/process/TransferAndCheckRemoteFile.py
+++ ehive/runnable/process/TransferAndCheckRemoteFile.py
@@ -7,13 +7,14 @@ class TransferAndCheckRemoteFile(IGFBaseProcess):
   A class for transferring files from remote server and checking the file checksum value
   '''
   def param_defaults(self):
-    return {'log_slack':True,
-            'log_asana':True,
+    params_dict=IGFBaseProcess.param_defaults()
+    params_dict.update({
             'seqrun_server':'orwell.hh.med.ic.ac.uk',
             'chacksum_type':'md5',
             'seqrun_local_dir':None,
             'seqrun_source':None
-           }
+           })
+    return params_dict
     
     
   def run(self):
@@ -26,13 +27,19 @@ class TransferAndCheckRemoteFile(IGFBaseProcess):
       seqrun_file_name=self.param_required('seqrun_file_name')
       file_md5_value=self.param_required('file_md5')
     
-      source_file_path=os.path.join(seqrun_source,seqrun_igf_id,seqrun_file_name) # get new seqrun path
-      dir_name=os.path.dirname(seqrun_file_name)                                  # returns dir name or empty strings
-      destination_dir=os.path.join(seqrun_local_dir,seqrun_igf_id,dir_name)# get file copy path 
+      source_file_path=os.path.join(seqrun_source,
+                                    seqrun_igf_id,
+                                    seqrun_file_name)                           # get new seqrun path
+      dir_name=os.path.dirname(seqrun_file_name)                                # returns dir name or empty strings
+      destination_dir=os.path.join(seqrun_local_dir,seqrun_igf_id,dir_name)     # get file copy path 
       if not os.path.exists(destination_dir):
-        os.mkdir(destination_dir)                                          # create directory is its not present
-      destination_path=os.path.join(seqrun_local_dir,seqrun_igf_id,seqrun_file_name)  
-      copy_remote_file(source_path=source_file_path, destinationa_path=destination_path, source_address=seqrun_server) # copy remote file
+        os.mkdir(destination_dir)                                               # create directory is its not present
+      destination_path=os.path.join(seqrun_local_dir,
+                                    seqrun_igf_id,
+                                    seqrun_file_name)                           # get destination path
+      copy_remote_file(source_path=source_file_path,
+                       destinationa_path=destination_path, 
+                       source_address=seqrun_server)                            # copy remote file
       if not os.path.exists(destination_path):
         raise IOError('failed to copy file {0} for seqrun {1}'.format(seqrun_file_name,seqrun_igf_id))
       new_checksum=calculate_file_checksum(destination_path)
