7bfb4e4ec8dce78fe9393b02380427c035012584
Avik Datta <reach4avik@yahoo.com>
2017-09-13 15:40:05 +0100

    bugfix, changed parent class name for default parameters

--- ehive/runnable/jobfactory/SamplesheetProjectFactory.py
+++ ehive/runnable/jobfactory/SamplesheetProjectFactory.py
@@ -8,7 +8,7 @@ class SampleSheetProjectFactory(IGFBaseJobFactory):
   A class for finding all the projects mentioned in the SampleSheet
   '''
   def param_defaults(self):
-    params_dict=IGFBaseProcess.param_defaults()
+    params_dict=IGFBaseJobFactory.param_defaults()
     params_dict.update({
         'samplesheet':None,
         '10X_label':'10X'
