b15dac7424e68334b307ee664e854940378953f6
Avik Datta <reach4avik@yahoo.com>
2017-10-03 16:57:11 +0100

    bugfix for checkindexbarcode runnable

--- ehive/runnable/process/CheckIndexStats.py
+++ ehive/runnable/process/CheckIndexStats.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import os
 from ehive.runnable.IGFBaseProcess import IGFBaseProcess
-from igf_data.task_tracking.igf_slack import IGF_slack
-from igf_data.task_tracking.igf_asana import IGF_asana
-from igf_data.utils.fileutils import get_temp_dir,remove_dir
+from igf_data.utils.fileutils import get_temp_dir
 from igf_data.process.data_qc.check_sequence_index_barcodes import CheckSequenceIndexBarcodes,IndexBarcodeValidationError
-from igf_data.task_tracking import igf_slack
 
 class CheckIndexStats(IGFBaseProcess):
   '''
@@ -18,18 +15,15 @@ class CheckIndexStats(IGFBaseProcess):
       'strict_check':True,
       })
     return params_dict
-  
+
   def run(self):
     try:
       samplesheet_file=self.param_required('original_samplesheet')
       fastq_dir=self.param_required('fastq_dir')
-      base_work_dir=self.param_required('base_work_dir')
       stats_filename=self.param('stats_filename')
       seqrun_local_dir=self.param_required('seqrun_local_dir')
       strict_check=self.param('strict_check')
-      igf_slack=self.param('igf_slack')
-      igf_asana=self.param('igf_asana')
-      job_name=self.job_name()
+      
       work_dir=get_temp_dir                                                     # get work directory name
       seqrun_name=os.path.basename(seqrun_local_dir)                            # get seqrun name
       stats_json_file=os.path.join(fastq_dir,stats_filename)                    # get stats file path
@@ -40,7 +34,7 @@ class CheckIndexStats(IGFBaseProcess):
     except IndexBarcodeValidationError as e:
       self.param('dataflow_params',{'barcode_qc_stats':'FAIL'})                 # seed dataflow for failed lanes
       for plot_file in e.plots:
-        self.post_file_to_slack(message=e.message,file=plot_file)               # posting plot files to slack
+        self.post_file_to_slack(message=e.message,filepath=plot_file)           # posting plot files to slack
         self.upload_file_to_asana_task(task_name=seqrun_name, \
                                        filepath=plot_file, \
                                        comment=e.message)                       # upload plots to asana
