84622c1d504afc9ebb060db27400d7bd4c7dc4a6
Avik Datta <reach4avik@yahoo.com>
2017-10-12 11:32:23 +0100

    initial commit for collect fastq runnable

--- ehive/runnable/process/CollectFastqToDbCollection.py
+++ ehive/runnable/process/CollectFastqToDbCollection.py
@@ -5,10 +5,20 @@ class CollectFastqToDbCollection(IGFBaseProcess):
   A ehive runnable class for adding fastq files to database
   as the experiment and runs
   '''
+  def param_defaults(self):
+    params_dict=IGFBaseProcess.param_defaults()
+    params_dict.update({'file_location':'HPC_PROJECT',
+                        'samplesheet_filename':'SampleSheet.csv',
+                      })
+    
   def run(self):
     try:
       fastq_dir=self.param_required('fastq_dir')
-      igf_session_class = self.param_required('igf_session_class')
+      igf_session_class=self.param_required('igf_session_class')
+      model_name=self.param_required('model_name')
+      flowcell_id=self.param_required('flowcell_id')
+      file_location=self.param('file_location')
+      samplesheet_filename=self.param('samplesheet_filename')
       
     except:
       raise
