99e3c0c3386427a9db23a0c421d22fae1cf98b13
Avik Datta <reach4avik@yahoo.com>
2017-10-17 16:12:58 +0100

    adeed hasher type for checksum calculation and posting error msg to slack for bcl file transfer class

--- ehive/runnable/process/TransferAndCheckRemoteFile.py
+++ ehive/runnable/process/TransferAndCheckRemoteFile.py
@@ -15,11 +15,11 @@ class TransferAndCheckRemoteFile(IGFBaseProcess):
             'seqrun_source':None
            })
     return params_dict
-    
-    
+
+
   def run(self):
     try:
-      sequn_igf_id=self.param_required('seqrun_igf_id')
+      seqrun_igf_id=self.param_required('seqrun_igf_id')
       seqrun_source=self.param_required('seqrun_source')
       seqrun_server=self.param_required('seqrun_server')
       seqrun_local_dir=self.seqrun_local_dir('seqrun_local_dir')
@@ -34,6 +34,7 @@ class TransferAndCheckRemoteFile(IGFBaseProcess):
       destination_dir=os.path.join(seqrun_local_dir,seqrun_igf_id,dir_name)     # get file copy path 
       if not os.path.exists(destination_dir):
         os.mkdir(destination_dir)                                               # create directory is its not present
+
       destination_path=os.path.join(seqrun_local_dir,
                                     seqrun_igf_id,
                                     seqrun_file_name)                           # get destination path
@@ -41,10 +42,18 @@ class TransferAndCheckRemoteFile(IGFBaseProcess):
                        destinationa_path=destination_path, 
                        source_address=seqrun_server)                            # copy remote file
       if not os.path.exists(destination_path):
-        raise IOError('failed to copy file {0} for seqrun {1}'.format(seqrun_file_name,seqrun_igf_id))
-      new_checksum=calculate_file_checksum(destination_path)
+        raise IOError('failed to copy file {0} for seqrun {1}'.\
+                      format(seqrun_file_name,seqrun_igf_id))                   # check destination file after copy
+
+      new_checksum=calculate_file_checksum(destination_path,\
+                                           hasher=chacksum_type)                # calculate checksum of the transferred file
       if new_checksum != file_md5_value:
-        raise ValueError('checksum not matching for file {0}, expected: {1}, got {2}'.format(seqrun_file_name,file_md5_value, new_checksum))
+        raise ValueError('checksum not matching for file {0}, expected: {1}, got {2}'.\
+                         format(seqrun_file_name,file_md5_value, new_checksum)) # raise error if checksum doesn't match
+
       self.param('dataflow_params',{'seqrun_file_name':seqrun_file_name})
-    except:
+    except Exception as e:
+      message='Error in {0}: {1}'.format(self.__class__.__name__, e)
+      self.warning(message)
+      self.post_message_to_slack(message,reaction='fail')                       # post msg to slack for failed jobs
       raise
