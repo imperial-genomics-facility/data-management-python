36b3842d3b9eea75cfb9a64cc5f0b6f7c5477bb7
Avik Datta <reach4avik@yahoo.com>
2017-10-20 11:40:35 +0100

    assigned default params for seqrun file factory class

--- ehive/runnable/jobfactory/SeqrunFileFactory.py
+++ ehive/runnable/jobfactory/SeqrunFileFactory.py
@@ -8,7 +8,10 @@ class SeqrunFileFactory(IGFBaseJobFactory):
     params_dict=super(IGFBaseJobFactory,self).param_defaults()
     params_dict.update({ 
              'seqrun_md5_type':'ILLUMINA_BCL_MD5',
-             'seqrun_server':'orwell.hh.med.ic.ac.uk'
+             'seqrun_server':'orwell.hh.med.ic.ac.uk',
+             'hpc_location':'HPC_PROJECT',
+             'db_file_location_label':'location',
+             'db_file_path_label':'file_path'
            })
     return params_dict
 
@@ -21,6 +24,9 @@ class SeqrunFileFactory(IGFBaseJobFactory):
       seqrun_user=self.param_required('seqrun_user')
       igf_session_class=self.param_required('igf_session_class')
       seqrun_md5_type=self.param_required('seqrun_md5_type')
+      hpc_location=self.param_required('hpc_location')
+      db_file_location_label=self.param_required('db_file_location_label')
+      db_file_path_label=self.param_required('db_file_path_label')
 
       seqrun_path=os.path.join(seqrun_source,seqrun_igf_id)                     # get new seqrun path
       seqrun_server_login='{0}@{1}'.format(seqrun_user, seqrun_server)          # get destination path
@@ -43,9 +49,9 @@ class SeqrunFileFactory(IGFBaseJobFactory):
         raise ValueError('sequencing run {0} does not have any md5 json file'.\
                          format(seqrun_igf_id))
       
-      md5_json_location=files[0]['location']
-      md5_json_path=files[0]['file_path']
-      if md5_json_location !='HPC_PROJECT':
+      md5_json_location=files[0][db_file_location_label]
+      md5_json_path=files[0][db_file_path_label]
+      if md5_json_location !=hpc_location:
         temp_dir=get_temp_dir(work_dir=os.getcwd())                             # create a temp directory
         destination_path=ps.path.join(temp_dir,os.path.basename(md5_json_path)) # get destination path for md5 file
         copy_remote_file(source_path=md5_json_path,
@@ -56,6 +62,7 @@ class SeqrunFileFactory(IGFBaseJobFactory):
       with open(md5_json_path) as json_data:
             md5_json=json.load(json_data)                                       # read json data, get all file and md5 from json file
       self.param('sub_tasks',md5_json)                                          # seed dataflow
+      
       message='seeded {0} files for copy'.format(len(md5_json))
       self.warning(message)
       self.post_message_to_slack(message,reaction='pass')
