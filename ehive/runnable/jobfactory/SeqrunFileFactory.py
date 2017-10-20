7ac5554469c87d3ac9f9e163c650c0196a4b50ed
Avik Datta <reach4avik@yahoo.com>
2017-10-20 11:46:45 +0100

    corrected typo in seqrn file factory

--- ehive/runnable/jobfactory/SeqrunFileFactory.py
+++ ehive/runnable/jobfactory/SeqrunFileFactory.py
@@ -4,7 +4,7 @@ from ehive.runnable.IGFBaseJobFactory import IGFBaseJobFactory
 from igf_data.igfdb.collectionadaptor import CollectionAdaptor
 
 class SeqrunFileFactory(IGFBaseJobFactory):
-  def param_defaults(seld):
+  def param_defaults(self):
     params_dict=super(IGFBaseJobFactory,self).param_defaults()
     params_dict.update({ 
              'seqrun_md5_type':'ILLUMINA_BCL_MD5',
