45a9268fb35b829a451a1afd8ce61922ccbb3330
Avik Datta <reach4avik@yahoo.com>
2017-10-24 12:22:47 +0100

    updated logging for seed status update

--- ehive/runnable/process/ChangePipelineSeedStatus.py
+++ ehive/runnable/process/ChangePipelineSeedStatus.py
@@ -3,13 +3,14 @@ from ehive.runnable.IGFBaseProcess import IGFBaseProcess
 
 class ChangePipelineSeedStatus(IGFBaseProcess):
   def param_defaults(self):
-    params_dict=IGFBaseProcess.param_defaults()
+    params_dict=super(IGFBaseProcess,self).param_defaults()
     return params_dict
   
   def run(self):
     try:
       igf_session_class = self.param_required('igf_session_class')              # set by base class
       pipeline_name = self.param_required('pipeline_name')
+      seqrun_igf_id=self.param_required('seqrun_igf_id')
       seed_id=self.param_required('seed_id')
       seed_table=self.param_required('seed_table')
       status=self.param_required('status')
@@ -26,7 +27,9 @@ class ChangePipelineSeedStatus(IGFBaseProcess):
                                         status.upper()),\
                                  reaction='pass')
     except Exception as e:
-      message='Error in {0}: {1}'.format(self.__class__.__name__, e)
+      message='seqrun: {2}, Error in {0}: {1}'.format(self.__class__.__name__, \
+                                                      e, \
+                                                      seqrun_igf_id)
       self.warning(message)
-      self.post_message_to_slack(message,reaction='fail')
+      self.post_message_to_slack(message,reaction='fail')                       # post msg to slack for failed jobs
       raise
