a47622bb3eb009106e355cd995049be497f4797b
Avik Datta <reach4avik@yahoo.com>
2017-10-24 11:26:22 +0100

    minor changes to the bases mask runnable

--- ehive/runnable/process/CalculateBasesMask.py
+++ ehive/runnable/process/CalculateBasesMask.py
@@ -11,9 +11,8 @@ class CalculateBasesMask(IGFBaseProcess):
   Illumina samplesheet and RunInfor.xml file
   '''
   def param_defaults(self):
-    params_dict=IGFBaseProcess.param_defaults()
+    params_dict=super(IGFBaseProcess,self).param_defaults()
     params_dict.update({
-        'samplesheet_filename':'SampleSheet.csv',
         'runinfo_filename':'RunInfo.xml',
         'read_offset':1,
         'index_offset':0,
@@ -26,10 +25,12 @@ class CalculateBasesMask(IGFBaseProcess):
       seqrun_igf_id=self.param_required('seqrun_igf_id')
       seqrun_local_dir=self.param_required('seqrun_local_dir')
       samplesheet_file=self.param_required('samplesheet')
-      samplesheet_filename=self.param('samplesheet_filename')
-      runinfo_filename=self.aparam('runinfo_filename')
+      flowcell_lane=self.param_required('flowcell_lane')
+      project_name=self.param_required('project_name')
+      runinfo_filename=self.param('runinfo_filename')
       read_offset=self.param('read_offset')
       index_offset=self.param('index_offset')
+      
       runinfo_file=os.path.join(seqrun_local_dir,seqrun_igf_id,runinfo_filename)
     
       if not os.path.exists(samplesheet_file):
@@ -41,8 +42,17 @@ class CalculateBasesMask(IGFBaseProcess):
       bases_mask_object=BasesMask(samplesheet_file=samplesheet_file,
                                   runinfo_file=runinfo_file,
                                   read_offset=read_offset,
-                                  index_offset=index_offset)                      # create bases mask object
-      bases_mask_value=bases_mask_object.calculate_bases_mask()                   # calculate bases mask
+                                  index_offset=index_offset)                    # create bases mask object
+      bases_mask_value=bases_mask_object.calculate_bases_mask()                 # calculate bases mask
       self.param('dataflow_params',{'basesmask':bases_mask_value})
-    except:
+      message='seqrun: {0}, project: {1}, lane: {2}, mask: {3}'.\
+            format(seqrun_igf_id, project_name, flowcell_lane, bases_mask_value)
+      self.post_message_to_slack(message,reaction='pass')                       # send log to slack
+      self.comment_asana_task(task_name=seqrun_igf_id, comment=message)         # send log to asana
+    except Exception as e:
+      message='seqrun: {2}, Error in {0}: {1}'.format(self.__class__.__name__, \
+                                                      e, \
+                                                      seqrun_igf_id)
+      self.warning(message)
+      self.post_message_to_slack(message,reaction='fail')                       # post msg to slack for failed jobs
       raise
