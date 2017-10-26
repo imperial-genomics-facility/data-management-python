599f7582c6ba076d6f153861b9bbb6c0132d633b
Avik Datta <reach4avik@yahoo.com>
2017-10-26 11:44:57 +0100

    added revcomp function to sequtils

---
+++ igf_data/utils/sequtils.py
@@ -0,0 +1,27 @@
+import sys
+
+try:
+  if sys.version_info[0] < 3:
+    # Python 2.x specific import
+    from string import maketrans
+except:
+  raise
+
+def revcomp(input_seq):
+  '''
+  A function for converting nucleotide sequence to its reverse complement
+  required params:
+  input_seq: A nucleotide sequence
+  returns the reverse complement version of the input sequence
+  '''
+  try:
+    revcomp_seq=None
+    if sys.version_info[0] < 3:
+      # For Python 2.x, use maketrans
+      revcomp_seq=input_seq.upper().translate(maketrans('ACGT','TGCA'))[::-1]
+    else:
+      # For Python 3.x, use str.maketrans
+      revcomp_seq=input_seq.upper().translate(str.maketrans('ACGT','TGCA'))[::-1]
+    return revcomp_seq
+  except:
+    raise
