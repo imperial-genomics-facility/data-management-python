4e5d1a6c74c77e52f1ea490cc5b5d138d2dff896
Avik Datta <reach4avik@yahoo.com>
2017-10-30 16:20:05 +0000

    sending error messages to slack

--- ehive/runnable/jobfactory/ProjectFastqdirFactory.py
+++ ehive/runnable/jobfactory/ProjectFastqdirFactory.py
@@ -9,5 +9,10 @@ class ProjectFastqdirFactory(IGFBaseJobFactory):
       project_fastq=self.param_required('project_fastq')
       seed_data=[{'fastq_dir':fastq_dir} for fastq_dir in project_fastq.key()]  # define seed data
       self.param('sub_tasks',seed_data)                                         # add param for dataflow
-    except:
+    except Exception as e:
+      message='seqrun: {2}, Error in {0}: {1}'.format(self.__class__.__name__, \
+                                                      e, \
+                                                      seqrun_igf_id)
+      self.warning(message)
+      self.post_message_to_slack(message,reaction='fail')                       # post msg to slack for failed jobs
       raise
