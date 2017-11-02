4e9954d3d9c7ad4d55f098c1620aa3540f2fe395
Avik Datta <reach4avik@yahoo.com>
2017-11-02 11:39:44 +0000

    updated fastqscreen runnable for param parsing

--- ehive/runnable/process/RunFastqscreen.py
+++ ehive/runnable/process/RunFastqscreen.py
@@ -7,15 +7,16 @@ class RunFastqscreen(IGFBaseProcess):
   def param_defaults(self):
     params_dict=super(IGFBaseProcess,self).param_defaults()
     params_dict.update({
-      'overwrite_output':True,
+      'force_overwrite':True,
+      'tag':None,
       'fastqscreen_dir_label':'fastqscreen',
       'fastqscreen_exe':'fastqscreen',
       'fastqscreen_conf':None,
       'fastqscreen_options':{'--aligner':'bowtie2',
                              '--force':'',
                              '--quiet':'',
-                             '--subset':100000,
-                             '--threads':1},
+                             '--subset':'100000',
+                             '--threads':'1'},
       })
     return params_dict
   
@@ -25,19 +26,30 @@ class RunFastqscreen(IGFBaseProcess):
       seqrun_igf_id=self.param_required('seqrun_igf_id')
       base_results_dir=self.param_required('base_results_dir')
       project_name=self.param_required('project_name')
-      sample_igf_id=self.param_required('sample_igf_id')
-      run_igf_id=self.param_required('run_igf_id')
+      seqrun_date=self.param_required('seqrun_date')
+      flowcell_id=self.param_required('flowcell_id')
       fastqscreen_exe=self.param_required('fastqscreen_exe')
       fastqscreen_conf=self.param_required('fastqscreen_conf')
+      tag=self.param_required('tag')
       fastqscreen_options=self.param('fastqscreen_options')
-      overwrite_output=self.param('overwrite_output')
+      force_overwrite=self.param('force_overwrite')
       fastqscreen_dir_label=self.param('fastqscreen_dir_label')
+      
+      lane_index_info=os.path.basename(os.path.dirname(fastq_file))             # get the lane and index length info
+      fastq_file_label=os.path.basename(fastq_file).replace('.fastq.gz','')
       
       fastqscreen_result_dir=os.path.join(base_results_dir, \
                                           project_name, \
                                           fastqscreen_dir_label, \
-                                          sample_igf_id, \
-                                          run_igf_id)                           # get fastqscreen final output path
+                                          seqrun_date, \
+                                          flowcell_id, \
+                                          lane_index_info,\
+                                          tag,\
+                                          fastq_file_label)                     # result dir path is generic
+      
+      if os.path.exists(fastqscreen_result_dir) and force_overwrite:
+        remove_dir(fastqscreen_result_dir)                                      # remove existing output dir if force_overwrite is true
+      
       if not os.path.exists(fastqscreen_result_dir):
         os.mkdir(fastqscreen_result_dir)                                        # create output dir if its not present
       
@@ -45,14 +57,11 @@ class RunFastqscreen(IGFBaseProcess):
       if not os.path.exists(fastq_file):
         raise IOError('fastq file {0} not readable'.format(fastq_file))         # raise if fastq file path is not readable
       
-      filename=os.path.basename(fastq_file)                                     # get fastqfile base name
-      filename=filename.replace('.fastq.gz','')                                 # remove file ext
+      filename=os.path.basename(fastq_file).replace('.fastq.gz','')             # get fastqfile base name and remove file ext
       fastqscreen_output=os.path.join(temp_work_dir,filename)
       os.mkdir(fastqscreen_output)                                              # create fastqc output dir
       
-      fastqscreen_param=[[param,value] if value else [param] \
-                         for param, value in fastqscreen_options.items()]       # remove empty values
-      fastqscreen_param=[col for row in param for col in fastqscreen_param]     # flatten sub lists
+      fastqscreen_param=self.format_tool_options(fastqscreen_options)           # format fastqc params
       fastqscreen_cmd=[fastqscreen_exe,
                        '-conf',fastqscreen_conf,
                        '--outdir',fastqscreen_output,
@@ -61,17 +70,13 @@ class RunFastqscreen(IGFBaseProcess):
       fastqscreen_cmd.extend(fastq_file)                                        # fastqscreen input file
       subprocess.check_call(fastqscreen_cmd)                                    # run fastqscreen
       
-      if overwrite_output:
-        rmdir(os.path.join(fastqscreen_result_dir,filename))                    # remove fastqscreen results if its already present
-        
       copytree(fastqscreen_output,fastqscreen_result_dir)                       # copy fastqscreen output files
-      fastqscreen_path=os.path.join(fastqscreen_result_dir,filename)
       
       fastqscreen_stat=None
       fastqscreen_html=None
       fastqscreen_png=None
       
-      for root,dirs,files in os.walk(top=fastqscreen_path):
+      for root,dirs,files in os.walk(top=fastqscreen_result_dir):
         for file in files:
           if fnmatch.fnmatch(file, '*.txt'):
             fastqscreen_stat=os.path.join(root,file)
@@ -83,7 +88,7 @@ class RunFastqscreen(IGFBaseProcess):
             fastqscreen_png=os.path.join(root,file)
             
       self.param('dataflow_params',{'fastqscreen': \
-                                    {'fastqscreen_path':fastqscreen_path,
+                                    {'fastqscreen_path':fastqscreen_result_dir,
                                      'fastqscreen_stat':fastqscreen_stat,
                                      'fastqscreen_html':fastqscreen_html,
                                      'fastqscreen_png':fastqscreen_png}})       # set dataflow params
