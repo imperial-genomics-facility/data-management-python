e3beeed7a71b847f5459d9b662c4aea18a76264d
Avik Datta <reach4avik@yahoo.com>
2017-11-24 10:50:31 +0000

    get remote file info for known fq

--- ehive/runnable/process/CollectQcForFastqDir.py
+++ ehive/runnable/process/CollectQcForFastqDir.py
@@ -3,12 +3,18 @@ from ehive.runnable.IGFBaseProcess import IGFBaseProcess
 class CollectQcForFastqDir(IGFBaseProcess):
   def param_defaults(self):
     params_dict=super(IGFBaseProcess,self).param_defaults()
+    params_dict.update({
+      'remote_fastqc_info':None,
+      'remote_fastqs_info':None,
+      })
     return params_dict
   
   def run(self):
     try:
       fastqc_info=self.param_required('fastqc_info')
       fastqscreen_info=self.param_required('fastqscreen_info')
+      remote_fastqc_info=self.param('remote_fastqc_info')
+      remote_fastqs_info=self.param('remote_fastqs_info')                       # remote file info available only for the known fq
       
       fastqc_files=[fqc_file for fqc_file in fastqc_info.values()]
       fastqscreen_files=[fsr_file for fsr_file in fastqscreen_info.values()]
