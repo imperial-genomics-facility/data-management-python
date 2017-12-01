dbdda5338d047fb1d3f39010503b2ad71238e5e4
Avik Datta <reach4avik@yahoo.com>
2017-12-01 14:14:37 +0000

    skip fastq files for qc analysis if they are not R1 or R2 reads

--- ehive/runnable/jobfactory/FastqFileFactory.py
+++ ehive/runnable/jobfactory/FastqFileFactory.py
@@ -1,4 +1,4 @@
-import os,fnmatch
+import os,re,fnmatch
 from ehive.runnable.IGFBaseJobFactory import IGFBaseJobFactory
 
 class FastqFileFactory(IGFBaseJobFactory):
@@ -10,6 +10,7 @@ class FastqFileFactory(IGFBaseJobFactory):
     params_dict.update({
         'required_keyword':None,
         'filter_keyword':None,
+        'read_pattern':'\S+_L00\d_R[12]_\d+\.fastq(\.gz)?',
       })
     return params_dict
   
@@ -20,7 +21,9 @@ class FastqFileFactory(IGFBaseJobFactory):
       seqrun_igf_id=self.param_required('seqrun_igf_id')
       required_keyword=self.param_required('required_keyword')
       filter_keyword=self.param_required('filter_keyword')
+      read_pattern=self.param_required('read_pattern')
       
+      read_pattern=re.compile(r'{0}'.format(read_pattern))                      # compile read pattern
       if not os.path.exists(fastq_dir):
         raise IOError('fastq dir {0} not accessible'.format(fastq_dir))
       fastq_list=list()                                                         # create empty output list
@@ -28,11 +31,12 @@ class FastqFileFactory(IGFBaseJobFactory):
       for root, dirs, files in os.walk(top=fastq_dir):
         for file in files:
           if fnmatch.fnmatch(file, '*.fastq.gz'):                               # only consider fastq.gz files for now
-            if required_keyword and fnmatch.fnmatch(file, required_keyword ):
-              fastq_list.append({'fastq_file':os.path.join(root,file)})         # add fastq file to the list if its amatch
+            if re.search(read_pattern,file):                                    # skip if its not R1 and R2 reads and not illumina format name
+              if required_keyword and fnmatch.fnmatch(file, required_keyword ):
+                fastq_list.append({'fastq_file':os.path.join(root,file)})       # add fastq file to the list if its amatch
             
-            elif filter_keyword and not fnmatch.fnmatch(file, filter_keyword ):
-              fastq_list.append({'fastq_file':os.path.join(root,file)})         # add fastq file to the list if its not a match
+              elif filter_keyword and not fnmatch.fnmatch(file, filter_keyword ):
+                fastq_list.append({'fastq_file':os.path.join(root,file)})       # add fastq file to the list if its not a match
             
       self.param('sub_tasks',fastq_list)                                        # add fastq files to the dataflow
     except Exception as e:
