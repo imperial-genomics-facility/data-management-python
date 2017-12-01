79a57a105cae211914b9b17cb89b5a24891e8595
Avik Datta <reach4avik@yahoo.com>
2017-12-01 15:01:44 +0000

    added runnable module for clean up file or dir

---
+++ ehive/runnable/process/CleanupDirOrFile.py
@@ -0,0 +1,46 @@
+import os
+from igf_data.utils.fileutils import get_temp_dir,remove_dir
+from ehive.runnable.IGFBaseProcess import IGFBaseProcess
+
+class CleanupDirOrFile(IGFBaseProcess):
+  def param_defaults(self):
+    params_dict=super(IGFBaseProcess,self).param_defaults()
+    params_dict.update({
+      'cleanup_status':False,
+      })
+    return params_dict
+  
+  def run(self):
+    try:
+      seqrun_igf_id=self.param_required('seqrun_igf_id')
+      path=self.param_required('path')
+      cleanup_status=self.param_required('cleanup_status')
+      
+      message=None
+      if cleanup_status:
+        if not os.path.exists(path):
+          raise IOError('path {0} is not accessible'.format(path))
+        
+        if os.path.isdir(path):
+          remove_dir(path)
+          message='removed dir {0}'.format(path)
+        elif os.path.isfile(path):
+          os.remove(path)
+          message='removed file {0}'.format(path)
+        else:
+          message='path {0} is not file or directory, skipped removing'.\
+                  format(path)
+          
+      else:
+        message='Not removing path {0} as cleanup_status is not True'.\
+                format(path)
+      if message:
+        self.post_message_to_slack(message,reaction='pass')                     # send msg to slack
+        self.comment_asana_task(task_name=seqrun_igf_id, comment=message)       # send msg to asana
+    except Exception as e:
+      message='seqrun: {2}, Error in {0}: {1}'.format(self.__class__.__name__, \
+                                                      e, \
+                                                      seqrun_igf_id)
+      self.warning(message)
+      self.post_message_to_slack(message,reaction='fail')                       # post msg to slack for failed jobs
+      raise
