f1a4bf7a7f4a9b1012f1e93673212badfd4ddd6d
Avik Datta <reach4avik@yahoo.com>
2017-12-14 16:16:04 +0000

    fetching data for projects, users, project users and samples from the project_info_file

--- igf_data/process/seqrun_processing/find_and_register_new_project_data.py
+++ igf_data/process/seqrun_processing/find_and_register_new_project_data.py
@@ -1,6 +1,10 @@
+import pandas as pd
+from sqlalchemy.sql import column
 import os,subprocess,fnmatch
 from igf_data.utils.dbutils import read_dbconf_json
 from igf_data.igfdb.baseadaptor import BaseAdaptor
+from igf_data.igfdb.fileadaptor import FileAdaptor
+from igf_data.igfdb.igfTables import Project, User, Sample
 
 class Find_and_register_new_project_data:
   '''
@@ -16,17 +20,70 @@ class Find_and_register_new_project_data:
     self.projet_info_path=projet_info_path
     self.user_account_template=user_account_template
     dbparams = read_dbconf_json(dbconfig)
+    base=BaseAdaptor(**dbparam)
     self.session_class = base.get_session_class()
-    
   
-  def find_new_project_info(self):
+  
+  def process_project_data_and_account(self):
     '''
-    A method for fetching new project info file
+    A method for finding new project info and registering them to database
+    and user account creation
     '''
     try:
+      new_project_info_list=self._find_new_project_info()
+      for project_info_file in new_project_info_list:
+        (new_project_info,new_user_info,new_sample_info)=\
+          self._read_project_info_and_get_new_entries(project_info_file)        # get new project, user and samples information
+    except:
+      raise
+    
+    
+  def _read_project_info_and_get_new_entries(self,project_info_file):
+    '''
+    An internal method for processing project info data
+    required params:
+    project_info_file: A filepath for project_info csv files
+    It returns following information
+    new_project_info
+    new_user_info
+    new_sample_info
+    '''
+    try:
+      project_info_data=pd.read_csv(project_info_file)                          # read project info data
+      base=BaseAdaptor(**{'session_class':self.session_class})
+      required_project_columns=base.get_table_columns(table_name=Project, \
+                                             excluded_columns=['project_id'])   # get project columns
+      required_user_columns=base.get_table_columns(table_name=User, \
+                                             excluded_columns=['user_id'])      # get user columns
+      required_sample_columns=base.get_table_columns(table_name=Sample, \
+                                             excluded_columns=['sample_id',\
+                                                               'project_id'])   # get sample columns
+      required_project_user_columns=['project_igf_id','email_id']               # get project user columns
+      required_sample_columns.append('project_igf_id')
+      project_data=project_info_data.loc[:,required_project_columns]
+      user_data=project_info_data.loc[:,required_user_columns]
+      project_user_data=project_info_data.loc[:,required_project_user_columns]
+      sample_data=project_info_data.loc[:,required_sample_columns]
+    except:
+      raise
+    
+    
+  def _find_new_project_info(self):
+    '''
+    An internal method for fetching new project info file
+    It returns a list one new project info file
+    '''
+    try:
+      new_project_info_list=list()
+      fa=FileAdaptor(**{'session_class':self.session_class})
+      fa.start_session()                                                        # connect to db
       for root_path,dirs,files in os.walk(self.projet_info_path, topdown=True):
-        for file in files:
+        for file_path in files:
           if fnmatch.fnmatch(file_name, '*.csv'):                               # only consider csv files
-            pass
+            file_check=fa.check_file_records_file_path(file_path=file_path)     # check for file in db
+            if not file_check:
+              new_project_info_list.append(os.path.join(root,file))             # collect new project info files
+      fa.close_session()                                                        # disconnect db
+      return new_project_info_list
     except:
       raise
