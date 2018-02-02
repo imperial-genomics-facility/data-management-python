06f7fd6d5427b46588fb86469acbf44884b01e5f
Avik Datta <reach4avik@yahoo.com>
2018-02-02 10:25:42 +0000

    fixed filepath checking in db for input metdata files

--- igf_data/process/seqrun_processing/find_and_register_new_project_data.py
+++ igf_data/process/seqrun_processing/find_and_register_new_project_data.py
@@ -589,7 +589,8 @@ class Find_and_register_new_project_data:
       for root_path,dirs,files in os.walk(self.projet_info_path, topdown=True):
         for file_path in files:
           if fnmatch.fnmatch(file_path, '*.csv'):                               # only consider csv files
-            file_check=fa.check_file_records_file_path(file_path=file_path)     # check for file in db
+            file_check=fa.check_file_records_file_path(file_path=os.path.join(root_path,
+                                                                              file_path)) # check for filepath in db
             if not file_check:
               new_project_info_list.append(os.path.join(root_path,file_path))   # collect new project info files
       fa.close_session()                                                        # disconnect db
