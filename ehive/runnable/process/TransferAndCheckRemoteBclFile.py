2ed61ed893c3558c4d6bc3f9f86811037c84a744
Avik Datta <reach4avik@yahoo.com>
2018-02-15 14:05:07 +0000

    check of md5 value of existing file before transferring it from remote server

--- ehive/runnable/process/TransferAndCheckRemoteBclFile.py
+++ ehive/runnable/process/TransferAndCheckRemoteBclFile.py
@@ -15,7 +15,6 @@ class TransferAndCheckRemoteBclFile(IGFBaseProcess):
             'seqrun_source':None,
             'seqrun_user':None,
             'seqrun_server':None,
-            
            })
     return params_dict
 
@@ -30,7 +29,7 @@ class TransferAndCheckRemoteBclFile(IGFBaseProcess):
       chacksum_type=self.param_required('checksum_type')
       seqrun_file_name=self.param_required('seqrun_file_name')
       file_md5_value=self.param_required('file_md5')
-    
+      transfer_remote_file=True                                                 # transfer file from remote server
       source_file_path=os.path.join(seqrun_source,
                                     seqrun_igf_id,
                                     seqrun_file_name)                           # get new seqrun path
@@ -39,27 +38,37 @@ class TransferAndCheckRemoteBclFile(IGFBaseProcess):
 
       destination_path=os.path.join(destination_dir,
                                     os.path.basename(seqrun_file_name))         # get destination path
-      if seqrun_user is None and seqrun_server is None:
-        raise ValueError('seqrun: {0}, missing required value for seqrun_user or seqrun_server'.\
-                         format(seqrun_igf_id))
-      
-      source_address='{0}@{1}'.format(seqrun_user,seqrun_server)                # get host username and address
-      copy_remote_file(source_path=source_file_path,
-                       destinationa_path=destination_path, 
-                       source_address=source_address,
-                       check_file=False)                                        # copy remote file
-      if not os.path.exists(destination_path):
-        raise IOError('failed to copy file {0} for seqrun {1}'.\
-                      format(seqrun_file_name,seqrun_igf_id))                   # check destination file after copy
+      if os.path.exists(destination_path) and \
+         os.path.isfile(destination_path):
+        existing_checksum=calculate_file_checksum(destination_path,\
+                                                  hasher=chacksum_type)         # calculate checksum of existing file
+        if existing_checksum == file_md5_value:
+          transfer_remote_file=False                                            # skip file transfer if its up to date
+        else:
+          os.remove(destination_path)                                           # remove existing file
 
-      new_checksum=calculate_file_checksum(destination_path,\
-                                           hasher=chacksum_type)                # calculate checksum of the transferred file
-      if new_checksum != file_md5_value:
-        raise ValueError('seqrun:{3}, checksum not matching for file {0}, expected: {1}, got {2}'.\
-                         format(seqrun_file_name, \
-                                file_md5_value, \
-                                new_checksum, \
-                                seqrun_igf_id)) # raise error if checksum doesn't match
+      if transfer_remote_file:
+        if seqrun_user is None and seqrun_server is None:
+          raise ValueError('seqrun: {0}, missing required value for seqrun_user or seqrun_server'.\
+                           format(seqrun_igf_id))
+
+        source_address='{0}@{1}'.format(seqrun_user,seqrun_server)              # get host username and address
+        copy_remote_file(source_path=source_file_path,
+                         destinationa_path=destination_path, 
+                         source_address=source_address,
+                         check_file=False)                                      # copy remote file
+        if not os.path.exists(destination_path):
+          raise IOError('failed to copy file {0} for seqrun {1}'.\
+                        format(seqrun_file_name,seqrun_igf_id))                 # check destination file after copy
+
+        new_checksum=calculate_file_checksum(destination_path,\
+                                             hasher=chacksum_type)              # calculate checksum of the transferred file
+        if new_checksum != file_md5_value:
+          raise ValueError('seqrun:{3}, checksum not matching for file {0}, expected: {1}, got {2}'.\
+                           format(seqrun_file_name, \
+                                  file_md5_value, \
+                                  new_checksum, \
+                                  seqrun_igf_id))                               # raise error if checksum doesn't match
 
       self.param('dataflow_params',{'seqrun_file_name':seqrun_file_name})
     except Exception as e:
