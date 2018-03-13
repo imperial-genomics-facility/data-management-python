c78dd3602c09adc5c68122ebf777efe1f84646d1
Avik Datta <reach4avik@yahoo.com>
2018-03-13 15:53:51 +0000

    added utility method for parsing seqrun_igf_id list and writing a json file

--- igf_data/utils/project_data_display_utils.py
+++ igf_data/utils/project_data_display_utils.py
@@ -1,5 +1,6 @@
 import gviz_api
 import pandas as pd
+from igf_data.utils.seqrunutils import get_seqrun_date_from_igf_id
 
 def _count_total_reads(data,seqrun_list):
   '''
@@ -34,6 +35,10 @@ def convert_project_data_gviz_data(input_data,output_file):
   output_file: A filepath for writing gviz json data
   '''
   try:
+    if not isinstance(input_data, pd.DataFrame):
+      raise AttributeError('Expecting a pandas dataframe and got {0}'.\
+                           format(type(input_data)))
+
     processed_data=input_data.\
                    pivot_table(values='attribute_value',
                                index=['sample_igf_id',
@@ -82,5 +87,50 @@ def convert_project_data_gviz_data(input_data,output_file):
     final_data=data_table.ToJSon(columns_order=tuple(column_list))              # create final data structure
     with open(output_file,'w') as jf:
         jf.write(final_data)                                                    # write final data to output file
+  except:
+    raise
+
+def _modify_seqrun_data(data_series):
+  '''
+  An internal method for parsing seqrun dataframe and adding remote dir path
+  required columns: seqrun_igf_id, flowcell_id
+  output column: flowcell_id, path
+  '''
+  try:
+    if not isinstance(data_series,pd.Series):
+      raise AttributeError('Expecting a pandas data series and got {0}'.\
+                           format(type(data_series)))
+
+    seqrun_igf_id=data_series['seqrun_igf_id']
+    flowcell_id=data_series['flowcell_id']
+    seqrun_date=get_seqrun_date_from_igf_id(seqrun_igf_id)
+    data_series['path']=os.path.join(seqrun_date,flowcell_id)
+    del data_series['seqrun_igf_id']
+    return data_series
+  except:
+    raise
+
+def add_seqrun_path_info(input_data,output_file):
+  '''
+  A utility method for adding remote path to a dataframe for each sequencing runs
+  of a project
+  
+  required params:
+  input_data: A input dataframe containing the following columns
+              seqrun_igf_id
+              flowcell_id
+  output_file: An output filepath for the json data
+  '''
+  try:
+    if not isinstance(input_data,pd.DataFrame):
+      raise AttributeError('Expecting a pandas dataframe and got {0}'.\
+                           format(type(input_data)))
+
+    input_data.drop_duplicates(inplace=True)                                    # remove duplicate entries
+    input_data=input_data.\
+               apply(lambda line: \
+                     _modify_seqrun_data(data_series=line),
+                     axis=1)                                                    # add remote seqrun path
+    input_data.to_json(output_file)                                             # write output json
   except:
     raise
