268e50bb56f32c54d754718a1d94d7fc980d1a94
Avik Datta <reach4avik@yahoo.com>
2018-05-10 13:05:36 +0100

    loading test data to sqlite db and creating input list

--- test/reset_samplesheet_md5_test.py
+++ test/reset_samplesheet_md5_test.py
@@ -1,7 +1,9 @@
 import unittest,os
+import pandas as pd
 from igf_data.utils.dbutils import read_dbconf_json
 from sqlalchemy import create_engine
 from igf_data.igfdb.igfTables import Base
+from igf_data.igfdb.collectionadaptor import CollectionAdaptor
 
 class Reset_samplesheet_md5_test1(unittest.TestCase):
   def setUp(self):
@@ -16,9 +18,23 @@ class Reset_samplesheet_md5_test1(unittest.TestCase):
     self.json_collection_name='seqrun1'
     self.json_collection_type='ILLUMINA_BCL_MD5'
     self.seqrun_path='data/reset_samplesheet_md5'
+    self.seqrun_input_list='data/reset_samplesheet_md5/seqrun_input_list.txt'
+    ca=CollectionAdaptor(**{'session_class': self.session_class})
+    ca.start_session()
+    data=pd.DataFRame([{'name':self.json_collection_name,
+                        'type':self.json_collection_type,
+                        'table':'seqrun',
+                        'file_path':self.json_file_path,
+                        }])
+    ca.load_file_and_create_collection(data,
+                                       autosave=True,
+                                       hasher='md5')
+    ca.close_session()
+    with open(self.seqrun_input_list,'w') as fp:
+      fp.write(self.json_collection_name)
 
   def tearDown(self):
     Base.metadata.drop_all(self.engine)
     os.remove(self.dbname)
+    os.remove(self.seqrun_input_list)
 
-  
