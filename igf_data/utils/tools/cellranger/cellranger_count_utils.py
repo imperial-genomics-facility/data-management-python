f4c9ceea4dc5a515c50a927e3a64eb25baa7e119
Avik Datta <reach4avik@yahoo.com>
2018-05-25 16:30:19 +0100

    listing fastq dirs for cellranger input

--- igf_data/utils/tools/cellranger/cellranger_count_utils.py
+++ igf_data/utils/tools/cellranger/cellranger_count_utils.py
@@ -1,4 +1,4 @@
-
+import os
 from igf_data.igfdb.baseadaptor import BaseAdaptor
 from igf_data.igfdb.collectionadaptor import CollectionAdaptor
 from igf_data.igfdb.experimentadaptor import ExperimentAdaptor
@@ -16,12 +16,32 @@ def get_cellranger_input_list(db_session_class,experiment_igf_id,
   :returns: A list of fastq dir path for the cellranger count run
   '''
   try:
+    fastq_dir_list=[]                                                           # default list of fastq dirs
     dbconnected=False
     base=BaseAdaptor(**{'session_class':db_session_class})
     base.start_session()
     dbconnected=True
-    
+    subquery=base.session.\
+             query(Run.run_igf_id).\
+             join(Experiment).\
+             filter(Run.experiment_id==Experiment.experiment_id).\
+             filter(Experiment.experiment_igf_id==experiment_igf_id)            # get subquery for run_igf_ids
+    query=base.session.\
+          query(File.file_path).\
+          join(Collection).\
+          join(Collection_group).\
+          filter(Collection.collection_id==Collection_group.collection_id).\
+          filter(Collection_group.file_id==File.file_id).\
+          filter(Collection.type==fastq_collection_type).\
+          filter(Collection.name.in_(subquery))                                 # select file_path linked to run_igf_ids
+    results=base.fetch_records(query=query,
+                               output_mode='object')                            # fetch results as generator expression
+    for row in results:
+      fastq_dir_list.append(os.path.dirname(row.file_path))                     # add fastq directory path to the putput list
+    if len(fastq_dir_list) > 0:
+      fastq_dir_list=list(set(fastq_dir_list))                                  # remove redundant values
     base.close_session()
+    return fastq_dir_list                                                       # return list of fastq dirs
   except:
     if dbconnected:
       base.close_session()
