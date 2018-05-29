bf9f442579eafe67e92b0398731ee2ed390b0d4d
Avik Datta <reach4avik@yahoo.com>
2018-05-29 15:33:28 +0100

    added method for checking cellranger output directory

--- igf_data/utils/tools/cellranger/cellranger_count_utils.py
+++ igf_data/utils/tools/cellranger/cellranger_count_utils.py
@@ -5,8 +5,8 @@ from igf_data.igfdb.experimentadaptor import ExperimentAdaptor
 from igf_data.igfdb.igfTables import File,Experiment,Run,Collection,Collection_group
 
 
-def get_cellranger_input_list(db_session_class,experiment_igf_id,
-                              fastq_collection_type='demultiplexed_fastq'):
+def get_cellranger_count_input_list(db_session_class,experiment_igf_id,
+                                    fastq_collection_type='demultiplexed_fastq'):
   '''
   A function for fetching input list for cellranger count run for a specific experiment
   
@@ -45,4 +45,54 @@ def get_cellranger_input_list(db_session_class,experiment_igf_id,
   except:
     if dbconnected:
       base.close_session()
+    raise
+
+def check_cellranger_count_outrput(output_path,
+                                   file_list=['web_summary.html',
+                                              'metrics_summary.csv',
+                                              'possorted_genome_bam.bam',
+                                              'possorted_genome_bam.bam.bai',
+                                              'filtered_gene_bc_matrices_h5.h5',
+                                              'raw_gene_bc_matrices_h5.h5',
+                                              'molecule_info.h5',
+                                              'cloupe.cloupe',
+                                              'analysis/tsne/2_components/projection.csv',
+                                              'analysis/clustering/graphclust/clusters.csv',
+                                              'analysis/diffexp/kmeans_3_clusters/differential_expression.csv',
+                                              'analysis/pca/10_components/variance.csv'
+                                             ]):
+  '''
+  A function for checking cellranger count output
+  
+  :param output_path: A filepath for cellranger count output directory
+  :param file_list: List of files to check in the output directory
+    
+                      default file list to check
+                        web_summary.html
+                        metrics_summary.csv
+                        possorted_genome_bam.bam
+                        possorted_genome_bam.bam.bai
+                        filtered_gene_bc_matrices_h5.h5
+                        raw_gene_bc_matrices_h5.h5
+                        molecule_info.h5
+                        cloupe.cloupe
+                        analysis/tsne/2_components/projection.csv
+                        analysis/clustering/graphclust/clusters.csv
+                        analysis/diffexp/kmeans_3_clusters/differential_expression.csv
+                        analysis/pca/10_components/variance.csv
+  :returns: Nill
+  :raises LookupError: when any file is missing from the output path
+  '''
+  try:
+    file_dict_value=[i for i in range(len(file_list))]                          # get file index values
+    file_dict=dict(zip(file_list,file_dict_value))                              # convert file list to a dictionary
+    for root,dir,files in os.walk(output_path):
+      for file in files:
+        if file in file_dict:
+          del file_dict[file]                                                   # remove file from dictionary if its present
+    if len(list(file_dict.keys())) > 0:
+      raise LookupError('Partial Cellranger count output found, missing following files: {0}'.\
+                    format(',',join(list(file_dict.keys()))))
+
+  except:
     raise
