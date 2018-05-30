e74bb77502fb8fb513661a1910f5c08e6874a277
Avik Datta <reach4avik@yahoo.com>
2018-05-30 10:58:48 +0100

    adding docstring to the run method

--- ehive/runnable/process/alignment/RunCellrangerCount.py
+++ ehive/runnable/process/alignment/RunCellrangerCount.py
@@ -5,10 +5,10 @@ from shutil import copytree
 from igf_data.utils.fileutils import get_temp_dir,remove_dir
 from ehive.runnable.IGFBaseProcess import IGFBaseProcess
 from igf_data.utils.tools.cellranger.cellranger_count_utils import get_cellranger_count_input_list
-from igf_data.utils.tools.cellranger.cellranger_count_utils import check_cellranger_count_outrput
+from igf_data.utils.tools.cellranger.cellranger_count_utils import check_cellranger_count_output
 from igf_data.utils.tools.cellranger.cellranger_count_utils import get_cellranger_reference_genome
 
-class RunBcl2Fastq(IGFBaseProcess):
+class RunellrangerCount(IGFBaseProcess):
   '''
   A ehive process class for running cellranger count pipeline
   '''
@@ -24,6 +24,29 @@ class RunBcl2Fastq(IGFBaseProcess):
     return params_dict
 
   def run(self):
+    '''
+    A method for running the cellranger count for a given sample using ehive pipeline
+    
+    :param project_igf_id: A project igf id
+    :param experiment_igf_id: An experiment igf id
+    :param sample_igf_id: A sample igf id
+    :param igf_session_class: A database session class
+    :param cellranger_exe: Cellranger executable path
+    :param cellranger_options: Cellranger parameters
+                               
+                               List of default parameters
+                                 --jobmode=pbspro
+                                 --localcores=1
+                                 --localmem=4
+                                 --mempercore=4
+                                 --maxjobs=20
+    
+    :param base_work_dir: Base work directory path
+    :param fastq_collection_type: Collection type name for input fastq files, default demultiplexed_fastq
+    :param species_name: Reference genome collection name
+    :param reference_type: Reference genome collection type, default cellranger_reference
+    :returns: Adding cellranger_output to the dataflow_params
+    '''
     try:
       project_igf_id=self.param_required('project_igf_id')
       experiment_igf_id=self.param_required('experiment_igf_id')
@@ -31,11 +54,12 @@ class RunBcl2Fastq(IGFBaseProcess):
       sample_submitter_id=self.param_required('sample_submitter_id')
       igf_session_class=self.param_required('igf_session_class')
       cellranger_exe=self.param_required('cellranger_exe')
+      cellranger_options=self.param_required('cellranger_options')
       base_work_dir=self.param_required('base_work_dir')
       fastq_collection_type=self.param_required('fastq_collection_type')
       species_name=self.param('species_name')
       reference_type=self.param('reference_type')
-      
+
       job_name=self.job_name()
       work_dir=os.path.join(base_work_dir,seqrun_igf_id,job_name)               # get work directory name
       if not os.path.exists(work_dir):
@@ -62,10 +86,11 @@ class RunBcl2Fastq(IGFBaseProcess):
                      ]                                                          # set initial parameters
       cellranger_cmd.extend(cellranger_options)                                 # add optional parameters
       subprocess.check_call(cellranger_cmd)                                     # run cellranger count
-      check_cellranger_count_outrput(output_path=os.path.join(work_dir,
-                                                              experiment_igf_id,
-                                                              'outs'))          # check output file
-
+      cellranger_output=os.path.join(work_dir,
+                                     experiment_igf_id,
+                                     'outs')                                    # get cellranger output path
+      check_cellranger_count_output(output_path=cellranger_output)              # check output file
+      self.param('dataflow_params',{'cellranger_output':cellranger_output})     # pass on cellranger output path
     except:
       message='project: {2}, sample:{3}, Error in {0}: {1}'.format(self.__class__.__name__, \
                                                       e, \
