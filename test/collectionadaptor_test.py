bdcfe125c7199a52ba3872ed79328fea342d07b9
Avik Datta <reach4avik@yahoo.com>
2018-06-06 14:10:01 +0100

    added unittest for collection group removal method

--- test/collectionadaptor_test.py
+++ test/collectionadaptor_test.py
@@ -38,7 +38,7 @@ class CollectionAdaptor_test1(unittest.TestCase):
     fa.close_session()
     file_md5=file_data.md5
     self.assertEqual(file_md5, 'd5798564a14a09b9e80640ac2f42f47e')
-    
+
   def test_fetch_collection_name_and_table_from_file_path(self):
     file_path='data/collect_fastq_dir/1_16/IGFP0001_test_22-8-2017_rna/IGF00002/IGF00002-2_S1_L001_R1_001.fastq.gz'
     data=[{'name':'IGF00001_MISEQ_000000000-D0YLK_1',\
@@ -52,6 +52,54 @@ class CollectionAdaptor_test1(unittest.TestCase):
     (name,table)=ca.fetch_collection_name_and_table_from_file_path(file_path)
     ca.close_session()
     self.assertEqual(name, 'IGF00001_MISEQ_000000000-D0YLK_1')
-    
+
+  def test_remove_collection_group_info(self):
+    collection_data=[{ 'name':'IGF001_MISEQ',
+                     'type':'ALIGNMENT_CRAM',
+                     'table':'experiment',
+                     'file_path':'a.cram',
+                   },
+                   { 'name':'IGF001_MISEQ',
+                     'type':'ALIGNMENT_CRAM',
+                     'table':'experiment',
+                     'file_path':'a1.cram',
+                   },
+                   { 'name':'IGF003_MISEQ',
+                     'type':'ALIGNMENT_CRAM',
+                     'table':'experiment',
+                     'file_path':'b.cram',
+                   },
+                   { 'name':'IGF003_MISEQ',
+                     'type':'ALIGNMENT_CRAM',
+                     'table':'experiment',
+                     'file_path':'b1.cram',
+                   }]
+    ca=CollectionAdaptor(**{'session_class':self.session_class})
+    ca.start_session()
+    ca.load_file_and_create_collection(data=collection_data,
+                                       calculate_file_size_and_md5=False)
+    remove_data_list1=[{'name':'IGF001_MISEQ',
+                        'type':'ALIGNMENT_CRAM',
+                        'table':'experiment',
+                        'file_path':'a.cram',
+                       }]
+    ca.remove_collection_group_info(data=remove_data_list1)
+    cg_data=ca.get_collection_files(collection_name='IGF001_MISEQ',
+                                    collection_type='ALIGNMENT_CRAM',
+                                    output_mode='dataframe')
+    self.assertEqual(len(cg_data.index), 1)
+    self.assertEqual(cg_data['file_path'].values[0],'a1.cram')
+    remove_data_list2=[{ 'name':'IGF003_MISEQ',
+                         'type':'ALIGNMENT_CRAM',
+                         'table':'experiment',
+                      }]
+    ca.remove_collection_group_info(data=remove_data_list2)
+    cg_data2=ca.get_collection_files(collection_name='IGF003_MISEQ',
+                                     collection_type='ALIGNMENT_CRAM',
+                                     output_mode='dataframe')
+    self.assertEqual(len(cg_data2.index), 0)
+    ca.close_session()
+
+
 if __name__=='__main__':
   unittest.main()
