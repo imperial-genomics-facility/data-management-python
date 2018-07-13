7991819a3dcfd2048ef69d6f433670b1f2ff9430
Avik Datta <reach4avik@yahoo.com>
2018-07-13 12:10:55 +0100

    file md5 calculation is optional

--- scripts/db_scripts/load_files_collecion_to_db.py
+++ scripts/db_scripts/load_files_collecion_to_db.py
@@ -7,22 +7,27 @@ A script for loading file collections to the database
 
 :param collection_file_data: A json file with following entries
 
-                            name (collection name)
-                            type (collection type)
-                            table (collection table)
-                            file_path
+                            name (required)
+                            type (required)
+                            table
+                            file_path (required)
                             location
+                            size
+                            md5
 
 :param dbconfig_path: A database configuration file
+:param calculate_checksum: Toggle file checksum calculation
 '''
 
 parser=argparse.ArgumentParser()
 parser.add_argument('-f','--collection_file_data', required=True, help='Collection file data json file')
 parser.add_argument('-d','--dbconfig_path', required=True, help='Database configuration json file')
+parser.add_argument('-s','--calculate_checksum', default=False, action='store_true', help='Toggle file checksum calculation')
 args=parser.parse_args()
 
 dbconfig_path=args.dbconfig_path
 collection_file_data=args.collection_file_data
+calculate_checksum=args.calculate_checksum
 
 try:
   dbconnected=False
@@ -38,7 +43,7 @@ try:
   ca.start_session()                                                            # connect to database
   dbconnected=True
   ca.load_file_and_create_collection(data=collection_data,
-                                     calculate_file_size_and_md5=True,
+                                     calculate_file_size_and_md5=calculate_checksum,
                                      autosave=True)                             # load data and commit changes
   ca.close_session()
   dbconnected=False
