75b7dda6b295bea2e2c32e3ebada125ad4e2dbc8
Avik Datta <reach4avik@yahoo.com>
2018-07-17 11:36:57 +0100

    added new util method for fastq file fetching for individual experiment ids

---
+++ igf_data/utils/analysis_fastq_fetch_utils.py
@@ -0,0 +1,60 @@
+import os
+from igf_data.igfdb.baseadaptor import BaseAdaptor
+from igf_data.igfdb.collectionadaptor import CollectionAdaptor
+from igf_data.igfdb.experimentadaptor import ExperimentAdaptor
+from igf_data.igfdb.igfTables import File,Experiment,Run,Collection,Collection_group
+
+def get_fastq_input_list(db_session_class,experiment_igf_id,combine_fastq_dir=False,
+                         fastq_collection_type='demultiplexed_fastq',
+                         active_status='ACTIVE'):
+  '''
+  A function for fetching all the fastq files linked to a specific experiment id
+  
+  :param db_session_class: A database session class
+  :param experiment_igf_id: An experiment igf id
+  :param fastq_collection_type: Fastq collection type name, default demultiplexed_fastq
+  :param active_status: text label for active runs, default ACTIVE
+  :param combine_fastq_dir: Combine fastq file directories for output line, default False
+  :returns: A list of fastq file or fastq dir paths for the analysis run
+  :raises ValueError: It raises ValueError if no fastq directory found
+  '''
+  try:
+    fastq_dir_list=[]                                                           # default list of fastq dirs
+    dbconnected=False
+    base=BaseAdaptor(**{'session_class':db_session_class})
+    base.start_session()
+    dbconnected=True
+    subquery=base.session.\
+             query(Run.run_igf_id).\
+             join(Experiment).\
+             filter(Run.experiment_id==Experiment.experiment_id).\
+             filter(Experiment.experiment_igf_id==experiment_igf_id).\
+             filter(Run.status==active_status)                                  # get subquery for run_igf_ids
+    query=base.session.\
+          query(File.file_path).\
+          join(Collection_group).\
+          join(Collection).\
+          filter(Collection.collection_id==Collection_group.collection_id).\
+          filter(Collection_group.file_id==File.file_id).\
+          filter(Collection.type==fastq_collection_type).\
+          filter(Collection.name.in_(subquery))                                 # select file_path linked to run_igf_ids
+    results=base.fetch_records(query=query,
+                               output_mode='object')                            # fetch results as generator expression
+    for row in results:
+      if combine_fastq_dir:
+        fastq_output_list.append(os.path.dirname(row.file_path))                # add fastq directory path to the output list
+      else:
+        fastq_output_list.append(row.file_path)                                 # add fastq files to the output list
+
+    if len(fastq_output_list) > 0:
+      fastq_output_list=list(set(fastq_output_list))                            # remove redundant values
+    else:
+      raise ValueError('No fastq found for experiment {0}'.\
+                       format(experiment_igf_id))
+
+    base.close_session()
+    return fastq_dir_list                                                       # return list of fastq dirs
+  except:
+    if dbconnected:
+      base.close_session()
+    raise
