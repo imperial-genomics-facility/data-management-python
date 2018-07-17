57bb8d7a542eff8f6cededb419cf076b35d4d30b
Avik Datta <reach4avik@yahoo.com>
2018-07-17 11:41:49 +0100

    updated fastq dir fetching for cellranger using the generic method

--- igf_data/utils/tools/cellranger/cellranger_count_utils.py
+++ igf_data/utils/tools/cellranger/cellranger_count_utils.py
@@ -1,9 +1,5 @@
 import os
-from igf_data.igfdb.baseadaptor import BaseAdaptor
-from igf_data.igfdb.collectionadaptor import CollectionAdaptor
-from igf_data.igfdb.experimentadaptor import ExperimentAdaptor
-from igf_data.igfdb.igfTables import File,Experiment,Run,Collection,Collection_group
-
+from igf_data.utils.analysis_fastq_fetch_utils import get_fastq_input_list
 
 def get_cellranger_count_input_list(db_session_class,experiment_igf_id,
                                     fastq_collection_type='demultiplexed_fastq',
@@ -19,41 +15,15 @@ def get_cellranger_count_input_list(db_session_class,experiment_igf_id,
   :raises ValueError: It raises ValueError if no fastq directory found
   '''
   try:
-    fastq_dir_list=[]                                                           # default list of fastq dirs
-    dbconnected=False
-    base=BaseAdaptor(**{'session_class':db_session_class})
-    base.start_session()
-    dbconnected=True
-    subquery=base.session.\
-             query(Run.run_igf_id).\
-             join(Experiment).\
-             filter(Run.experiment_id==Experiment.experiment_id).\
-             filter(Experiment.experiment_igf_id==experiment_igf_id).\
-             filter(Run.status==active_status)                                  # get subquery for run_igf_ids
-    query=base.session.\
-          query(File.file_path).\
-          join(Collection_group).\
-          join(Collection).\
-          filter(Collection.collection_id==Collection_group.collection_id).\
-          filter(Collection_group.file_id==File.file_id).\
-          filter(Collection.type==fastq_collection_type).\
-          filter(Collection.name.in_(subquery))                                 # select file_path linked to run_igf_ids
-    results=base.fetch_records(query=query,
-                               output_mode='object')                            # fetch results as generator expression
-    for row in results:
-      fastq_dir_list.append(os.path.dirname(row.file_path))                     # add fastq directory path to the putput list
-    if len(fastq_dir_list) > 0:
-      fastq_dir_list=list(set(fastq_dir_list))                                  # remove redundant values
-    else:
-      raise ValueError('No fastq directory found for experiment {0}'.\
-                       format(experiment_igf_id))
-
-    base.close_session()
+    fastq_dir_list=get_fastq_input_list(db_session_class=db_session_class,
+                                        experiment_igf_id=experiment_igf_id,
+                                        combine_fastq_dir=True,
+                                        fastq_collection_type='demultiplexed_fastq',
+                                        active_status='ACTIVE')                 # get fastq dir list
     return fastq_dir_list                                                       # return list of fastq dirs
   except:
-    if dbconnected:
-      base.close_session()
     raise
+
 
 def check_cellranger_count_output(output_path,
                                   file_list=['web_summary.html',
