33136de3374ec83e041b1f5464feec84a814ad89
Avik Datta <reach4avik@yahoo.com>
2018-07-18 14:56:29 +0100

    updated picard tool wrapper for list of specific commands

--- igf_data/utils/tools/picard_util.py
+++ igf_data/utils/tools/picard_util.py
@@ -1,34 +1,156 @@
 import os, subprocess
 from shlex import quote
+from igf_data.utils.fileutils import check_file_path
 
-class Picard_util:
+class Picard_tools:
   '''
   A class for running picard tool
   
   :param java_exe: Java executable path
   :param picard_jar: Picard path
+  :param input_file: Input bam filepath
+  :param output_file: Output filepath
+  :param ref_fasta: Input reference fasta filepath
+  :param picard_option: Additional picard run parameters as dictionary, default None
+  :param chart_file: Output chart filepath, default None
+  :param metrics_file: Output metrics filepath, default None
   :param java_param: Java parameter, default '-Xmx4g'
+  :param strand_info: RNA-Seq strand information, default NONE
+  :param ref_flat_file: Input ref_flat file path, default None
+  :param suported_commands: A list of supported picard commands
+  
+                           CollectAlignmentSummaryMetrics
+                           CollectGcBiasMetrics
+                           QualityScoreDistribution
+                           CollectRnaSeqMetrics
+                           CollectBaseDistributionByCycle
+                           MarkDuplicates
+                           AddOrReplaceReadGroups
   '''
-  def __init__(self,java_exe,picard_jar,java_param='-Xmx4g'):
+  def __init__(self,java_exe,picard_jar,input_file,output_file,ref_fasta,
+               chart_file=None,metrics_file=None,picard_option=None,
+               java_param='-Xmx4g',strand_info='NONE',ref_flat_file=None,
+               suported_commands=['CollectAlignmentSummaryMetrics',
+                                  'CollectGcBiasMetrics',
+                                  'QualityScoreDistribution',
+                                  'CollectRnaSeqMetrics',
+                                  'CollectBaseDistributionByCycle',
+                                  'MarkDuplicates',
+                                  'AddOrReplaceReadGroups']):
     try:
+      check_file_path(file_path=java_exe)
       self.java_exe=java_exe
+      check_file_path(file_path=picard_jar)
       self.picard_jar=picard_jar
       self.java_param=java_param
-      if not os.path.exists(self.java_exe):
-        raise IOError('Missing java executable')
-
-      if not os.path.exists(self.picard_jar):
-        raise IOError('Missing picard jar file')
+      check_file_path(file_path=input_file)
+      self.input_file=input_file
+      self.output_file=output_file
+      check_file_path(file_path=ref_fasta)
+      self.ref_fasta=ref_fasta
+      self.chart_file=chart_file
+      self.metrics_file=metrics_file
+      self.picard_option=picard_option
+      self.strand_info=strand_info
+      self.ref_flat_file=ref_flat_file
+      self.suported_commands=suported_commands
 
     except:
       raise
 
-  def run_picard_command(self,command_name,picard_option):
+  def _get_param_for_picard_command(self,command_name):
+    '''
+    An internal method for configuring run parameters for picard commands
+    
+    :param command_name: A picard command name
+    :returns: A dictionary of picard run parameter if command is supported or None 
+    '''
+    try:
+      param_dict=None
+      if command_name=='CollectAlignmentSummaryMetrics':
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file,
+                    'R':self.ref_fasta
+                   })
+
+      elif command_name=='CollectGcBiasMetrics':
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file,
+                    'R':self.ref_fasta
+                   })
+        if self.chart_file is not None:
+          param_dict.update({'CHART':self.chart_file})                          # add chart file for GC metrics
+
+        if self.metrics_file is not None:
+          param_dict.update({'S':self.metrics_file})                            # add summary metrics for GC metrics
+
+      elif command_name=='QualityScoreDistribution':
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file,
+                   })
+        if self.chart_file is not None:
+          param_dict.update({'CHART':self.chart_file})                          # add chart file for qual dist metrics
+
+      elif command_name=='CollectRnaSeqMetrics':
+        if self.ref_flat_file is None:
+          raise ValueError('Missing refFlat annotation file for command {0}'.\
+                           format(command_name))
+
+        check_file_path(file_path=ref_flat_file)                                # check refFlat file path
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file,
+                    'R':self.ref_fasta,
+                    'REF_FLAT':self.ref_flat_file,
+                    'STRAND':self.strand_info
+                   })
+        if self.chart_file is not None:
+          param_dict.update({'CHART':self.chart_file})                          # add chart file for rna-seq metrics
+
+      elif command_name=='CollectBaseDistributionByCycle':
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file
+                   })
+        if self.chart_file is not None:
+          param_dict.update({'CHART':self.chart_file})                          # add chart file for base dist metrics
+
+      elif command_name=='MarkDuplicates':
+        if self.metrics_file is None:
+          raise ValueError('Missing metrics output path for running command {0}'.\
+                           format(command_name))                                # metrics file is required for mark duplicate
+
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file,
+                    'M':self.metrics_file
+                   })
+
+      elif command_name=='AddOrReplaceReadGroups':
+        param_dict=dict()
+        param_dict.update({
+                    'I':self.input_file,
+                    'O':self.output_file
+                   })                                                           # not checking for other required inputs
+
+      return param_dict
+    except:
+      raise
+
+  def run_picard_command(self,command_name):
     '''
     A method for running generic picard command
     
     :param command_name: Picard command name
-    :param picard_option: Picard command options as python dictionary
     '''
     try:
       command=[self.java_exe,
@@ -36,14 +158,22 @@ class Picard_util:
                '-jre',
                self.picard_jar,
                quote(command_name)]
-      if isinstance(picard_option,dict) and \
-          len(picard_option)>1:
+      if isinstance(self.picard_option,dict) and \
+          len(self.picard_option)>0:
         picard_option=['{0}={1}'.format(param,quote(val))
-                       for param,val in picard_option.items()]
-        command.extend(picard_option)
-        subprocess.check_call(command)
+                       for param,val in self.picard_option.items()]
+        command.extend(picard_option)                                           # additional picard params
+
+      picard_run_patam=self._get_param_for_picard_command(command_name=command_name)
+      if isinstance(picard_run_patam,dict) and \
+          len(picard_run_patam)>1:
+        picard_option=['{0}={1}'.format(param,quote(val))
+                       for param,val in picard_run_patam.items()]
+        command.extend(picard_option)                                           # main picard params
+        subprocess.check_call(command)                                          # run picard
       else:
-        raise ValueError('Missing required parameters')
+        raise ValueError('Picard command {0} not supported yet'.\
+                         format(command_name))
 
     except:
       raise
