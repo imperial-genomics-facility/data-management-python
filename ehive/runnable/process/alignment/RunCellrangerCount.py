b5210ecf1ed248e4363c9bdaf9f78af6ca067c2b
Avik Datta <reach4avik@yahoo.com>
2018-07-18 10:53:42 +0100

    removed cellranger output processing from the run module

--- ehive/runnable/process/alignment/RunCellrangerCount.py
+++ ehive/runnable/process/alignment/RunCellrangerCount.py
@@ -4,13 +4,10 @@ from shlex import quote
 from shutil import copytree
 from fnmatch import fnmatch
 from igf_data.utils.fileutils import get_temp_dir,remove_dir,move_file
-from igf_data.utils.fileutils import create_file_manifest_for_dir
-from igf_data.utils.fileutils import prepare_file_archive
 from ehive.runnable.IGFBaseProcess import IGFBaseProcess
 from igf_data.utils.tools.cellranger.cellranger_count_utils import get_cellranger_count_input_list
 from igf_data.utils.tools.cellranger.cellranger_count_utils import check_cellranger_count_output
 from igf_data.utils.tools.reference_genome_utils import Reference_genome_utils
-from igf_data.utils.analysis_collection_utils import Analysis_collection_utils
 
 class RunCellrangerCount(IGFBaseProcess):
   '''
@@ -136,60 +133,8 @@ class RunCellrangerCount(IGFBaseProcess):
       self.comment_asana_task(task_name=project_igf_id, comment=message)        # send comment to Asana
       # validate output files after cellranger run
       check_cellranger_count_output(output_path=cellranger_output)              # check output file
-      # prepare manifest file for the results dir
-      manifest_file=os.path.join(cellranger_output,
-                                 manifest_filename)                             # get name of the manifest file
-      create_file_manifest_for_dir(results_dirpath=cellranger_output,
-                                   output_file=manifest_file,
-                                   md5_label='md5',
-                                   exclude_list=['*.bam','*.bai','*.cram'])     # create manifest for output dir
-      # create archive for the results dir
-      temp_archive_name=os.path.join(get_temp_dir(),
-                                     '{0}.tar.gz'.format(experiment_igf_id))    # get the name of temp archive file
-      prepare_file_archive(results_dirpath=cellranger_output,
-                           output_file=temp_archive_name,
-                           exclude_list=['*.bam','*.bai','*.cram'])             # archive cellranget output
-      # load archive file to db collection and results dir
-      au=Analysis_collection_utils(dbsession_class=igf_session_class,
-                                   analysis_name=analysis_name,
-                                   tag_name=species_name,
-                                   collection_name=experiment_igf_id,
-                                   collection_type=collection_type,
-                                   collection_table=collection_table,
-                                   base_path=base_result_dir)                   # initiate loading of archive file
-      output_file_list=au.load_file_to_disk_and_db(\
-                            input_file_list=[temp_archive_name],
-                            withdraw_exisitng_collection=True)                  # load file to db and disk
-      # find bam path for the data flow
-      bam_list=list()                                                           # define empty bamfile list
-      for file in os.listdir(cellranger_output):
-        if fnmatch(file, '*.bam'):
-          bam_list.append(os.path.join(cellranger_output,
-                                       file))                                   # add all bams to bam_list
 
-      if len(bam_list)>1:
-        raise ValueError('More than one bam found for cellranger count run:{0}'.\
-                         format(cellranger_output))                             # check number of bams, presence of one bam is already validated by check method
-
-      bam_file=bam_list[0]
-      au=Analysis_collection_utils(dbsession_class=igf_session_class,
-                                   analysis_name=analysis_name,
-                                   tag_name=species_name,
-                                   collection_name=experiment_igf_id,
-                                   collection_type=collection_type,
-                                   collection_table=collection_table)           # initiate bam file rename
-      new_bam_name=au.get_new_file_name(input_file=bam_file)
-      if os.path.basename(bam_file)!=new_bam_name:
-        new_bam_name=os.path.join(os.path.dirname(bam_file),
-                              new_bam_name)                                     # get ne bam path
-        move_file(source_path=bam_file,
-                  destinationa_path=new_bam_name,
-                  force=True)                                                   # move bam file
-
-      self.param('dataflow_params',{'cellranger_output':cellranger_output,
-                                    'bam_file':bam_file,
-                                    'analysis_output_list':output_file_list
-                                   })                                           # pass on cellranger output path
+      self.param('dataflow_params',{'cellranger_output':cellranger_output})     # pass on cellranger output path
     except Exception as e:
       message='project: {2}, sample:{3}, Error in {0}: {1}'.format(self.__class__.__name__, \
                                                       e, \
