a00e20b10d58ea4554bd26db23506bdd5011c5da
Avik Datta <reach4avik@yahoo.com>
2018-07-19 20:10:52 +0100

    updated samtools runnable

--- ehive/runnable/process/alignment/RunSamtools.py
+++ ehive/runnable/process/alignment/RunSamtools.py
@@ -1,12 +1,14 @@
-
+import os
 from ehive.runnable.IGFBaseProcess import IGFBaseProcess
+from igf_data.utils.fileutils import move_file,get_temp_dir,remove_dir
+from igf_data.utils.tools.samtools_utils import run_bam_flagstat,run_bam_idxstat
 
 class RunSamtools(IGFBaseProcess):
   '''
   A ehive process class for running samtools analysis
   '''
   def param_defaults(self):
-    params_dict=super(ConvertBamToCram,self).param_defaults()
+    params_dict=super(RunSamtools,self).param_defaults()
     params_dict.update({
         'reference_type':'GENOME_FASTA',
         'threads':4,
@@ -32,7 +34,33 @@ class RunSamtools(IGFBaseProcess):
       bam_file=self.param_required('bam_file')
       reference_type=self.param('reference_type')
       threads=self.param('threads')
-      
+      base_work_dir=self.param_required('base_work_dir')
+      samtools_command=self.param_required('samtools_command')
+      temp_output_dir=get_temp_dir()                                            # get temp work dir
+      work_dir_prefix=os.path.join(base_work_dir,
+                                   project_igf_id,
+                                   sample_igf_id,
+                                   experiment_igf_id)
+      work_dir=self.get_job_work_dir(work_dir=work_dir_prefix)                  # get a run work dir
+      if samtools_command == 'idxstats':
+        temp_output=run_bam_idxstat(bam_file=bam_file,
+                                    output_dir=temp_output_dir,
+                                    force=True)
+      elif samtools_command == 'flagstat':
+        temp_output=run_bam_flagstat(bam_file=bam_file,
+                                     output_dir=temp_output_dir,
+                                     threads=threads,
+                                     force=True)
+      else:
+        raise ValueError('Samtools command {0} not supported'.\
+                         format(samtools_command))
+
+      dest_path=os.path.join(work_dir_prefix,
+                             os.path.basename(temp_output))
+      move_file(source_path=temp_output,
+                destinationa_path=dest_path,
+                force=True)
+      self.param('dataflow_params',{samtools_command:dest_path})                # pass on output list
     except Exception as e:
       message='project: {2}, sample:{3}, Error in {0}: {1}'.format(self.__class__.__name__, \
                                                       e, \
