27ba8c1dd6a823c99c5250e444b5f0cf4a6bd724
Avik Datta <reach4avik@yahoo.com>
2018-09-28 13:31:30 +0100

    removed unused param

--- igf_data/utils/tools/fastp_util.py
+++ igf_data/utils/tools/fastp_util.py
@@ -5,12 +5,11 @@ class Fastp_utils:
   A class for running fastp tool for a list of input fastq files
   
   '''
-  def __init__(self,fastp_exe,input_fastq_list,output_dir,split_by_line=False,
+  def __init__(self,fastp_exe,input_fastq_list,output_dir,
                split_by_lines_count=5000000,fastp_options={}):
     self.fastp_exe=fastp_exe
     self.input_fastq_list=input_fastq_list
     self.output_dir=output_dir
-    self.split_by_line=split_by_line
     self.split_by_lines_count=split_by_lines_count
     self.fastp_options=fastp_options
 
