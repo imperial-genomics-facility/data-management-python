a43c29869d94386637058d4bfb3f13eb6283139b
Avik Datta <reach4avik@yahoo.com>
2018-09-28 13:30:28 +0100

    added initial structure for fastp wrapper

---
+++ igf_data/utils/tools/fastp_util.py
@@ -0,0 +1,31 @@
+import os,subprocess
+
+class Fastp_utils:
+  '''
+  A class for running fastp tool for a list of input fastq files
+  
+  '''
+  def __init__(self,fastp_exe,input_fastq_list,output_dir,split_by_line=False,
+               split_by_lines_count=5000000,fastp_options={}):
+    self.fastp_exe=fastp_exe
+    self.input_fastq_list=input_fastq_list
+    self.output_dir=output_dir
+    self.split_by_line=split_by_line
+    self.split_by_lines_count=split_by_lines_count
+    self.fastp_options=fastp_options
+
+  def run_adapter_trimming(self):
+    '''
+    '''
+    try:
+      pass
+    except:
+      raise
+
+  def run_adapter_trimming_and_splitting(self):
+    '''
+    '''
+    try:
+      pass
+    except:
+      raise
