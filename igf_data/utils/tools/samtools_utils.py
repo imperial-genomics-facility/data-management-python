f9ed3b59a44da40fb1dc0f01fb3af816e6f064d9
Avik Datta <reach4avik@yahoo.com>
2018-10-02 14:25:43 +0100

    changed samtools wrapper and removed pysam dependency

--- igf_data/utils/tools/samtools_utils.py
+++ igf_data/utils/tools/samtools_utils.py
@@ -1,8 +1,6 @@
-import os
-import pysam
-import fnmatch
+import os,subprocess,fnmatch
 from shlex import quote
-from igf_data.utils.fileutils import get_temp_dir,remove_dir,move_file
+from igf_data.utils.fileutils import check_file_path,get_temp_dir,remove_dir,copy_local_file
 
 def _check_cram_file(cram_path):
   '''
@@ -13,10 +11,7 @@ def _check_cram_file(cram_path):
   :raises IOError: It raises IOError if the cram_path doesn't exists
   '''
   try:
-    if not os.path.exists(cram_path):
-      raise IOError('Output cram not present {0}'.\
-                    format(cram_path))
-
+    check_file_path(cram_path)
     if not fnmatch.fnmatch(cram_path, '*.cram'):
       raise ValueError('Cram file extension is not correct: {0}'.\
                        format(cram_path))
@@ -33,34 +28,41 @@ def _check_bam_file(bam_file):
   :raises IOError: It raises IOError if the bam_file doesn't exists
   '''
   try:
-    if not os.path.exists(bam_file):
-      raise IOError('Input bam not found {0}'.format(bam_file))
-
+    check_file_path(bam_file)
     if not fnmatch.fnmatch(bam_file, '*.bam'):
       raise ValueError('Bam file extension is not correct: {0}'.\
                        format(bam_file))
   except:
     raise
 
-def _check_bam_index(bam_file):
+def _check_bam_index(samtools_exe,bam_file):
   '''
   An internal method for checking bam index files. It will generate a new index if its not found.
   
+  :param samtools_exe: samtools executable path
   :param bam_file: A bam file path
   '''
   try:
+    check_file_path(samtools_exe)
     bam_index='{0}.bai'.format(bam_file)
     if not os.path.exists(bam_index):
-      pysam.index(quote(bam_file))                                              # generate bam index, if its not present
+      index_cmd=[quotes(samtools_exe),
+                 'index',
+                 quote(bam_file)
+                ]                                                               # generate bam index, if its not present
+      subprocess.check_call(index_cmd,
+                            shell=False)
 
   except:
     raise
 
 
-def convert_bam_to_cram(bam_file,reference_file,cram_path,threads=1,force=False):
+def convert_bam_to_cram(samtools_exe,bam_file,reference_file,cram_path,threads=1,
+                        force=False):
   '''
   A function for converting bam files to cram using pysam utility
   
+  :param samtools_exe: samtools executable path
   :param bam_file: A bam filepath with / without index. Index file will be created if its missing
   :param reference_file: Reference genome fasta filepath
   :param cram_path: A cram output file path
@@ -73,35 +75,38 @@ def convert_bam_to_cram(bam_file,reference_file,cram_path,threads=1,force=False)
                       cram_path doesn't have .cram extension
   '''
   try:
-    if not os.path.exists(reference_file):
-      raise IOError('Input reference fasta not found {0}'.format(reference_file))
-
+    check_file_path(samtools_exe)
+    check_file_path(reference_file)
     _check_bam_file(bam_file=bam_file)                                          # check bam file
-    _check_bam_index(bam_file=bam_file)                                         # check bam index
-
+    _check_bam_index(samtools_exe=samtools_exe,
+                     bam_file=bam_file)                                         # check bam index
     temp_file=os.path.join(get_temp_dir(),
                            os.path.basename(cram_path))                         # get temp cram file path
-    pysam.samtools.view(bam_file,
-                        '-C',
-                        '-OCRAM',
-                        '-@{0}'.format(threads),
-                        '-T{0}'.format(quote(reference_file)),
-                        '-o{0}'.format(quote(temp_file)),
-                        catch_stdout=False
-                        )                                                       # convert bam to cram using pysam view
+    view_cmd=[quotes(samtools_exe),
+              'view',
+              '-C',
+              '-OCRAM',
+              '-@{0}'.format(quotes(threads)),
+              '-T{0}'.format(quote(reference_file)),
+              '-o{0}'.format(quote(temp_file)),
+              quotes(bam_file)
+             ]                                                                  # convert bam to cram using pysam view
+    subprocess.check_call(view_cmd)
     _check_cram_file(cram_path=temp_file)                                       # check cram output
-    move_file(source_path=temp_file,
-              destinationa_path=cram_path,
-              force=force)                                                      # move cram file to original path
+    copy_local_file(\
+      source_path=temp_file,
+      destinationa_path=cram_path,
+      force=force)                                                              # move cram file to original path
     remove_dir(dir_path=os.path.dirname(temp_file))                             # remove temp directory
   except:
     raise
 
 
-def run_bam_flagstat(bam_file,output_dir,threads=1,force=False):
+def run_bam_flagstat(samtools_exe,bam_file,output_dir,threads=1,force=False):
   '''
   A method for generating bam flagstat output
   
+  :param samtools_exe: samtools executable path
   :param bam_file: A bam filepath with / without index. Index file will be created if its missing
   :param output_dir: Bam flagstat output directory path
   :param threads: Number of threads to use for conversion, default 1
@@ -109,8 +114,10 @@ def run_bam_flagstat(bam_file,output_dir,threads=1,force=False):
   :returns: Output file path
   '''
   try:
+    check_file_path(samtools_exe)
     _check_bam_file(bam_file=bam_file)                                          # check bam file
-    _check_bam_index(bam_file=bam_file)                                         # generate bam index
+    _check_bam_index(samtools_exe=samtools_exe,
+                     bam_file=bam_file)                                         # generate bam index
     output_path='{0}.{1}.{2}'.\
                 format(os.path.basename(bam_file),
                        'flagstat',
@@ -124,27 +131,35 @@ def run_bam_flagstat(bam_file,output_dir,threads=1,force=False):
       raise ValueError('Output file {0} already present, use force to overwrite'.\
                        format(output_path))
 
+    flagstat_cmd=[quotes(samtools_exe),
+                  'flagstat',
+                  '-@{0}'.format(quotes(threads)),
+                  quotes(bam_file)
+                 ]
     with open(output_path,'w') as fp:
-      fp.write(pysam.flagstat(quote(bam_file),
-                              '-@{0}'.format(threads)))                         # write bam flagstat output
+      with subprocess.Popen(flagstat_cmd, stdout=PIPE) as proc:
+        fp.write(proc.stdout.read())                                        # write bam flagstat output
 
     return output_path
   except:
     raise
 
 
-def run_bam_idxstat(bam_file,output_dir,force=False):
+def run_bam_idxstat(samtools_exe,bam_file,output_dir,force=False):
   '''
   A function for running samtools index stats generation
   
+  :param samtools_exe: samtools executable path
   :param bam_file: A bam filepath with / without index. Index file will be created if its missing
   :param output_dir: Bam idxstats output directory path
   :param force: Output idxstats file will be overwritten if force is True, default False
   :returns: Output file path
   '''
   try:
+    check_file_path(samtools_exe)
     _check_bam_file(bam_file=bam_file)                                          # check bam file
-    _check_bam_index(bam_file=bam_file)                                         # generate bam index
+    _check_bam_index(samtools_exe=samtools_exe,
+                     bam_file=bam_file)                                         # generate bam index
     output_path='{0}.{1}.{2}'.\
                 format(os.path.basename(bam_file),
                        'idxstats',
@@ -158,8 +173,13 @@ def run_bam_idxstat(bam_file,output_dir,force=False):
       raise ValueError('Output file {0} already present, use force to overwrite'.\
                        format(output_path))
 
+    idxstat_cmd=[quotes(samtools_exe),
+                  'idxstats',
+                  quotes(bam_file)
+                ]
     with open(output_path,'w') as fp:
-      fp.write(pysam.idxstats(quote(bam_file)))                                 # write bam flagstat output
+      with subprocess.Popen(idxstat_cmd, stdout=PIPE) as proc:
+        fp.write(proc.stdout.read())                                            # write bam flagstat output
 
     return output_path
   except:
