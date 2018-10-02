7b7fb29d24408fea342adcfbca11d035904647f6
Avik Datta <reach4avik@yahoo.com>
2018-10-02 16:37:50 +0100

    added method for running gatk bqsr

---
+++ igf_data/utils/tools/gatk_utils.py
@@ -0,0 +1,73 @@
+import os,subprocess
+from shlex import quote
+from igf_data.utils.fileutils import check_file_path,get_temp_dir,remove_dir,copy_local_file
+
+class GATK_tools:
+  '''
+  A python class for running gatk tools
+  
+  :param gatk_exe: Gatk exe path
+  :param java_param: Java parameter, default '-XX:ParallelGCThreads=1 -Xmx4g'
+  :param ref_fasta: Input reference fasta filepath
+  '''
+  def __init(self,gatk_exe,ref_fasta,java_param='-XX:ParallelGCThreads=1 -Xmx4g'):
+    self.gatk_exe=gatk_exe
+    self.java_param=java_param
+    self.ref_fasta=ref_fasta
+    self.suported_commands=suported_commands
+
+  def _run_gatk_checks(self):
+    '''
+    An internal method for running checks before GATK run
+    '''
+    try:
+      check_file_path(self.gatk_exe)
+      check_file_path(self.ref_fasta)
+    except:
+      raise
+
+  def run_BaseRecalibrator(self,input_bam,output_table,known_snp_sites=None,
+                           known_indel_sites=None,force=False,dry_run=False):
+    '''
+    A method for running GATK BaseRecalibrator
+    
+    :param input_bam: An input bam file
+    :param output_table: An output table filepath for recalibration results
+    :param known_snp_sites: Known snp sites (e.g. dbSNP vcf file), default None
+    :param known_indel_sites: Known indel sites (e.g.Mills_and_1000G_gold_standard indels vcf), default None
+    :param force: Overwrite output file, if force is True
+    :param dry_run: Return GATK command, if its true, default False
+    '''
+    try:
+      self._run_gatk_checks()                                                   # run initial checks
+      check_file_path(input_bam)
+      temp_dir=get_temp_dir()                                                   # get temp dir
+      temp_output=os.path.join(temp_dir,
+                               os.path.basename(output_table))
+      gatk_cmd=[quotes(self.gatk_exe),
+                'BaseRecalibrator',
+                '-I',quotes(input_bam),
+                '-O',quotes(temp_output),
+                '--reference',quotes(self.ref_fasta),
+                '--java-options',quotes(self.java_param)
+               ]
+      if known_snp_sites is not None:
+        check_file_path(known_snp_sites)
+        gatk_cmd.append('--known-sites',
+                        quotes(known_snp_sites))
+
+      if known_indel_sites:
+        check_file_path(known_indel_sites)
+        gatk_cmd.append('--known-sites',
+                        quotes(known_indel_sites))
+
+      if dry_run:
+        return gatk_cmd
+
+      subprocess.check_call(gatk_cmd)                                           # run gatk cmd
+      copy_local_file(source_path=temp_output,
+                      destinationa_path=output_table,
+                      force=force)                                              # copy output file
+      remove_dir(temp_dir)                                                      # remove temp dir
+    except:
+      raise
