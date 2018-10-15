17d25a34fc7ccc3ac1addd1c0a8abb572b7696ad
Avik Datta <reach4avik@yahoo.com>
2018-10-15 12:55:58 +0100

    added param for enabling fastp polyg trim for NextSeq and NovaSeq data

--- igf_data/utils/tools/fastp_utils.py
+++ igf_data/utils/tools/fastp_utils.py
@@ -12,17 +12,19 @@ class Fastp_utils:
   :param output_dir: A output directory path
   :param split_by_lines_count: Number of entries for splitted fastq files, default 5000000
   :param run_thread: Number of threads to use, default 1
+  :param enable_polyg_trim: Enable poly G trim for NextSeq and NovaSeq, default False
   :param fastp_options_list: A list of options for running fastp, default
                                --qualified_quality_phred 15
                                --length_required 15
   '''
-  def __init__(self,fastp_exe,input_fastq_list,output_dir,run_thread=1,
+  def __init__(self,fastp_exe,input_fastq_list,output_dir,run_thread=1,enable_polyg_trim=False,
                split_by_lines_count=5000000,fastp_options_list=['--qualified_quality_phred=15',
                                                                 '--length_required=15']):
     self.fastp_exe=fastp_exe
     self.input_fastq_list=input_fastq_list
     self.output_dir=output_dir
     self.run_thread=run_thread
+    self.enable_polyg_trim=enable_polyg_trim
     self.split_by_lines_count=split_by_lines_count
     self.fastp_options_list=fastp_options_list
 
@@ -85,6 +87,9 @@ class Fastp_utils:
                               for line in self.fastp_options_list
                                 for opt in split(line)]                         # wrap options in quotes
         cmd.extend(self.fastp_options_list)                                     # add fastp options
+
+      if self.enable_polyg_trim:
+        cmd.append('--trim_poly_g')                                             # turning on polyg trim for NextSeq and NovaSeq data
 
       if split_fastq:
         cmd.append('--split_by_lines',
