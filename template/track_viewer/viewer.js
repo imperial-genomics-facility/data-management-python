35ed9d091faf2b54bf63f464ef7c3cf802f60591
Avik Datta <reach4avik@yahoo.com>
2018-10-30 12:23:50 +0000

    updated template for genome browser config file

--- template/track_viewer/viewer.js
+++ template/track_viewer/viewer.js
@@ -1,16 +1,29 @@
 new Browser({
-    chr:          '{{ chr }}',
+    chr:          '{{ chrname }}',
     viewStart:    {{ startPosition }},
     viewEnd:      {{ endPosition }},
 
     coordSystem: {
-      speciesName: '{{ speciesName }}',
-      taxon: {{ taxonId }},
-      auth: '{{ refGenomeAuth }}',
+      speciesName: '{{ speciesCommonName }}',
+      taxon: {{ speciesTaxon }},
+      auth: '{{ refAuthority }}',
       version: '{{ refVersion }}',
-      ucscName: '{{ refUcscName }}'
+      ucscName: '{{ speciesUcscName }}'
     },
     cookieKey: true,
-    sources:     {{ trackList }}
-
-  });
+    sources:     [{name:      'Genome',
+                   twoBitURI: '{{ twoBitRefRemoteUrl }}',
+                   tier_type: 'sequence'},
+                  {name:      'Ensembl transcripts',
+                   uri:       'http://rest.ensembl.org',
+                   tier_type: 'ensembl',
+                   species:   '{{ ensemblSpecies }}',
+                   type:      ['transcript', 'exon', 'cds']},
+                  {% for row in bwdata %}
+                  {name:     '{{ row["track_name"] }}',
+                   desc:     '{{ row["track_desc"] }}',
+                   bwgURI:   '{{ row["track_path"] }}', 
+                   noDownsample: true},
+                  {% endfor %}
+                 ]
+});
