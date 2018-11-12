7178d3b7939ef1bcef701a3ad3992f2175cb05ba
Avik Datta <reach4avik@yahoo.com>
2018-11-12 11:14:41 +0000

    combine analysis chunks from previous collect step

--- ehive/runnable/process/alignment/CollectExpAnalysisChunks.py
+++ ehive/runnable/process/alignment/CollectExpAnalysisChunks.py
@@ -7,7 +7,8 @@ class CollectExpAnalysisChunks(IGFBaseProcess):
   def param_defaults(self):
     params_dict=super(CollectExpAnalysisChunks,self).param_defaults()
     params_dict.update({
-      'output_mode':'list'
+      'output_mode':'list',
+      'exp_chunk_list':list()
     })
     return params_dict
 
@@ -21,9 +22,10 @@ class CollectExpAnalysisChunks(IGFBaseProcess):
       accu_data=self.param_required('accu_data')
       output_mode=self.param_required('output_mode')
       base_work_dir=self.param_required('base_work_dir')
+      exp_chunk_list=self.param('exp_chunk_list')
       seed_date_stamp=self.param_required('date_stamp')
       seed_date_stamp=get_datestamp_label(seed_date_stamp)
-      exp_analysis_files=accu_data.get(experiment_igf_id).get(seed_date_stamp)
+      exp_analysis_files=accu_data.get(experiment_igf_id).get(seed_date_stamp)  # fetch data from accu table
       if isinstance(exp_analysis_files,list) and \
          isinstance(exp_analysis_files[0],list):
         exp_analysis_files=reduce(lambda x,y: x+y, exp_analysis_files)          # reduce list of list to a list
@@ -37,8 +39,11 @@ class CollectExpAnalysisChunks(IGFBaseProcess):
         raise ValueError('No run level file found in accu data for exp {0} and date_stamp {1}'.\
                          format(experiment_igf_id,seed_date_stamp))             # zero input file
 
+      if isinstance(exp_chunk_list,list):
+         exp_chunk_list.extend(exp_analysis_files)
+
       if output_mode=='list':
-        self.param('dataflow_params',{'exp_chunk_list':exp_analysis_files})
+        self.param('dataflow_params',{'exp_chunk_list':exp_chunk_list})
       elif output_mode=='file':
         work_dir_prefix=os.path.join(base_work_dir,
                                      project_igf_id,
@@ -47,7 +52,7 @@ class CollectExpAnalysisChunks(IGFBaseProcess):
         work_dir=self.get_job_work_dir(work_dir=work_dir_prefix)                # get a run work dir
         output_file=os.path.join(work_dir,'exp_level_chunk.txt')
         with open(output_file,'w') as fp:
-          fp.write('\n'.join(exp_analysis_files))
+          fp.write('\n'.join(exp_chunk_list))
 
         self.param('dataflow_params',{'exp_chunk_list_file':output_file})
       else:
