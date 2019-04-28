0be85cc0906fa6e435e23c21db39dfedcf130ed5
Avik Datta <reach4avik@yahoo.com>
2019-04-28 08:53:32 +0100

    updates for analysis adaptor

--- igf_data/igfdb/analysisadaptor.py
+++ igf_data/igfdb/analysisadaptor.py
@@ -11,22 +11,23 @@ class AnalysisAdaptor(BaseAdaptor):
   def store_analysis_data(self, data, autosave=True):
     '''
     A methodd for storing analysis data
-    required params:
-    data: A dictionary or a dataframe. It sould have following columns
-          project_igf_id / project_id
-          analysis_type: (RNA_DIFFERENTIAL_EXPRESSION/RNA_TIME_SERIES/CHIP_PEAK_CALL/SOMATIC_VARIANT_CALLING)
-          analysis_description 
+    :params data: A dictionary or a dataframe. It sould have following columns
+
+                  * project_igf_id / project_id
+                  * analysis_type: (RNA_DIFFERENTIAL_EXPRESSION/RNA_TIME_SERIES/CHIP_PEAK_CALL/SOMATIC_VARIANT_CALLING)
+                  * analysis_description 
     '''
     try:
       if not isinstance(data, pd.DataFrame):
         data=pd.DataFrame(data)
       
       if 'project_igf_id' in data.columns:
-        project_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
-                                                data=x, \
-                                                lookup_table=Project, \
-                                                lookup_column_name='project_igf_id', \
-                                                target_column_name='project_id')       # prepare the function for project id
+        project_map_function=\
+          lambda x: self.map_foreign_table_and_store_attribute(\
+                      data=x,
+                      lookup_table=Project,
+                      lookup_column_name='project_igf_id',
+                      target_column_name='project_id')                          # prepare the function for project id
         new_data=data.apply(project_map_function, axis=1)                              # map project id foreign key id
         data=new_data
 
@@ -39,19 +40,23 @@ class AnalysisAdaptor(BaseAdaptor):
       raise
 
 
-  def fetch_analysis_records_project_igf_id(self, project_igf_id='', output_mode='dataframe',):
+  def fetch_analysis_records_project_igf_id(self, project_igf_id='', 
+                                            output_mode='dataframe'):
     '''
     A method for fetching analysis records based on project_igf_id
-    required params:
-      project_igf_id: default: null
-      output_mode:   dataframe / object, default: dataframe
+    :params project_igf_id: default: null
+    :param output_mode:   dataframe / object, default: dataframe
     '''
-    session=self.session
-    query=session.query(Project.project_igf_id, Analysis.analysis_type, Analysis.analysis_description).join(Analysis)
-    if project_igf_id:
-      query=query.filter(Project.project_igf_id==project_igf_id)
+    try:
+      session=self.session
+      query=session.\
+            query(Project.project_igf_id,
+                  Analysis.analysis_type,
+                  Analysis.analysis_description).\
+            join(Analysis,Project.project_id==Analysis.project_id)
+      if project_igf_id:
+        query=query.filter(Project.project_igf_id==project_igf_id)
 
-    try:    
       results=self.fetch_records(query=query, output_mode=output_mode)
       return results
     except:
