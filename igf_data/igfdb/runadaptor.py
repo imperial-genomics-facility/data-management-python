bd1c90d2a5502d3c348ea83d6df46dded392f79c
Avik Datta <reach4avik@yahoo.com>
2019-04-28 10:02:22 +0100

    reformatted run adaptor class

--- igf_data/igfdb/runadaptor.py
+++ igf_data/igfdb/runadaptor.py
@@ -15,7 +15,8 @@ class RunAdaptor(BaseAdaptor):
     :param data: A list of dictionaries or a Pandas DataFrame containing the run data
     :param autosave: A toggle for saving data automatically to db, default True
     '''
-    (run_data, run_attr_data)=self.divide_data_to_table_and_attribute(data=data)
+    (run_data, run_attr_data)=\
+      self.divide_data_to_table_and_attribute(data=data)
 
     try:
       self.store_run_data(data=run_data)                                        # store run
@@ -47,11 +48,14 @@ class RunAdaptor(BaseAdaptor):
       if not isinstance(data, pd.DataFrame):
         data=pd.DataFrame(data)
 
-      run_columns=self.get_table_columns(table_name=Run,
-                                         excluded_columns=['run_id',
-                                                           'seqrun_id',
-                                                           'experiment_id'])    # get required columns for run table
-      run_columns.extend(['seqrun_igf_id', 'experiment_igf_id'])
+      run_columns=\
+        self.get_table_columns(\
+          table_name=Run,
+          excluded_columns=['run_id',
+                            'seqrun_id',
+                            'experiment_id'])                                   # get required columns for run table
+      run_columns.extend(['seqrun_igf_id',
+                          'experiment_igf_id'])
       (run_df, run_attr_df)=\
         BaseAdaptor.\
           divide_data_to_table_and_attribute(\
@@ -79,20 +83,22 @@ class RunAdaptor(BaseAdaptor):
         data=pd.DataFrame(data)
 
       if 'seqrun_igf_id' in data.columns:
-        seqrun_map_function=lambda x: self.map_foreign_table_and_store_attribute( \
-                                             data=x,
-                                             lookup_table=Seqrun,
-                                             lookup_column_name='seqrun_igf_id',
-                                             target_column_name='seqrun_id')    # prepare seqrun mapping function
+        seqrun_map_function=\
+          lambda x: self.map_foreign_table_and_store_attribute(\
+                      data=x,
+                      lookup_table=Seqrun,
+                      lookup_column_name='seqrun_igf_id',
+                      target_column_name='seqrun_id')                           # prepare seqrun mapping function
         new_data=data.apply(seqrun_map_function, axis=1)                        # map seqrun id
         data=new_data                                                           # overwrite data
 
       if 'experiment_igf_id' in data.columns:
-        exp_map_function=lambda x: self.map_foreign_table_and_store_attribute(\
-                                          data=x,
-                                          lookup_table=Experiment,
-                                          lookup_column_name='experiment_igf_id',
-                                          target_column_name='experiment_id')   # prepare experiment mapping function
+        exp_map_function=\
+          lambda x: self.map_foreign_table_and_store_attribute(\
+                      data=x,
+                      lookup_table=Experiment,
+                      lookup_column_name='experiment_igf_id',
+                      target_column_name='experiment_id')                       # prepare experiment mapping function
         new_data=data.apply(exp_map_function, axis=1)                           # map experiment id
         data=new_data                                                           # overwrite data
 
@@ -117,18 +123,20 @@ class RunAdaptor(BaseAdaptor):
         data=pd.DataFrame(data)                                                 # convert data to dataframe
 
       if 'run_igf_id' in data.columns:
-        run_map_function=lambda x: self.map_foreign_table_and_store_attribute(\
-                                               data=x,
-                                               lookup_table=Run,
-                                               lookup_column_name='run_igf_id',
-                                               target_column_name='run_id')     # prepare run mapping function
+        run_map_function=\
+          lambda x: self.map_foreign_table_and_store_attribute(\
+                      data=x,
+                      lookup_table=Run,
+                      lookup_column_name='run_igf_id',
+                      target_column_name='run_id')                              # prepare run mapping function
         new_data=data.apply(run_map_function, axis=1)
         data=new_data                                                           # overwrite data
 
-      self.store_attributes(attribute_table=Run_attribute,
-                            linked_column='run_id',
-                            db_id=run_id,
-                            data=data)                                          # store without autocommit
+      self.store_attributes(\
+        attribute_table=Run_attribute,
+        linked_column='run_id',
+        db_id=run_id,
+        data=data)                                                              # store without autocommit
       if autosave:
         self.commit_session()
     except:
@@ -149,10 +157,12 @@ class RunAdaptor(BaseAdaptor):
       run_check=False
       column=[column for column in Run.__table__.columns \
                        if column.key == target_column_name][0]
-      run_obj=self.fetch_records_by_column(table=Run, \
-                                           column_name=column, \
-                                           column_id=run_igf_id, \
-                                           output_mode='one_or_none')
+      run_obj=\
+        self.fetch_records_by_column(\
+          table=Run,
+          column_name=column,
+          column_id=run_igf_id,
+          output_mode='one_or_none')
       if run_obj is not None:
         run_check=True
       return run_check
@@ -170,10 +180,12 @@ class RunAdaptor(BaseAdaptor):
     try:
       column=[column for column in Run.__table__.columns \
                        if column.key == target_column_name][0]
-      run=self.fetch_records_by_column(table=Run, \
-                                           column_name=column, \
-                                           column_id=run_igf_id, \
-                                           output_mode='one')
+      run=\
+        self.fetch_records_by_column(\
+          table=Run,
+          column_name=column,
+          column_id=run_igf_id,
+          output_mode='one')
       return run  
     except:
       raise
@@ -187,11 +199,17 @@ class RunAdaptor(BaseAdaptor):
     '''
     try:
       session=self.session
-      query=session.query(Sample).\
-                    join(Experiment,Sample.sample_id==Experiment.sample_id).\
-                    join(Run,Experiment.experiment_id==Run.experiment_id).\
-                    filter(Run.run_igf_id==run_igf_id)
-      samples=self.fetch_records(query=query, output_mode='dataframe')          # get results
+      query=session.\
+            query(Sample).\
+            join(Experiment,
+                 Sample.sample_id==Experiment.sample_id).\
+            join(Run,
+                 Experiment.experiment_id==Run.experiment_id).\
+            filter(Run.run_igf_id==run_igf_id)
+      samples=\
+        self.fetch_records(\
+          query=query,
+          output_mode='dataframe')                                              # get results
       samples=samples.to_dict(orient='records')
       return samples[0]
     except:
@@ -204,9 +222,9 @@ class RunAdaptor(BaseAdaptor):
     
     :param run_igf_id: A run igf id string
     :returns: A list of three strings, or None if not found
-               project_igf_id
-               sample_igf_id
-               experiment_igf_id
+               * project_igf_id
+               * sample_igf_id
+               * experiment_igf_id
     '''
     try:
       project_igf_id=None
@@ -216,15 +234,20 @@ class RunAdaptor(BaseAdaptor):
             query(Project.project_igf_id,
                   Sample.sample_igf_id,
                   Experiment.experiment_igf_id).\
-            join(Sample,Project.project_id==Sample.project_id).\
-            join(Experiment,Sample.sample_id==Experiment.sample_id).\
-            join(Run,Experiment.experiment_id==Run.experiment_id).\
+            join(Sample,
+                 Project.project_id==Sample.project_id).\
+            join(Experiment,
+                Sample.sample_id==Experiment.sample_id).\
+            join(Run,
+                 Experiment.experiment_id==Run.experiment_id).\
             filter(Project.project_id==Sample.project_id).\
             filter(Sample.sample_id==Experiment.sample_id).\
             filter(Experiment.experiment_id==Run.experiment_id).\
             filter(Run.run_igf_id==run_igf_id)                                  # get query
-      data=self.fetch_records(query=query,
-                              output_mode='one_or_none')
+      data=\
+        self.fetch_records(\
+          query=query,
+          output_mode='one_or_none')
       if data is not None:
         project_igf_id=data.project_igf_id
         sample_igf_id=data.sample_igf_id
@@ -250,11 +273,13 @@ class RunAdaptor(BaseAdaptor):
                   Run.lane_number,
                   Seqrun.flowcell_id
             ).\
-            join(Seqrun,Run.seqrun_id==Seqrun.seqrun_id).\
+            join(Seqrun,
+                 Run.seqrun_id==Seqrun.seqrun_id).\
             filter(Run.run_igf_id==run_igf_id)
-      data=self.fetch_records(query=query,
-                              output_mode='one_or_none'
-                             )
+      data=\
+        self.fetch_records(\
+          query=query,
+          output_mode='one_or_none')
       if data is not None:
         flowcell_id=data.flowcell_id
         lane_number=data.lane_number
