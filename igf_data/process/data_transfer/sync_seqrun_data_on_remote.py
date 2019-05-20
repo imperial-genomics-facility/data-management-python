e07f1a01fcbf247004b9e96dbe4a26686dec3db4
Avik Datta <reach4avik@yahoo.com>
2019-05-20 14:55:46 +0100

    check for existing seqruns again and again

--- igf_data/process/data_transfer/sync_seqrun_data_on_remote.py
+++ igf_data/process/data_transfer/sync_seqrun_data_on_remote.py
@@ -3,6 +3,14 @@ from igf_data.utils.fileutils import list_remote_file_or_dirs,copy_remote_file,c
 from igf_data.process.seqrun_processing.find_and_process_new_seqrun import check_seqrun_dir_in_db
 
 class Sync_seqrun_data_from_remote:
+  '''
+  A python class for syncing sequencing runs from remote servers
+
+  :param seqrun_server: Remote server address
+  :param seqrun_path: Base dir path in remote server
+  :param database_config_file:Database config file
+  :param output_dir: Local path for file copy
+  '''
   def __init__(self,seqrun_server,seqrun_path,database_config_file,output_dir):
     self.seqrun_server = seqrun_server
     self.seqrun_path = seqrun_path
@@ -10,6 +18,9 @@ class Sync_seqrun_data_from_remote:
     self.output_dir = output_dir
 
   def run_sync(self):
+    '''
+    A method for running the sequencing run sync
+    '''
     try:
       check_file_path(self.output_dir)
       all_seqrun_dir = \
@@ -24,10 +35,20 @@ class Sync_seqrun_data_from_remote:
           all_seqrun_dir=all_seqrun_dir,
           dbconfig=self.database_config_file)                                   # filter existing seqruns
       for seqrun in new_seqrun_dirs:
-        copy_remote_file(\
-          source_path=os.path.join(self.seqrun_path,seqrun),
-          destinationa_path=self.output_dir,
-          source_address=self.seqrun_server)                                    # sync dirs
+        try:
+          new_seqruns = \
+            check_seqrun_dir_in_db(\
+              all_seqrun_dir=[seqrun],
+              dbconfig=self.database_config_file)                               # filter existing seqrun again
+          if len(new_seqruns)>0:
+            copy_remote_file(\
+              source_path=os.path.join(self.seqrun_path,seqrun),
+              destinationa_path=self.output_dir,
+              source_address=self.seqrun_server)                                # sync dirs if its still new
+
+        except Exception as e:
+          raise ValueError('Failed to sync seqrun {0}, got error {1}'.\
+                           format(seqrun,e))
 
     except Exception as e:
       raise ValueError('Stopped syncing seqrun data, got error: {0}'.\
