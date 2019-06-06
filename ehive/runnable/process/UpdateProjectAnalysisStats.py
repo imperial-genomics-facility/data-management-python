11674e518cd5df83b05b8076b994fce672d320e2
Avik Datta <reach4avik@yahoo.com>
2019-06-06 16:11:34 +0100

    removed chipseq and rnaseq plots from analysis page for this version

--- ehive/runnable/process/UpdateProjectAnalysisStats.py
+++ ehive/runnable/process/UpdateProjectAnalysisStats.py
@@ -8,35 +8,6 @@ from igf_data.utils.project_analysis_utils import Project_analysis
 class UpdateProjectAnalysisStats(IGFBaseProcess):
   '''
   An ehive runnable class for updating data for project analysis info page
-
-  :param analysis_stats_columns: A list of alignment stats to show on report, default columns are following
-
-                                  * SAMPLE_ID
-                                  * SAMTOOLS_STATS_raw_total_sequences
-                                  * SAMTOOLS_STATS_reads_mapped
-                                  * SAMTOOLS_STATS_reads_duplicated
-                                  * SAMTOOLS_STATS_reads_mapped_and_paired
-                                  * SAMTOOLS_STATS_reads_unmapped
-
-  :param rnaseq_stats_columns: A list of RNA-Seq stats to show on report, default columns are following
-
-                                  * SAMPLE_ID
-                                  * CollectRnaSeqMetrics_CODING_BASES
-                                  * CollectRnaSeqMetrics_INTERGENIC_BASES
-                                  * CollectRnaSeqMetrics_INTRONIC_BASES
-                                  * CollectRnaSeqMetrics_PF_ALIGNED_BASES
-                                  * CollectRnaSeqMetrics_PF_BASES
-                                  * CollectRnaSeqMetrics_RIBOSOMAL_BASES
-                                  * CollectRnaSeqMetrics_UTR_BASES
-
-  :param chipseq_stats_columns: A list of ChIP-Seq stats to show on report, default columns are following
-
-                                  * SAMPLE_ID
-                                  * PPQT_Normalized_SCC_NSC
-                                  * PPQT_Relative_SCC_RSC
-                                  * PPQT_corr_estFragLen
-                                  * PPQT_corr_phantomPeak
-                                  * PPQT_min_corr
   '''
   def param_defaults(self):
     params_dict=super(UpdateProjectAnalysisStats,self).param_defaults()
@@ -45,33 +16,14 @@ class UpdateProjectAnalysisStats(IGFBaseProcess):
       'remote_user':None,
       'remote_host':None,
       'analysis_data_json':'analysis_data.json',
+      'chart_data_json':'analysis_chart_data.json',
+      'chart_data_csv':'analysis_chart_data.csv',
       'sample_igf_id':None,
       'attribute_collection_file_type':'ANALYSIS_CRAM',
       'pipeline_seed_table':'experiment',
       'pipeline_finished_status':'FINISHED',
       'sample_id_label':'SAMPLE_ID',
       'remote_analysis_dir':'analysis',
-      'analysis_stats_columns':('SAMPLE_ID',
-                                'SAMTOOLS_STATS_raw_total_sequences',
-                                'SAMTOOLS_STATS_reads_mapped',
-                                'SAMTOOLS_STATS_reads_duplicated',
-                                'SAMTOOLS_STATS_reads_mapped_and_paired',
-                                'SAMTOOLS_STATS_reads_unmapped',
-                                'SAMTOOLS_STATS_non-primary_alignments'),
-      'rnaseq_stats_columns':('SAMPLE_ID',
-                              'CollectRnaSeqMetrics_CODING_BASES',
-                              'CollectRnaSeqMetrics_INTERGENIC_BASES',
-                              'CollectRnaSeqMetrics_INTRONIC_BASES',
-                              'CollectRnaSeqMetrics_PF_ALIGNED_BASES',
-                              'CollectRnaSeqMetrics_PF_BASES',
-                              'CollectRnaSeqMetrics_RIBOSOMAL_BASES',
-                              'CollectRnaSeqMetrics_UTR_BASES'),
-      'chipseq_stats_columns':('SAMPLE_ID',
-                               'PPQT_Normalized_SCC_NSC',
-                               'PPQT_Relative_SCC_RSC',
-                               'PPQT_corr_estFragLen',
-                               'PPQT_corr_phantomPeak',
-                               'PPQT_min_corr')
     })
     return params_dict
 
@@ -90,13 +42,14 @@ class UpdateProjectAnalysisStats(IGFBaseProcess):
       attribute_collection_file_type = self.param('attribute_collection_file_type')
       pipeline_seed_table = self.param('pipeline_seed_table')
       pipeline_finished_status = self.param('pipeline_finished_status')
-      analysis_stats_columns = self.param('analysis_stats_columns')
-      rnaseq_stats_columns = self.param('rnaseq_stats_columns')
-      chipseq_stats_columns = self.param('chipseq_stats_columns')
+      chart_data_json = self.param('chart_data_json')
+      chart_data_csv = self.param('chart_data_csv')
       sample_id_label = self.param('sample_id_label')
 
       temp_dir = get_temp_dir(use_ephemeral_space=False)
       output_file = os.path.join(temp_dir,analysis_data_json)
+      chart_json_output_file = os.path.join(temp_dir,chart_data_json)
+      csv_output_file = os.path.join(temp_dir,chart_data_csv)
       prj_data = \
         Project_analysis(\
           igf_session_class=igf_session_class,
@@ -108,17 +61,50 @@ class UpdateProjectAnalysisStats(IGFBaseProcess):
           pipeline_finished_status=pipeline_finished_status,
           sample_id_label=sample_id_label)
       prj_data.\
-      get_analysis_data_for_project(project_igf_id=project_igf_id,
-                                    output_file=output_file)
-      remote_file_path=os.path.join(remote_project_path,
-                                    project_igf_id,
-                                    analysis_data_json)
+        get_analysis_data_for_project(\
+          project_igf_id=project_igf_id,
+          output_file=output_file,
+          chart_json_output_file=chart_json_output_file,
+          csv_output_file=csv_output_file)
+      remote_file_path = \
+        os.path.join(\
+          remote_project_path,
+          project_igf_id,
+          analysis_data_json)
       os.chmod(output_file,
                mode=0o754)                                                      # changed file permission before copy
-      self._check_and_copy_remote_file(remote_user=remote_user,
-                                       remote_host=remote_host,
-                                       source_file=output_file,
-                                       remote_file=remote_file_path)
+      remote_config = \
+        '{0}@{1}'.format(remote_user,remote_host)
+      copy_remote_file(\
+        source_path=output_file,
+        destinationa_path=remote_file_path,
+        destination_address=remote_config)
+      remote_chart_file_path = \
+        os.path.join(\
+          remote_project_path,
+          project_igf_id,
+          chart_data_json)
+      os.chmod(chart_json_output_file,
+               mode=0o754)
+      copy_remote_file(\
+        source_path=chart_json_output_file,
+        destinationa_path=remote_chart_file_path,
+        destination_address=remote_config)
+      remote_csv_file_path = \
+        os.path.join(\
+          remote_project_path,
+          project_igf_id,
+          chart_data_csv)
+      os.chmod(csv_output_file,
+               mode=0o754)
+      copy_remote_file(\
+        source_path=csv_output_file,
+        destinationa_path=remote_csv_file_path,
+        destination_address=remote_config)
+      #self._check_and_copy_remote_file(remote_user=remote_user,
+      #                                 remote_host=remote_host,
+      #                                 source_file=output_file,
+      #                                 remote_file=remote_file_path)
       self.param('dataflow_params',{'remote_file_path':remote_file_path})
     except Exception as e:
       message='project: {2}, sample:{3}, Error in {0}: {1}'.format(self.__class__.__name__, \
