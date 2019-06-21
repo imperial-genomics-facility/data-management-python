48656813b0f3d76e5eea13607af7f6d906634156
Avik Datta <reach4avik@yahoo.com>
2019-06-21 13:34:26 +0100

    updated fastp wrapper and runnable with auto adapter trim param

--- ehive/runnable/process/alignment/RunFastp.py
+++ ehive/runnable/process/alignment/RunFastp.py
@@ -7,7 +7,8 @@ class RunFastp(IGFBaseProcess):
   def param_defaults(self):
     params_dict=super(RunFastp,self).param_defaults()
     params_dict.update({
-        'fastp_options_list':['--qualified_quality_phred=15',
+        'fastp_options_list':['-a=auto',
+                              '--qualified_quality_phred=15',
                               '--length_required=15'],
         'split_by_lines_count':5000000,
         'run_thread':1,
@@ -30,62 +31,71 @@ class RunFastp(IGFBaseProcess):
     :param run_thread: Number of threads for fastp run, default 1
     :param split_fastq: Enable splitting fastq files, default None
     :param split_by_lines_count: Number of fastq lines to be used if split_fastq is True, default 5000000
-    :param fastp_options_list: A list of fasrp tool options, default ['--qualified_quality_phred=15','--length_required=15']
+    :param fastp_options_list: A list of fasrp tool options, default ['-a=auto','--qualified_quality_phred=15','--length_required=15']
     :param platform_name: Sequencing platform name from dataflow
     :param polyg_platform_list: A list of Illumin platforms which emit poly Gs for empty cycles, default ['NextSeq','NOVASEQ6000']
     :param enable_polyg_trim: Enable Fastp poly G trim, default False
     '''
     try:
-      project_igf_id=self.param_required('project_igf_id')
-      experiment_igf_id=self.param_required('experiment_igf_id')
-      sample_igf_id=self.param_required('sample_igf_id')
-      run_igf_id=self.param_required('run_igf_id')
-      fastp_exe=self.param_required('fastp_exe')
-      input_fastq_list=self.param_required('input_fastq_list')
-      base_work_dir=self.param_required('base_work_dir')
-      run_thread=self.param('run_thread')
-      split_fastq=self.param('split_fastq')
-      split_by_lines_count=self.param('split_by_lines_count')
-      fastp_options_list=self.param('fastp_options_list')
-      platform_name=self.param_required('platform_name')
-      polyg_platform_list=self.param('polyg_platform_list')
-      enable_polyg_trim=self.param('enable_polyg_trim')
-      seed_date_stamp=self.param_required('date_stamp')
-      seed_date_stamp=get_datestamp_label(seed_date_stamp)
-      work_dir_prefix=os.path.join(base_work_dir,
-                                   project_igf_id,
-                                   sample_igf_id,
-                                   experiment_igf_id)
-      work_dir=self.get_job_work_dir(work_dir=work_dir_prefix)                  # get a run work dir
-      split_fastq=False if split_fastq is None else True                        # set default value for split fastq
+      project_igf_id = self.param_required('project_igf_id')
+      experiment_igf_id = self.param_required('experiment_igf_id')
+      sample_igf_id = self.param_required('sample_igf_id')
+      run_igf_id = self.param_required('run_igf_id')
+      fastp_exe = self.param_required('fastp_exe')
+      input_fastq_list = self.param_required('input_fastq_list')
+      base_work_dir = self.param_required('base_work_dir')
+      run_thread = self.param('run_thread')
+      split_fastq = self.param('split_fastq')
+      split_by_lines_count = self.param('split_by_lines_count')
+      fastp_options_list = self.param('fastp_options_list')
+      platform_name = self.param_required('platform_name')
+      polyg_platform_list = self.param('polyg_platform_list')
+      enable_polyg_trim = self.param('enable_polyg_trim')
+      seed_date_stamp = self.param_required('date_stamp')
+      seed_date_stamp = get_datestamp_label(seed_date_stamp)
+      work_dir_prefix = \
+        os.path.join(\
+          base_work_dir,
+          project_igf_id,
+          sample_igf_id,
+          experiment_igf_id)
+      work_dir = self.get_job_work_dir(work_dir=work_dir_prefix)                # get a run work dir
+      split_fastq = \
+        False if split_fastq is None else True                                  # set default value for split fastq
       if platform_name in polyg_platform_list:
         enable_polyg_trim=True                                                  # enable poly G trim for new Illumin platforms
 
-      fastp_obj=Fastp_utils(fastp_exe=fastp_exe,
-                            input_fastq_list=input_fastq_list,
-                            log_output_prefix=run_igf_id,
-                            output_dir=work_dir,
-                            run_thread=run_thread,
-                            enable_polyg_trim=enable_polyg_trim,
-                            split_by_lines_count=split_by_lines_count,
-                            fastp_options_list=fastp_options_list)              # setup fastp tool for run
-      output_read1,output_read2,output_html_file,output_json_file,fastp_cmd=\
-          fastp_obj.run_adapter_trimming(split_fastq=split_fastq)               # run fastp trimming
-      self.param('dataflow_params',{'output_read1':output_read1,
-                                    'output_read2':output_read2,
-                                    'output_html_file':output_html_file,
-                                    'output_json_file':output_json_file,
-                                    'seed_date_stamp':seed_date_stamp
-                                   })                                           # pass on fastp output list
-      message='finished fastp for {0} {1}'.\
-              format(project_igf_id,
-                     sample_igf_id)
+      fastp_obj = \
+        Fastp_utils(\
+          fastp_exe=fastp_exe,
+          input_fastq_list=input_fastq_list,
+          log_output_prefix=run_igf_id,
+          output_dir=work_dir,
+          run_thread=run_thread,
+          enable_polyg_trim=enable_polyg_trim,
+          split_by_lines_count=split_by_lines_count,
+          fastp_options_list=fastp_options_list)                                # setup fastp tool for run
+      output_read1, output_read2, output_html_file, output_json_file, _ = \
+        fastp_obj.\
+          run_adapter_trimming(split_fastq=split_fastq)                         # run fastp trimming
+      self.param('dataflow_params',
+                 {'output_read1':output_read1,
+                  'output_read2':output_read2,
+                  'output_html_file':output_html_file,
+                  'output_json_file':output_json_file,
+                  'seed_date_stamp':seed_date_stamp})                           # pass on fastp output list
+      message = 'finished fastp for {0} {1}'.\
+                format(project_igf_id,
+                       sample_igf_id)
       self.post_message_to_slack(message,reaction='pass')                       # send log to slack
     except Exception as e:
-      message='project: {2}, sample:{3}, Error in {0}: {1}'.format(self.__class__.__name__, \
-                                                      e, \
-                                                      project_igf_id,
-                                                      sample_igf_id)
+      message = \
+        'project: {2}, sample:{3}, Error in {0}: {1}'.\
+        format(\
+          self.__class__.__name__,
+          e,
+          project_igf_id,
+          sample_igf_id)
       self.warning(message)
       self.post_message_to_slack(message,reaction='fail')                       # post msg to slack for failed jobs
       raise
--- igf_data/utils/tools/fastp_utils.py
+++ igf_data/utils/tools/fastp_utils.py
@@ -15,21 +15,23 @@ class Fastp_utils:
   :param enable_polyg_trim: Enable poly G trim for NextSeq and NovaSeq, default False
   :param log_output_prefix: Output prefix for log file, default None
   :param fastp_options_list: A list of options for running fastp, default
+                               -a=auto
                                --qualified_quality_phred 15
                                --length_required=15
   '''
   def __init__(self,fastp_exe,input_fastq_list,output_dir,run_thread=1,enable_polyg_trim=False,
                split_by_lines_count=5000000,log_output_prefix=None,
-               fastp_options_list=('--qualified_quality_phred=15',
+               fastp_options_list=('-a=auto',
+                                   '--qualified_quality_phred=15',
                                    '--length_required=15')):
-    self.fastp_exe=fastp_exe
-    self.input_fastq_list=input_fastq_list
-    self.output_dir=output_dir
-    self.run_thread=run_thread
-    self.enable_polyg_trim=enable_polyg_trim
-    self.split_by_lines_count=split_by_lines_count
-    self.log_output_prefix=log_output_prefix
-    self.fastp_options_list=fastp_options_list
+    self.fastp_exe = fastp_exe
+    self.input_fastq_list = input_fastq_list
+    self.output_dir = output_dir
+    self.run_thread = run_thread
+    self.enable_polyg_trim = enable_polyg_trim
+    self.split_by_lines_count = split_by_lines_count
+    self.log_output_prefix = log_output_prefix
+    self.fastp_options_list = fastp_options_list
 
   def _run_checks(self):
     '''
@@ -41,7 +43,7 @@ class Fastp_utils:
         raise ValueError('No input fastq list found: {0}'.format(self.input_fastq_list))
 
       if  isinstance(self.run_thread, int):
-        self.run_thread=str(self.run_thread)                                    # convert run thread param to str
+        self.run_thread = str(self.run_thread)                                  # convert run thread param to str
 
       for file in self.input_fastq_list:
         check_file_path(file)
@@ -64,24 +66,27 @@ class Fastp_utils:
               and the fastp commandline
     '''
     try:
-      temp_dir=get_temp_dir(use_ephemeral_space=True)                           # create temp dir in ephemeral space
+      temp_dir = \
+        get_temp_dir(use_ephemeral_space=False)                                 # create temp dir in ephemeral space
       self._run_checks()
-      read1_list,read2_list=identify_fastq_pair(input_list=self.input_fastq_list) # fetch input files
-      log_output_prefix=self.log_output_prefix
+      read1_list, read2_list = \
+        identify_fastq_pair(input_list=self.input_fastq_list)                   # fetch input files
+      log_output_prefix = self.log_output_prefix
       if log_output_prefix is None:
-        log_output_prefix=os.path.basename(read1_list[0])
+        log_output_prefix = os.path.basename(read1_list[0])
 
-      cmd=[self.fastp_exe,
-           '--in1',quote(read1_list[0]),
-           '--out1',quote(os.path.join(temp_dir,
-                                       os.path.basename(read1_list[0]))),
-           '--html',quote(os.path.join(temp_dir,'{0}.html'.\
-                                       format(os.path.basename(read1_list[0])))),
-           '--json',quote(os.path.join(temp_dir,'{0}.fastp.json'.\
-                                       format(log_output_prefix))),
-           '--report_title',quote(log_output_prefix),
-           '--thread',quote(self.run_thread)
-          ]
+      cmd = \
+        [self.fastp_exe,
+         '--in1',quote(read1_list[0]),
+         '--out1',quote(os.path.join(temp_dir,
+                                     os.path.basename(read1_list[0]))),
+         '--html',quote(os.path.join(temp_dir,'{0}.html'.\
+                                     format(os.path.basename(read1_list[0])))),
+         '--json',quote(os.path.join(temp_dir,'{0}.fastp.json'.\
+                                     format(log_output_prefix))),
+         '--report_title',quote(log_output_prefix),
+         '--thread',quote(self.run_thread)
+        ]
       if len(read2_list) > 0:                                                   # add read 2 options
         cmd.extend(['--in2',quote(read2_list[0])])
         cmd.extend(['--out2',
@@ -90,10 +95,11 @@ class Fastp_utils:
 
       if isinstance(self.fastp_options_list,list) and \
          len(self.fastp_options_list)>0:
-        fastp_options_list=[quote(opt)
-                              for line in self.fastp_options_list
-                                for opt in split(line)]                         # wrap options in quotes
-        cmd.extend(self.fastp_options_list)                                     # add fastp options
+        fastp_options_list = \
+          [quote(opt)
+            for line in self.fastp_options_list
+              for opt in split(line)]                                           # wrap options in quotes
+        cmd.extend(fastp_options_list)                                          # add fastp options
 
       if self.enable_polyg_trim:
         cmd.append('--trim_poly_g')                                             # turning on polyg trim for NextSeq and NovaSeq data
@@ -102,32 +108,35 @@ class Fastp_utils:
         cmd.append('--split_by_lines',
                    quote(self.split_by_lines_count))
 
-      subprocess.check_call(cmd,shell=False)                                    # run fastp
-      fastq_pattern=re.compile(r'\S+\.fastq(\.gz)?$')
-      html_pattern=re.compile(r'\S+\.html$')
-      json_pattern=re.compile(r'\S+\.json$')
-      output_fastq_list=list()
-      output_html_file=''
-      output_json_file=''
+      subprocess.check_call(' '.join(cmd),shell=True)                           # run fastp
+      fastq_pattern = re.compile(r'\S+\.fastq(\.gz)?$')
+      html_pattern = re.compile(r'\S+\.html$')
+      json_pattern = re.compile(r'\S+\.json$')
+      output_fastq_list = list()
+      output_html_file = ''
+      output_json_file = ''
       for out_file in os.listdir(temp_dir):
-        output_file=os.path.join(self.output_dir,out_file)
+        output_file = os.path.join(self.output_dir,out_file)
         if re.match(fastq_pattern,out_file):
           output_fastq_list.append(output_file)
-          copy_local_file(source_path=os.path.join(temp_dir,out_file),
-                          destinationa_path=output_file,
-                          force=force_overwrite)                                # copy fastq file to output dir
+          copy_local_file(\
+            source_path=os.path.join(temp_dir,out_file),
+            destinationa_path=output_file,
+            force=force_overwrite)                                              # copy fastq file to output dir
 
         if re.match(html_pattern,out_file):
-          output_html_file=output_file
-          copy_local_file(source_path=os.path.join(temp_dir,out_file),
-                          destinationa_path=output_file,
-                          force=force_overwrite)                                # copy html file to output dir
+          output_html_file = output_file
+          copy_local_file(\
+            source_path=os.path.join(temp_dir,out_file),
+            destinationa_path=output_file,
+            force=force_overwrite)                                              # copy html file to output dir
 
         if re.match(json_pattern,out_file):
-          output_json_file=output_file
-          copy_local_file(source_path=os.path.join(temp_dir,out_file),
-                          destinationa_path=output_file,
-                          force=force_overwrite)                                # copy json file to output dir
+          output_json_file = output_file
+          copy_local_file(\
+            source_path=os.path.join(temp_dir,out_file),
+            destinationa_path=output_file,
+            force=force_overwrite)                                              # copy json file to output dir
 
       if len(output_fastq_list) == 0:
         raise ValueError('No output fastq files found as fastp output')
@@ -137,11 +146,12 @@ class Fastp_utils:
         raise ValueError('No fastp html or json report found')
 
       remove_dir(temp_dir)                                                      # clean up temp dir
-      output_read1,output_read2=\
-          identify_fastq_pair(input_list=output_fastq_list,
-                              sort_output=True,
-                              check_count=True)                                 #identify fastq pairs and validate output fastq pairs
-      return output_read1,output_read2,output_html_file,output_json_file,cmd
+      output_read1, output_read2 = \
+          identify_fastq_pair(\
+            input_list=output_fastq_list,
+            sort_output=True,
+            check_count=True)                                                   #identify fastq pairs and validate output fastq pairs
+      return output_read1, output_read2, output_html_file, output_json_file, cmd
     except:
       if os.path.exists(temp_dir):
         remove_dir(temp_dir)                                                    # remove temp dir
