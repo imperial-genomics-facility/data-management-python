de343710f0813dc7e7087a2a91401ed8da2767c6
Avik Datta <reach4avik@yahoo.com>
2019-08-22 11:13:34 +0100

    updated unittest for 10x na barcodes

--- test/process/processsinglecellsamplesheet_test.py
+++ test/process/processsinglecellsamplesheet_test.py
@@ -1,4 +1,5 @@
 import unittest, os
+import pandas as pd
 from igf_data.illumina.samplesheet import SampleSheet
 from igf_data.process.singlecell_seqrun.processsinglecellsamplesheet import ProcessSingleCellSamplesheet
 
@@ -42,5 +43,35 @@ class ProcessSingleCellSamplesheet_testA(unittest.TestCase):
                                    method='exclude')
     self.assertEqual(len(samplesheet._data),5)
     self.assertTrue('Original_Sample_ID' in samplesheet._data_header)
+
+class ProcessSingleCellSamplesheet_testB(unittest.TestCase):
+  def setUp(self):
+    self.samplesheet_file='data/singlecell_data/SampleSheet_10x_NA.csv'
+    self.singlecell_barcode_json='data/singlecell_data/chromium-shared-sample-indexes-plate_20180301.json'
+    self.output_file='data/singlecell_data/mod_SampleSheet.csv'
+    if os.path.exists(self.output_file):
+      os.remove(self.output_file)
+
+  def tearDown(self):
+    if os.path.exists(self.output_file):
+      os.remove(self.output_file)
+
+  def test_change_singlecell_barcodes3(self):
+    sc_data = \
+      ProcessSingleCellSamplesheet(\
+        samplesheet_file=self.samplesheet_file,
+        singlecell_barcode_json=self.singlecell_barcode_json)
+    sc_data.\
+      change_singlecell_barcodes(output_samplesheet=self.output_file)
+    sc_samplesheet = \
+      SampleSheet(infile=self.output_file)
+    sc_samplesheet.\
+      filter_sample_data(\
+        condition_key='Sample_Project',
+        condition_value='project_3',
+        method='include')
+    sc_index = sc_samplesheet._data[0]['index']
+    self.assertTrue(sc_index in ("AAACGGCG","CCTACCAT","GGCGTTTC","TTGTAAGA"))
+
 if __name__=='__main__':
   unittest.main()
