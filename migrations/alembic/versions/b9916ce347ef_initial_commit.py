cc9f75b0e6b2eb1ae40604163edefd276e5eabd7
Avik Datta <reach4avik@yahoo.com>
2019-10-04 15:48:28 +0100

    test with db migration

---
+++ migrations/alembic/versions/b9916ce347ef_initial_commit.py
@@ -0,0 +1,24 @@
+"""initial commit
+
+Revision ID: b9916ce347ef
+Revises: 
+Create Date: 2019-10-04 13:52:04.481894
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9916ce347ef'
+down_revision = None
+branch_labels = ('metadata_201908',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
