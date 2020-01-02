838fc8928519e4cd3d37d2c4a1ec19f1d6908db9
Avik Datta <reach4avik@yahoo.com>
2020-01-02 15:14:36 +0000

    update remote file copy script

--- scripts/file_copy/copy_remote_file.py
+++ scripts/file_copy/copy_remote_file.py
@@ -8,12 +8,14 @@ parser.add_argument('-i','--source_path',  required=True, help='Source file path
 parser.add_argument('-o','--dest_path', required=True, help='Destination file path')
 parser.add_argument('-s','--source_address', default=None, help='Source address with user name')
 parser.add_argument('-d','--dest_address', default=None, help='Destination address with user name')
+parser.add_argument('-f','--force_update', default=False, action='store_true', help='Force update existing file')
 
 args = parser.parse_args()
 source_path = args.source_path
 dest_path = args.dest_path
 source_address = args.source_address
 dest_address = args.dest_address
+force_update = args.force_update
 
 if __name__=='__main__':
   try:
@@ -24,6 +26,6 @@ if __name__=='__main__':
       destination_address=dest_address,
       copy_method='rsync',
       check_file=True,
-      force_update=False)
+      force_update=force_update)
   except Exception as e:
     raise ValueError("Failed to copy remote file, error:{0}".format(e))
