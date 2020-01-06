e406a6259280a5de7e945e8bf129ab0b888d4185
Avik Datta <reach4avik@yahoo.com>
2020-01-06 14:27:46 +0000

    copying image to tmpdir before run

--- igf_data/utils/singularity_run_wrapper.py
+++ igf_data/utils/singularity_run_wrapper.py
@@ -1,5 +1,6 @@
+import os
 from spython.main import Client
-from igf_data.utils.fileutils import check_file_path
+from igf_data.utils.fileutils import check_file_path,copy_local_file,get_temp_dir
 
 def singularity_run(image_path,path_bind,args_list,return_results=True):
   '''
@@ -13,12 +14,20 @@ def singularity_run(image_path,path_bind,args_list,return_results=True):
   try:
     check_file_path(image_path)
     check_file_path(path_bind)
+    temp_dir = get_temp_dir()
+    temp_image_path = \
+      os.path.join(
+        temp_dir,
+        os.path.basename(image_path))
+    copy_local_file(
+      image_path,
+      temp_image_path )                                                         # copy image to tmp dir
     if not isinstance(args_list,list) and \
        len(args_list) > 0:
        raise ValueError('No args provided for singularity run')                 # safemode
     args = ' '.join(args_list)                                                  # flatten args
     Client.run(
-      image=image_path,
+      image=temp_image_path,
       bind='{0}:/tmp'.format(path_bind),
       args=args,
       return_result=return_results)
