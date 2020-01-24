cccd8d6afc9994ec126870c5941868015e8b8ed9
Avik Datta <reach4avik@yahoo.com>
2020-01-24 16:04:31 +0000

    fix for dataflow param checking

--- ehive/runnable/IGFBaseProcess.py
+++ ehive/runnable/IGFBaseProcess.py
@@ -12,5 +12,5 @@ class IGFBaseProcess(IGFBaseRunnable):
     if self.param_is_defined('dataflow_params'):
       dataflow_params = self.param('dataflow_params')
       if not isinstance(dataflow_params, dict):
-        raise ValueError('expected a dictionary as the dataflow_param and got {0}'.format(type(dict)))
+        raise ValueError('expected a dictionary as the dataflow_param and got {0}'.format(type(dataflow_params)))
       self.dataflow(dataflow_params, 1)
