216948706ba953df283b6e923a86fea27d73881d
Avik Datta <reach4avik@yahoo.com>
2020-01-30 15:27:14 +0000

    formated docs for singularity script

--- scripts/singularity/run_singularity_container.py
+++ scripts/singularity/run_singularity_container.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
-################################################################################
-# A script for running commands within a singularity container in HPC
-#
-# USAGE:
-#   python run_singularity_container.py
-#     -i /path/SINGULARITY_IMAGE
-#     -b /path/CONTAINER_MOUNTED_DIR
-#     -a command
-#     -a arg1
-#     -a arg2
-#
-# It will run the following singularity run command:
-#   singularity run
-#     --bind /path/CONTAINER_MOUNTED_DIR:/tmp
-#     /path/SINGULARITY_IMAGE
-#       command arg1 arg2
-#
-################################################################################
-
-import argparse
+import argparse,textwrap
 from igf_data.utils.singularity_run_wrapper import singularity_run
 
-parser = argparse.ArgumentParser()
+description = textwrap.dedent(
+"""
+ A script for running commands within a singularity container in HPC
+
+ USAGE:
+   python run_singularity_container.py
+     -i /path/SINGULARITY_IMAGE
+     -b /path/CONTAINER_MOUNTED_DIR
+     -a command
+     -a arg1
+     -a arg2
+
+ It will run the following singularity run command:
+   singularity run
+     --bind /path/CONTAINER_MOUNTED_DIR:/tmp
+     /path/SINGULARITY_IMAGE
+       command arg1 arg2
+
+""")
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description=description)
 parser.add_argument('-i','--image_path', required=True, help='Singularity image path')
 parser.add_argument('-b','--path_bind', required=True, help='Path to bind to singularity /tmp dir')
 parser.add_argument('-a','--run_args', action='append', default=[], help='List of args for singularity run')
