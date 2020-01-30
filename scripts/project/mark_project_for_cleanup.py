c5347c158176417cfeeae371d0938ebab3c5ed00
Avik Datta <reach4avik@yahoo.com>
2020-01-30 15:30:05 +0000

    docs for project clean up script

--- scripts/project/mark_project_for_cleanup.py
+++ scripts/project/mark_project_for_cleanup.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
-
-################################################################################
-#
-# A script for marking project as withdrawn in IGF database and fetching list of
-# files for clean up
-#
-# USAGE: 
-#   python mark_project_for_cleanup.py
-#     -p PROJECTNAME
-#     -d /path/DBCONF_FILE
-#     -o /path/OUTPUT_DIR
-#
-# This script creates two files for each project
-#  * A file containing all the run or experiment level files in HPC
-#  * A file containing the IRODs dir for the project
-#
-################################################################################
-import argparse
+import argparse,textwrap
 from igf_data.utils.projectutils import mark_project_and_list_files_for_cleanup
 
-parser = argparse.ArgumentParser()
+description = textwrap.dedent(
+"""
+ A script for marking project as withdrawn in IGF database and fetching list of
+ files for clean up
+
+ USAGE: 
+   python mark_project_for_cleanup.py
+     -p PROJECTNAME
+     -d /path/DBCONF_FILE
+     -o /path/OUTPUT_DIR
+
+ This script creates two files for each project
+  * A file containing all the run or experiment level files in HPC
+  * A file containing the IRODs dir for the project
+
+""")
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description=description)
 parser.add_argument('-p','--project_igf_id', required=True, help='Project igf id for cleanup')
 parser.add_argument('-d','--dbconfig_file', required=True, help='Database configuration file path')
 parser.add_argument('-o','--output_dir', required=True, help='Output dir to write files lists')
