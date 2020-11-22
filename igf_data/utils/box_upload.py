3b95f7d5c029979b1751f94763819f86fbb40039
Avik Datta <reach4avik@yahoo.com>
2020-11-22 11:20:41 +0000

    added function for box file upload

---
+++ igf_data/utils/box_upload.py
@@ -0,0 +1,60 @@
+import os, json, time
+from ftplib import FTP_TLS
+from igf_data.utils.dbutils import read_json_data
+from igf_data.utils.fileutils import check_file_path
+
+def upload_file_or_dir_to_box(
+      box_config_file,file_path,upload_dir,box_username,
+      time_wait_sec=2,box_ftp_url='ftp.box.com',box_size_limit=34359738368):
+  try:
+    check_file_path(box_config_file)
+    check_file_path(file_path)
+    box_conf = read_json_data(box_config_file)
+    ftps = FTP_TLS()
+    ftps.connect(box_ftp_url)
+    ftps.login(
+      box_username,
+      box_conf.get('box_external_pass'))
+    target_dirs = \
+      [d for d in upload_dir.split('/') if d != '']
+    target_dir = ''
+    for d in target_dirs:
+      target_dir = \
+        os.path.join(target_dir,d)
+      if d not in ftps.nlst(target_dir):
+        ftps.mkd(os.path.dirname(target_dir))
+    if os.path.isdir(file_path):
+      for root,_,files in os.walk(file_path):
+        for file in files:
+          source_path = os.path.join(root,file)
+          file_size = os.stat(source_path).st_size
+          rel_path = os.path.relpath(file_path,source_path)
+          target_path = \
+            os.path.join(target_dir,rel_path)
+          if file_size <= box_size_limit:
+            dirs = os.path.dirname(rel_path).split('/')
+            for i in dirs:
+              if i not in ftps.nlst(target_dir):
+                ftps.mkd(os.path.join(target_dir,i))
+              target_dir = os.path.join(target_dir,i)
+            if file in ftps.nlst(target_dir):
+              ftp_file_size = \
+                ftps.size(os.path.join(target_dir,file))
+              if ftp_file_size != file_size:
+                ftps.storbinary('STOR {0}'.format(target_path),open(source_path,'rb'))
+                time.sleep(time_wait_sec)
+            else:
+              ftps.storbinary('STOR {0}'.format(target_path),open(source_path,'rb'))
+              time.sleep(time_wait_sec)
+    elif os.path.isfile(file_path):
+      target_path = \
+        os.path.join(
+          upload_dir,
+          os.path.basename(file_path))
+      ftps.storbinary('STOR {0}'.format(target_path),open(file_path,'rb'))
+      time.sleep(time_wait_sec)
+    else:
+      raise IOError('Not a file, {0}'.format(file_path))
+    ftps.close()
+  except Exception as e:
+    raise ValueError('Failed box upload, error: {0}'.format(e))
