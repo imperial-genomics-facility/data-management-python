1685e73bd5bccb8cdc94b3f98cc0edce9412f2ad
Avik Datta <avik.datta@imperial.ac.uk>
2021-02-07 19:19:38 +0000

    added alembic revision support for igfdb

---
+++ migrations/versions/bd380507518c_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: bd380507518c
+Revises: 
+Create Date: 2021-02-07 17:25:25.096632
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd380507518c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
