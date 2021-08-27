ce925298dc87bba94810610b4fca7ab5faee2773
Avik Datta <avik.datta@imperial.ac.uk>
2021-08-28 00:29:57 +0100

    added fix for cbcl files

--- igf_data/process/moveBclFilesForDemultiplexing.py
+++ igf_data/process/moveBclFilesForDemultiplexing.py
@@ -5,7 +5,9 @@ from igf_data.illumina.runinfo_xml import RunInfo_xml
 from igf_data.utils.fileutils import copy_local_file
 
 class moveBclFilesForDemultiplexing:
-  def __init__(self,input_dir,output_dir,samplesheet,run_info_xml,platform_model=None):
+  def __init__(
+    self, input_dir, output_dir, samplesheet,
+    run_info_xml, platform_model=None):
     self.input_dir = input_dir
     self.output_dir = output_dir
     self.samplesheet = samplesheet
@@ -21,15 +23,15 @@ class moveBclFilesForDemultiplexing:
       input_dir = self.input_dir
       output_dir = self.output_dir
       bcl_files_list = self._generate_platform_specific_list()
-      if len(bcl_files_list)==0:
+      if len(bcl_files_list) == 0:
         raise ValueError(
                 'No file list found for samplesheet {0}'.\
                   format(self.input_dir))
       for bcl_entity in bcl_files_list:
-        input_target = os.path.join(input_dir,bcl_entity)
+        input_target = os.path.join(input_dir, bcl_entity)
         output_target = output_target = \
-          os.path.join(output_dir,bcl_entity)
-        copy_local_file(input_target,output_target)
+          os.path.join(output_dir, bcl_entity)
+        copy_local_file(input_target, output_target)
     except Exception as e:
       raise ValueError(
               'Failed to copy bcl file, error: {0}'\
@@ -38,7 +40,7 @@ class moveBclFilesForDemultiplexing:
   def _generate_platform_specific_list(self,lane_list=()):
     '''
     An internal function for getting list of files and directories specific for each platform
-    
+
     :param lane_list: A list of lanes to check
     :returns: A list of files
     '''
@@ -48,14 +50,14 @@ class moveBclFilesForDemultiplexing:
       samplesheet_data = SampleSheet(infile=self.samplesheet)
       if platform_model is None:
         # set pattern for HiSeq platforms
-        hiseq_pattern = re.compile('^HISEQ',re.IGNORECASE)
-        nextseq_pattern = re.compile('^NEXTSEQ',re.IGNORECASE)
-        miseq_pattern = re.compile('^FASTQ Only',re.IGNORECASE)
+        hiseq_pattern = re.compile('^HISEQ', re.IGNORECASE)
+        nextseq_pattern = re.compile('^NEXTSEQ', re.IGNORECASE)
+        miseq_pattern = re.compile('^FASTQ Only', re.IGNORECASE)
         # read the samplesheet info
         platform_name = samplesheet_data.get_platform_name()
         runinfo_data = RunInfo_xml(xml_file=self.run_info_xml)
         platform_series = runinfo_data.get_platform_number()
-        if (re.search(hiseq_pattern,platform_name)):
+        if (re.search(hiseq_pattern, platform_name)):
           if platform_series.startswith('K'):
             platform_model = 'HISEQ4000'                                            # assign platform model for HISEQ4000
           else:
@@ -75,7 +77,7 @@ class moveBclFilesForDemultiplexing:
                   'Platform {0} not recognised'.\
                     format(platform_name))
       bcl_files_list = list()
-      if platform_model=='HISEQ4000':
+      if platform_model == 'HISEQ4000':
         bcl_files_list = [
           'Data/Intensities/s.locs',
           'InterOp',
@@ -92,7 +94,7 @@ class moveBclFilesForDemultiplexing:
             bcl_files_list.\
               append(
                 'Data/Intensities/BaseCalls/L00{0}'.format(lane))
-      elif platform_model=='NOVASEQ6000':
+      elif platform_model == 'NOVASEQ6000':
         bcl_files_list = [
           'Data/Intensities/s.locs',
           'InterOp',
@@ -109,13 +111,13 @@ class moveBclFilesForDemultiplexing:
             bcl_files_list.\
               append(
                 'Data/Intensities/BaseCalls/L00{0}'.format(lane))
-      elif platform_model=='NEXTSEQ':
+      elif platform_model == 'NEXTSEQ':
         bcl_files_list = [
           'Data',
           'InterOp',
           'RunInfo.xml',
           'RunParameters.xml']
-      elif platform_model=='MISEQ':
+      elif platform_model == 'MISEQ':
         bcl_files_list = [
           'Data',
           'InterOp',
@@ -143,8 +145,8 @@ class moveBclTilesForDemultiplexing(moveBclFilesForDemultiplexing):
     obj.copy_bcl_files()
 
   """
-  def __init__(self,input_dir,output_dir,samplesheet,run_info_xml,
-               platform_model=None,tiles_list=(1101,)):
+  def __init__(self, input_dir, output_dir, samplesheet, run_info_xml,
+               platform_model=None, tiles_list=(1101,)):
     self.tiles_list = list(tiles_list)
     super().__init__(
       input_dir,
@@ -160,7 +162,7 @@ class moveBclTilesForDemultiplexing(moveBclFilesForDemultiplexing):
     try:
       input_dir = self.input_dir
       output_dir = self.output_dir
-      bcl_files_list,final_tiles_list = \
+      bcl_files_list, final_tiles_list = \
       self._generate_platform_specific_list()
       if len(bcl_files_list)==0:
         raise ValueError(
@@ -168,9 +170,9 @@ class moveBclTilesForDemultiplexing(moveBclFilesForDemultiplexing):
                   format(self.input_dir))
       for bcl_entity in bcl_files_list:
         bcl_entity = bcl_entity.lstrip('/')
-        input_target = os.path.join(input_dir,bcl_entity)
+        input_target = os.path.join(input_dir, bcl_entity)
         output_target = os.path.join(output_dir, bcl_entity)
-        copy_local_file(input_target,output_target,force=False)
+        copy_local_file(input_target, output_target, force=False)
       return final_tiles_list
     except Exception as e:
       raise ValueError(
@@ -188,20 +190,33 @@ class moveBclTilesForDemultiplexing(moveBclFilesForDemultiplexing):
       final_tiles_list = list()
       for entry in bcl_files_list:
         if entry.startswith('Data/Intensities/BaseCalls/L00'):
-          lookup_path = os.path.join(self.input_dir,entry)
-          lane = os.path.basename(entry).replace('L00','')
+          lookup_path = os.path.join(self.input_dir, entry)
+          lane = os.path.basename(entry).replace('L00', '')
           for tile in self.tiles_list:
-            tile_id = 's_{0}_{1}'.format(lane,tile)
+            tile_id = 's_{0}_{1}'.format(lane, tile)
             final_tiles_list.append(tile_id)
-            for root,_,files in os.walk(lookup_path):
-              for f in files:
-                if fnmatch.fnmatch(f,'{0}*'.format(tile_id)):
-                  path = os.path.join(root,f)
-                  path = path.replace(self.input_dir,'')
-                  final_bcl_list.append(path)
+            if self.platform_model == 'HISEQ4000':
+              for root, _, files in os.walk(lookup_path):
+                for f in files:
+                  if fnmatch.fnmatch(f, '{0}*'.format(tile_id)):
+                    path = os.path.join(root, f)
+                    path = path.replace(self.input_dir, '')
+                    final_bcl_list.append(path)
+            elif self.platform_model == 'NOVASEQ6000':
+              temp_list = list()
+              for root, _, files in os.walk(lookup_path):
+                for f in files:
+                  if f.endswith('.filter'):
+                    if fnmatch.fnmatch(f, '{0}.filter'.format(tile_id)):
+                      temp_list.append(os.path.join(root, f))
+                  if f.endswith('.cbcl'):
+                    temp_list.append(os.path.join(root, f))
+              for path in temp_list:
+                path = path.replace(self.input_dir, '')
+                final_bcl_list.append(path)
         else:
           final_bcl_list.append(entry)
-      return final_bcl_list,final_tiles_list
+      return final_bcl_list, final_tiles_list
     except Exception as e:
       raise ValueError(
               'Failed to generate platform specific list got bcl tile, error: {0}'.\
