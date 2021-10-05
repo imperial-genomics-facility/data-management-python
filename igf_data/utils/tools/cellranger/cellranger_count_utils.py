96c465447a8bd7d50542b0dcde3299445ff2c48e
Avik Datta <avik.datta@imperial.ac.uk>
2021-10-05 12:15:15 +0100

    minor formatting change

--- igf_data/utils/tools/cellranger/cellranger_count_utils.py
+++ igf_data/utils/tools/cellranger/cellranger_count_utils.py
@@ -4,11 +4,24 @@ from igf_data.utils.fileutils import check_file_path,get_temp_dir,remove_dir,cop
 from igf_data.utils.analysis_fastq_fetch_utils import get_fastq_input_list
 
 def run_cellranger_multi(
-      cellranger_exe,library_csv,sample_id,output_dir,use_ephemeral_space=False,
-      job_timeout=43200,cellranger_options=('--localcores 1','--localmem 8')):
+      cellranger_exe, library_csv, sample_id, output_dir, use_ephemeral_space=False,
+      job_timeout=43200, cellranger_options=('--localcores 1','--localmem 8')):
+  """
+  A function for running Cellranger multi tool
+
+  :param cellranger_exe: Path to Cellranger exe
+  :param library_csv: Path to library.csv file
+  :param sample_id: Sample id
+  :param output_dir: Output path
+  :param use_ephemeral_space: A toggle for using EPHEMERAL temp space, default False
+  :param job_timeout: Job timeout, default 43200
+  :param cellranger_options: Cellranger multi options, default: ('--localcores 1','--localmem 8')
+  :returns: two strings, cmd and output_dir
+  """
   try:
     tmp_dir = \
-      get_temp_dir(use_ephemeral_space=use_ephemeral_space)
+      get_temp_dir(
+        use_ephemeral_space=use_ephemeral_space)
     os.chdir(tmp_dir)
     check_file_path(library_csv)
     check_file_path(cellranger_exe)
@@ -16,12 +29,12 @@ def run_cellranger_multi(
     cmd = [
       cellranger_exe,
       'multi',
-      '--id',sample_id,
-      '--csv',library_csv,
+      '--id', sample_id,
+      '--csv', library_csv,
       '--disable-ui']
-    if isinstance(cellranger_options,tuple):
+    if isinstance(cellranger_options, tuple):
       cellranger_options = list(cellranger_options)
-    if isinstance(cellranger_options,list) and \
+    if isinstance(cellranger_options, list) and \
        len(cellranger_options) > 0:
       cmd.extend(cellranger_options)
     cmd = \
@@ -42,20 +55,31 @@ def run_cellranger_multi(
         sample_id,
         'outs')
     _check_cellranger_multi_output(
-      cellranger_output,
-      library_csv)
+      cellranger_output=cellranger_output,
+      library_csv=library_csv,
+      sample_id=sample_id)
     output_dir = \
-      os.path.join(output_dir,sample_id)
+      os.path.join(
+        output_dir,
+        sample_id)
     copy_local_file(
       cellranger_output,
       output_dir)
-    return cmd,output_dir
+    return cmd, output_dir
   except Exception as e:
     raise ValueError(
             'Failed cellranger run: error: {0}'.format(e))
 
 
 def _check_cellranger_multi_output(cellranger_output, library_csv, sample_id):
+  """
+  Internal function for checking cellranger v6 multi output
+
+  :param cellranger_output: Cellranger output path
+  :param library_csv: path to cellranger multi csv
+  :param sample_id: Sample id, for non cell-multiplexing samples
+  :returns: None
+  """
   try:
     #file_check_dict = {
     #  'gene_expression':[
@@ -136,12 +160,12 @@ def _check_cellranger_multi_output(cellranger_output, library_csv, sample_id):
               format(e))
 
 
-def get_cellranger_count_input_list(db_session_class,experiment_igf_id,
-                                    fastq_collection_type='demultiplexed_fastq',
-                                    active_status='ACTIVE'):
+def get_cellranger_count_input_list(
+      db_session_class, experiment_igf_id, fastq_collection_type='demultiplexed_fastq',
+      active_status='ACTIVE'):
   '''
   A function for fetching input list for cellranger count run for a specific experiment
-  
+
   :param db_session_class: A database session class
   :param experiment_igf_id: An experiment igf id
   :param fastq_collection_type: Fastq collection type name, default demultiplexed_fastq
@@ -150,82 +174,84 @@ def get_cellranger_count_input_list(db_session_class,experiment_igf_id,
   :raises ValueError: It raises ValueError if no fastq directory found
   '''
   try:
-    fastq_dir_list=get_fastq_input_list(db_session_class=db_session_class,
-                                        experiment_igf_id=experiment_igf_id,
-                                        combine_fastq_dir=True,
-                                        fastq_collection_type='demultiplexed_fastq',
-                                        active_status='ACTIVE')                 # get fastq dir list
+    fastq_dir_list=get_fastq_input_list(
+      db_session_class=db_session_class,
+      experiment_igf_id=experiment_igf_id,
+      combine_fastq_dir=True,
+      fastq_collection_type='demultiplexed_fastq',
+      active_status='ACTIVE')                                                   # get fastq dir list
     return fastq_dir_list                                                       # return list of fastq dirs
-  except:
-    raise
+  except Exception as e:
+    raise ValueError(
+            'Failed to get cellranger count output list, error: {0}'.format(e))
 
 
-def check_cellranger_count_output(output_path,
-                                  file_list=('web_summary.html',
-                                             'metrics_summary.csv',
-                                             'possorted_genome_bam.bam',
-                                             'possorted_genome_bam.bam.bai',
-                                             'filtered_feature_bc_matrix.h5',
-                                             'raw_feature_bc_matrix.h5',
-                                             'molecule_info.h5',
-                                             'cloupe.cloupe',
-                                             'analysis/tsne/2_components/projection.csv',
-                                             'analysis/clustering/graphclust/clusters.csv',
-                                             'analysis/diffexp/kmeans_3_clusters/differential_expression.csv',
-                                             'analysis/pca/10_components/variance.csv'
-                                            )):
+def check_cellranger_count_output(
+      output_path,
+      file_list=(
+        'web_summary.html',
+        'metrics_summary.csv',
+        'possorted_genome_bam.bam',
+        'possorted_genome_bam.bam.bai',
+        'filtered_feature_bc_matrix.h5',
+        'raw_feature_bc_matrix.h5',
+        'molecule_info.h5',
+        'cloupe.cloupe',
+        'analysis/tsne/2_components/projection.csv',
+        'analysis/clustering/graphclust/clusters.csv',
+        'analysis/diffexp/kmeans_3_clusters/differential_expression.csv',
+        'analysis/pca/10_components/variance.csv')):
   '''
   A function for checking cellranger count output
-  
+
   :param output_path: A filepath for cellranger count output directory
   :param file_list: List of files to check in the output directory
-    
-                      default file list to check
-                        web_summary.html
-                        metrics_summary.csv
-                        possorted_genome_bam.bam
-                        possorted_genome_bam.bam.bai
-                        filtered_feature_bc_matrix.h5
-                        raw_feature_bc_matrix.h5
-                        molecule_info.h5
-                        cloupe.cloupe
-                        analysis/tsne/2_components/projection.csv
-                        analysis/clustering/graphclust/clusters.csv
-                        analysis/diffexp/kmeans_3_clusters/differential_expression.csv
-                        analysis/pca/10_components/variance.csv
-  :returns: Nill
+
+    default file list to check
+      * web_summary.html
+      * metrics_summary.csv
+      * possorted_genome_bam.bam
+      * possorted_genome_bam.bam.bai
+      * filtered_feature_bc_matrix.h5
+      * raw_feature_bc_matrix.h5
+      * molecule_info.h5
+      * cloupe.cloupe
+      * analysis/tsne/2_components/projection.csv
+      * analysis/clustering/graphclust/clusters.csv
+      * analysis/diffexp/kmeans_3_clusters/differential_expression.csv
+      * analysis/pca/10_components/variance.csv
+  :returns: None
   :raises IOError: when any file is missing from the output path
   '''
   try:
     file_list = list(file_list)
-    file_dict_value=[i for i in range(len(file_list))]                          # get file index values
-    file_dict=dict(zip(file_list,file_dict_value))                              # convert file list to a dictionary
+    file_dict_value = [
+      i for i in range(len(file_list))]                                         # get file index values
+    file_dict = dict(zip(file_list, file_dict_value))                           # convert file list to a dictionary
     for file_name in file_list:
-      file_path=os.path.join(output_path,
-                             file_name)
+      file_path = \
+        os.path.join(
+          output_path,
+          file_name)
       if os.path.exists(file_path) and \
          os.path.getsize(file_path) > 0:
         del file_dict[file_name]                                                # remove file from dictionary if its present
-
     if len(list(file_dict.keys())) > 0:
-      raise IOError('Partial Cellranger count output found, missing following files: {0}'.\
-                    format(list(file_dict.keys())))
+      raise IOError(
+              'Partial Cellranger count output found, missing following files: {0}'.\
+                format(list(file_dict.keys())))
+  except Exception as e:
+    raise ValueError('Failed to check cellranger output, error: {0}'.format(e))
 
-  except:
-    raise
 
-
-def extract_cellranger_count_metrics_summary(cellranger_tar,
-                                             collection_name=None,
-                                             collection_type=None,
-                                             attribute_name='attribute_name',
-                                             attribute_value='attribute_value',
-                                             attribute_prefix='None',
-                                             target_filename='metrics_summary.csv'):
+def extract_cellranger_count_metrics_summary(
+      cellranger_tar, collection_name=None, collection_type=None,
+      attribute_name='attribute_name', attribute_value='attribute_value',
+      attribute_prefix='None', target_filename='metrics_summary.csv'):
   '''
   A function for extracting metrics summary file for cellranger ourput tar and parse the file.
   Optionally it can add the collection name and type info to the output dictionary.
-  
+
   :param cellranger_tar: A cellranger output tar file
   :param target_filename: A filename for metrics summary file lookup, default metrics_summary.csv
   :param collection_name: Optional collection name, default None
@@ -235,44 +261,46 @@ def extract_cellranger_count_metrics_summary(cellranger_tar,
   '''
   try:
     check_file_path(cellranger_tar)
-    temp_work_dir = get_temp_dir(use_ephemeral_space=False)
+    temp_work_dir = \
+      get_temp_dir(use_ephemeral_space=False)
     metrics_file = None
-    with tarfile.open(cellranger_tar,mode='r') as tar:
+    with tarfile.open(cellranger_tar, mode='r') as tar:
       for file_name in tar.getnames():
         if os.path.basename(file_name) == target_filename:
-            tar.extract(file_name,path=temp_work_dir)
-            metrics_file = os.path.join(temp_work_dir,
-                                        file_name)
-
+            tar.extract(file_name, path=temp_work_dir)
+            metrics_file = \
+              os.path.join(
+                temp_work_dir,
+                file_name)
     if metrics_file is None:
-      raise IOError('Required file {0} not found in tar {1}'.\
-                    format(target_filename,cellranger_tar))
-
-    attribute_data = pd.read_csv(metrics_file).T.\
-                     reset_index()
-    attribute_data.columns = [attribute_name,attribute_value]
+      raise IOError(
+              'Required file {0} not found in tar {1}'.\
+                format(target_filename,cellranger_tar))
+    attribute_data = \
+      pd.read_csv(metrics_file).\
+        T.reset_index()
+    attribute_data.columns = [
+      attribute_name, attribute_value]
     if attribute_prefix is None:
       attribute_data[attribute_name] = \
         attribute_data[attribute_name].\
-          map(lambda x: x.replace(' ','_'))
+          map(lambda x: x.replace(' ', '_'))
     else:
       attribute_data[attribute_name] = \
         attribute_data[attribute_name].\
           map(lambda x: \
               '{0}_{1}'.format(\
                 attribute_prefix,
-                x.replace(' ','_')))
-
+                x.replace(' ', '_')))
     if collection_name is not None:
       attribute_data['name'] = collection_name
     if collection_type is not None:
       attribute_data['type'] = collection_type
-
-    attribute_data = attribute_data.\
-                     to_dict(orient='records')
+    attribute_data = \
+      attribute_data.\
+        to_dict(orient='records')
     remove_dir(temp_work_dir)
     return attribute_data
-  except:
-    raise
-
-
+  except Exception as e:
+    raise ValueError(
+            'Failed to extract cellranger tar, error: {0}'.format(e))
