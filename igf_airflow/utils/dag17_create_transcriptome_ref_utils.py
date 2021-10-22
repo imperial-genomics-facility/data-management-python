15e527a9276e527577fbd3ad283970b91a7f5f29
Avik Datta <avik.datta@imperial.ac.uk>
2021-10-22 20:02:25 +0100

    added refgenome building methods

--- igf_airflow/utils/dag17_create_transcriptome_ref_utils.py
+++ igf_airflow/utils/dag17_create_transcriptome_ref_utils.py
@@ -1,5 +1,4 @@
-import logging, os, requests, subprocess
-from tempfile import tempdir
+import logging, os, requests, subprocess, re, shutil
 import pandas as pd
 from airflow.models import Variable
 from igf_airflow.logging.upload_log_msg import send_log_to_channels
@@ -10,6 +9,7 @@ from igf_data.igfdb.baseadaptor import BaseAdaptor
 
 
 GENOME_FASTA_TYPE = 'GENOME_FASTA'
+GENOME_DICT_TYPE = 'GENOME_DICT'
 GENE_REFFLAT_TYPE = 'GENE_REFFLAT'
 RIBOSOMAL_INTERVAL_TYPE = 'RIBOSOMAL_INTERVAL'
 DATABASE_CONFIG_FILE = Variable.get('database_config_file', default_var=None)
@@ -17,13 +17,495 @@ SLACK_CONF = Variable.get('slack_conf', default_var=None)
 MS_TEAMS_CONF = Variable.get('ms_teams_conf', default_var=None)
 STAR_EXE = Variable.get('star_exe', default_var=None)
 STAR_REF_PATH = Variable.get('star_exe', default_var=None)
-RSEM_EXE = Variable.get('rsem_exe', default_var=None)
+RSEM_EXE_DIR = Variable.get('rsem_exe_dir', default_var=None)
 RSEM_REF_PATH = Variable.get('rsem_ref_path', default_var=None)
 BIGBED_REF_PATH = Variable.get('bigbed_ref_path', default_var=None)
 UCSC_EXE_DIR = Variable.get('ucsc_exe_dir', default_var=None)
 REFFLAT_REF_PATH = Variable.get('refflat_ref_path', default_var=None)
+RIBOSOMAL_INTERVAL_REF_PATH = Variable.get('ribosomal_interval_ref_path', default_var=None)
 CELLRANGER_EXE = Variable.get('cellranger_exe', default_var=None)
 CELLRANGER_REF_PATH = Variable.get('cellranger_ref_path', default_var=None)
+
+def filter_gtf_for_cellranger_ref(x):
+  try:
+    gene_id = None
+    biotype_pattern = \
+      "(protein_coding|lncRNA|IG_C_gene|IG_D_gene|IG_J_gene|IG_LV_gene|IG_V_gene|IG_V_pseudogene|IG_J_pseudogene|IG_C_pseudogene|TR_C_gene|TR_D_gene|TR_J_gene|TR_V_gene|TR_V_pseudogene|TR_J_pseudogene)"
+    gene_pattern =  \
+      re.compile(r'.*gene_type \"{0}\"'.format(biotype_pattern.replace('\n', '')))
+    tx_pattern = \
+      re.compile(r'.*transcript_type \"{0}\"'.format(biotype_pattern))
+    readthrough_pattern = \
+      re.compile(r".*tag \"readthrough_transcript\"")
+    par_pattern = \
+      re.compile(r".*tag \"PAR\"")
+    gene_id_pattern = \
+      re.compile(r'.*gene_id \"(\S+)\"')
+    if (re.match(gene_pattern, x) or \
+        re.match(tx_pattern, x)):
+      if re.match(gene_id_pattern, x):
+        gene_id = \
+          re.match(gene_id_pattern, x).group(1)
+    if re.match(readthrough_pattern, x) or \
+       re.match(par_pattern, x):
+        gene_id = None
+    return gene_id
+  except Exception as e:
+    raise ValueError(
+            'Failed to filter gtf for cellranger, error: {0}'.\
+              format(e))
+
+
+def extract_gene_id_from_gtf(x):
+  try:
+    gene_id_pattern = re.compile(r'.*gene_id \"(\S+)\"')
+    gene_id = None
+    if re.match(gene_id_pattern, x):
+      gene_id = re.match(gene_id_pattern, x).group(1)
+    return gene_id
+  except Exception as e:
+    raise ValueError(
+            'Failed to extract gene id, error: {0}'.\
+              format(e))
+
+
+def create_cellranger_ref_func(**context):
+  try:
+    gtf_xcom_key = \
+      context['params'].get('gtf_xcom_key')
+    gtf_xcom_task = \
+      context['params'].get('gtf_xcom_task')
+    cellranger_ref_xcom_key = \
+      context['params'].get('cellranger_ref_xcom_key')
+    skip_gtf_rows = \
+      context['params'].get('skip_gtf_rows')
+    threads = \
+      context['params'].get('threads')
+    memory = \
+      context['params'].get('memory')
+    dag_run = context.get('dag_run')
+    ti = context.get('ti')
+    gtf_file = \
+      ti.xcom_pull(
+        task_ids=gtf_xcom_task,
+        key=gtf_xcom_key)
+    check_file_path(gtf_file)
+    df = \
+      pd.read_csv(
+        gtf_file,
+        sep='\t',
+        skiprows=skip_gtf_rows,
+        header=None,
+        engine='c',
+        low_memory=True)
+    transcripts_df = \
+      df[df[2]=='transcript']
+    transcripts_df['gene_id'] = \
+      transcripts_df[8].\
+        map(lambda x: filter_gtf_for_cellranger_ref(x))
+    gene_id_list = \
+      transcripts_df['gene_id'].\
+      dropna().\
+      drop_duplicates().\
+      values.tolist()
+    df['gene_id'] = \
+      df[8].\
+      map(lambda x: extract_gene_id_from_gtf(x))
+    df = \
+      df[df['gene_id'].isin(gene_id_list)]
+    df['gene_id'].\
+      drop('gene_id', axis=1, inplace=True)
+    temp_dir = get_temp_dir()
+    filtered_gtf = \
+      os.path.join(
+        temp_dir,
+        '{0}_filtered'.format(os.path.basename(gtf_file)))
+    df.\
+      to_csv(
+        filtered_gtf,
+        sep='\t',
+        header=False,
+        index=False,
+        quotechar="'")
+    check_file_path(filtered_gtf)
+    if dag_run is not None and \
+       dag_run.conf is not None:
+      tag = dag_run.conf.get('tag')
+      species_name = dag_run.conf.get('species_name')
+      cellranger_version = dag_run.conf.get('cellranger_version')
+      if species_name is None or \
+         tag is None or \
+         cellranger_version is None:
+        raise ValueError(
+                'Missing species name, cellranger version or tag')
+      fasta_file = \
+        fetch_ref_genome_from_db(
+          dbconfig_file=DATABASE_CONFIG_FILE,
+          genome_build=species_name,
+          reference_type=GENOME_FASTA_TYPE)
+      cellranger_ref_name = \
+        '{0}_{1}_{2}'.\
+          format(
+            species_name,
+            tag,
+            cellranger_version)
+      target_cellranger_ref = \
+        os.path.join(
+          CELLRANGER_REF_PATH,
+          cellranger_ref_name)
+      if os.path.exists(target_cellranger_ref):
+        raise ValueError(
+                'Cellranger ref {0} already present'.\
+                  ormat(target_cellranger_ref))
+      cellranger_command = [
+        'cd', temp_dir,';',
+        CELLRANGER_EXE,
+        'mkref',
+        '--ref-version={0}_{1}_{2}'.\
+          format(species_name, tag, cellranger_version),
+        '--genome={0}'.format(cellranger_ref_name),
+        '--fasta={0}'.format(fasta_file),
+        '--genes={0}'.format(filtered_gtf),
+        '--disable-ui',
+        '--localcores', threads,
+        '--localmem', memory]
+      cellranger_command = \
+        ' '.join(cellranger_command)
+      subprocess.\
+        check_call(
+          cellranger_command,
+          shell=True)
+      copy_local_file(
+        os.path.join(temp_dir, cellranger_ref_name),
+        CELLRANGER_REF_PATH)
+      ti.xcom_push(
+        key=cellranger_ref_xcom_key,
+        value=target_cellranger_ref)
+    else:
+      raise ValueError('No dag_run.conf entry found')
+  except Exception as e:
+    logging.error(e)
+    message = \
+      'Cellranger ref building error: {0}'.\
+        format(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=message,
+      reaction='fail')
+    raise
+
+
+def filter_rRNA_genes_in_gtf(x):
+  try:
+    pattern = re.compile(r'.*gene_type \"rRNA\"')
+    if re.match(pattern, x):
+        return 'rRNA'
+    else:
+        return None
+  except Exception as e:
+    raise ValueError(
+            'Failed to get rRNA gene_type, error: {0}'.\
+              format(e))
+
+
+
+def extract_gene_name_from_gtf_info(x):
+  try:
+    pattern = re.compile(r'.*gene_name \"(\S+)\"')
+    gene_name = None
+    if re.match(pattern, x):
+        gene_name = re.match(pattern, x).group(1)
+    return gene_name
+  except Exception as e:
+    raise ValueError(
+            'Failed to extract gene name, error; {0}'.\
+              format(e))
+
+
+def create_ribosomal_interval_func(**context):
+  try:
+    gtf_xcom_key = \
+      context['params'].get('gtf_xcom_key')
+    gtf_xcom_task = \
+      context['params'].get('gtf_xcom_task')
+    ribosomal_ref_xcom_key = \
+      context['params'].get('ribosomal_ref_xcom_key')
+    skip_gtf_rows = \
+      context['params'].get('skip_gtf_rows')
+    dag_run = context.get('dag_run')
+    ti = context.get('ti')
+    gtf_file = \
+      ti.xcom_pull(
+        task_ids=gtf_xcom_task,
+        key=gtf_xcom_key)
+    check_file_path(gtf_file)
+    if dag_run is not None and \
+       dag_run.conf is not None:
+      tag = dag_run.conf.get('tag')
+      species_name = dag_run.conf.get('species_name')
+      if species_name is None or \
+         tag is None:
+        raise ValueError('Missing species name or tag')
+      dict_file = \
+        fetch_ref_genome_from_db(
+          dbconfig_file=DATABASE_CONFIG_FILE,
+          genome_build=species_name,
+          reference_type=GENOME_DICT_TYPE)
+      temp_dir = \
+        get_temp_dir(use_ephemeral_space=True)
+      ribosomal_ref_name = \
+        '{0}_{1}_ribosomal.interval'.\
+            format(species_name, tag)
+      temp_ribosomal_file = \
+        os.path.join(
+          temp_dir,
+          'temp_{0}'.format(ribosomal_ref_name))
+      temp_merged_file_path = \
+        os.path.join(
+          temp_dir,
+          ribosomal_ref_name)
+      target_ribisomal_file = \
+        os.path.join(
+          RIBOSOMAL_INTERVAL_REF_PATH,
+          ribosomal_ref_name)
+      if os.path.exists(target_ribisomal_file):
+        raise ValueError(
+                'Ribosomal file {0} already present, remove it before continuing'.\
+                  format(target_ribisomal_file))
+      df = \
+        pd.read_csv(
+          gtf_file,
+          sep='\t',
+          header=None,
+          skiprows=skip_gtf_rows)
+      df['rRNA'] = \
+        df[8].\
+          map(lambda x: filter_rRNA_genes_in_gtf(x))
+      df = \
+        df[df['rRNA']=='rRNA']                                                  # filter rRNA genes
+      df[8] = \
+        df[8].\
+          map(lambda x: extract_gene_name_from_gtf_info(x))                     # extract gene names
+      df.iloc[:,[0, 3, 4, 6, 8]].\
+        to_csv(
+          temp_ribosomal_file,
+          index=False,
+          sep='\t',
+          header=False)
+      with open(temp_merged_file_path ,'wb') as ofp:
+        for i in (dict_file, temp_ribosomal_file):
+          with open(i, 'rb') as fp:
+            shutil.copyfileobj(fp, ofp)
+      copy_local_file(
+        temp_merged_file_path,
+        target_ribisomal_file)
+      ti.xcom_push(
+        key=ribosomal_ref_xcom_key,
+        value=target_ribisomal_file)
+    else:
+      raise ValueError('No dag_run.conf entry found')
+  except Exception as e:
+    logging.error(e)
+    message = \
+      'Ribosomal ref building error: {0}'.\
+        format(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=message,
+      reaction='fail')
+    raise
+
+
+def create_reflat_index_func(**context):
+  try:
+    gtf_xcom_key = \
+      context['params'].get('gtf_xcom_key')
+    gtf_xcom_task = \
+      context['params'].get('gtf_xcom_task')
+    refflat_ref_xcom_key = \
+      context['params'].get('refflat_ref_xcom_key')
+    dag_run = context.get('dag_run')
+    ti = context.get('ti')
+    gtf_file = \
+      ti.xcom_pull(
+        task_ids=gtf_xcom_task,
+        key=gtf_xcom_key)
+    check_file_path(gtf_file)
+    if dag_run is not None and \
+       dag_run.conf is not None:
+      tag = dag_run.conf.get('tag')
+      species_name = dag_run.conf.get('species_name')
+      if species_name is None or \
+         tag is None:
+        raise ValueError('Missing species name or tag')
+      temp_dir = \
+        get_temp_dir(use_ephemeral_space=True)
+      refflat_ref_name = \
+        '{0}_{1}.refflat'.\
+            format(species_name, tag)
+      temp_refflat_file = \
+        os.path.join(
+          temp_dir,
+          refflat_ref_name)
+      target_refflat_file = \
+        os.path.join(
+          REFFLAT_REF_PATH,
+          refflat_ref_name)
+      if os.path.exists(target_refflat_file):
+        raise ValueError(
+                'REFFLAT file {0} already present, remove it before continuing'.\
+                  format(target_refflat_file))
+      intermediate_temp_file = \
+        '{0}_temp'.format(temp_refflat_file)
+      gtfToGenePred_exe = \
+        os.path.join(
+          UCSC_EXE_DIR,
+          'gtfToGenePred')
+      check_file_path(gtfToGenePred_exe)
+      gtf_to_genepred_cmd = [
+        gtfToGenePred_exe,
+        '-genePredExt',
+        '-geneNameAsName2',
+        gtf_file,
+        intermediate_temp_file]
+      subprocess.\
+        check_call(
+          ' '.join(gtf_to_genepred_cmd),
+          shell=True)
+      df = \
+        pd.read_csv(
+          intermediate_temp_file,
+          sep='\t',
+          header=None)
+      df.iloc[:,[11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]].\
+        to_csv(
+          temp_refflat_file,
+          index=False,
+          sep='\t',
+          header=False)
+      copy_local_file(
+        temp_refflat_file,
+        target_refflat_file)
+      ti.xcom_push(
+        key=refflat_ref_xcom_key,
+        value=target_refflat_file)
+    else:
+      raise ValueError('No dag_run.conf entry found')
+  except Exception as e:
+    logging.error(e)
+    message = \
+      'REFFLAT ref building error: {0}'.\
+        format(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=message,
+      reaction='fail')
+    raise
+
+
+def create_rsem_index_func(**context):
+  try:
+    gtf_xcom_key = \
+      context['params'].get('gtf_xcom_key')
+    gtf_xcom_task = \
+      context['params'].get('gtf_xcom_task')
+    threads = \
+      context['params'].get('threads')
+    rsem_ref_xcom_key = \
+      context['params'].get('rsem_ref_xcom_key')
+    rsem_options = \
+      context['params'].get('rsem_options')
+    dag_run = context.get('dag_run')
+    ti = context.get('ti')
+    gtf_file = \
+      ti.xcom_pull(
+        task_ids=gtf_xcom_task,
+        key=gtf_xcom_key)
+    if dag_run is not None and \
+       dag_run.conf is not None:
+      tag = dag_run.conf.get('tag')
+      species_name = dag_run.conf.get('species_name')
+      if species_name is None or \
+         tag is None:
+        raise ValueError('Missing species name or tag')
+      fasta_file = \
+        fetch_ref_genome_from_db(
+          dbconfig_file=DATABASE_CONFIG_FILE,
+          genome_build=species_name,
+          reference_type=GENOME_FASTA_TYPE)
+      temp_dir = \
+        get_temp_dir(use_ephemeral_space=True)
+      rsem_ref_name = \
+        '{0}_{1}_rsem'.\
+            format(species_name, tag)
+      temp_ref_dir = \
+        os.path.join(
+          temp_dir,
+          rsem_ref_name)
+      os.makedirs(
+        temp_ref_dir,
+        exist_ok=True)
+      target_ref_dir = \
+        os.path.join(
+          RSEM_REF_PATH,
+          rsem_ref_name)
+      if os.path.exists(target_ref_dir):
+        raise ValueError(
+                'RSEM REF {0} already present, remove it before continuing'.\
+                  format(target_ref_dir))
+      rsem_exe = \
+        os.path.join(
+          RSEM_EXE_DIR,
+          'rsem-prepare-reference')
+      check_file_path(rsem_exe)
+      rsem_cmd = [
+        rsem_exe,
+        '--num-threads', threads,
+        '--gtf', gtf_file]
+      if rsem_options is not None and \
+         isinstance(rsem_options, list) and \
+         len(rsem_options) > 0:
+        rsem_cmd.\
+          extend(rsem_options)
+      rsem_cmd.\
+        extend([
+          fasta_file,
+          os.path.join(temp_ref_dir, rsem_ref_name)])
+      subprocess.\
+        check_call(
+          ' '.join(rsem_cmd),
+          shell=True)
+      copy_local_file(
+        temp_ref_dir,
+        RSEM_REF_PATH)
+      ti.xcom_push(
+        key=rsem_ref_xcom_key,
+        value=target_ref_dir)
+    else:
+      raise ValueError('No dag_run.conf entry found')
+  except Exception as e:
+    logging.error(e)
+    message = \
+      'RSEM ref building error: {0}'.\
+        format(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=message,
+      reaction='fail')
+    raise
+
 
 def download_file(url, output_file):
   try:
@@ -65,7 +547,7 @@ def download_gtf_file_func(**context):
   except Exception as e:
     logging.error(e)
     message = \
-      'SampleSheet check error: {0}'.\
+      'GTF file download error: {0}'.\
         format(e)
     send_log_to_channels(
       slack_conf=SLACK_CONF,
@@ -131,11 +613,19 @@ def create_star_index_func(**context):
           reference_type=GENOME_FASTA_TYPE)
       temp_dir = \
         get_temp_dir(use_ephemeral_space=True)
+      star_ref_dirname = \
+        '{0}_{1}_star'.\
+            format(species_name, tag)
+      star_target_dir = \
+        os.path.join(STAR_REF_PATH, star_ref_dirname)
+      if os.path.exists(star_target_dir):
+        raise ValueError(
+                'Target STAR ref path {0} already present, remove it before continuing'.\
+                  format(star_target_dir))
       genome_dir = \
         os.path.join(
-          get_temp_dir,
-          '{0}_{1}_star'.\
-            format(species_name, tag))
+          temp_dir,
+          star_ref_dirname)
       star_cmd = [
         STAR_EXE,
         '--runThreadN', int(threads),
@@ -152,15 +642,18 @@ def create_star_index_func(**context):
         check_call(
           ' '.join(star_cmd),
           shell=True)
+      copy_local_file(
+        genome_dir,
+        STAR_REF_PATH)
       ti.xcom_push(
         key=star_ref_xcom_key,
-        value=genome_dir)
+        value=star_target_dir)
     else:
       raise ValueError('No dag_run.conf entry found')
   except Exception as e:
     logging.error(e)
     message = \
-      'SampleSheet check error: {0}'.\
+      'STAR ref building error: {0}'.\
         format(e)
     send_log_to_channels(
       slack_conf=SLACK_CONF,
