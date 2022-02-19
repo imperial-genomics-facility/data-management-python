6335fd96ff0781d91eea96108877648a1d0ac1ab
Avik Datta <avik.datta@imperial.ac.uk>
2022-02-19 22:42:21 +0000

    fixed file upload

--- igf_portal/api_utils.py
+++ igf_portal/api_utils.py
@@ -1,4 +1,4 @@
-import os, requests
+import os, requests, json
 from urllib.parse import urljoin
 from igf_data.utils.dbutils import read_json_data
 from igf_data.utils.fileutils import check_file_path
@@ -8,18 +8,25 @@ def get_request(portal_config_file, url_suffix):
 
 def post_request(url,  data, headers=None, verify=False, file_attachment=None):
   try:
-      res = \
-        requests.post(
-          url,
-          data=data,
-          headers=headers,
-          verify=verify,
-          files=file_attachment)
-      if res.status_code != 200:
-        raise ValueError(
-                "Failed post request, got status: {0}".\
-                  format(res.status_code))
-      return res
+    files = None
+    if file_attachment is not None:
+      files = {
+        'file': (
+          os.path.join(file_attachment),
+          open(file_attachment, 'rb'), 'application/json')}
+    res = \
+      requests.post(
+        url=url,
+        data=data,
+        headers=headers,
+        verify=verify,
+        files=files)
+    if res.status_code != 200:
+      raise ValueError(
+              "Failed post request, got status: {0}".\
+                format(res.status_code))
+    res = res.json()
+    return res
   except Exception as e:
     raise
 
@@ -27,20 +34,23 @@ def post_request(url,  data, headers=None, verify=False, file_attachment=None):
 def get_login_token(portal_config_file, verify=False, url_suffix='/api/v1/security/login'):
   try:
     portal_config = read_json_data(portal_config_file)
+    if isinstance(portal_config, list):
+      portal_config = portal_config[0]
     base_url = portal_config.get('base_url')
     login_data = portal_config.get('login_data')
     if login_data is None:
       raise KeyError("Missing logging info")
+    if isinstance(login_data, dict):
+      login_data = json.dumps(login_data)
     if base_url is None:
       raise KeyError("Missing base url")
     url = urljoin(base_url, url_suffix)
-    res = \
+    json_res = \
       post_request(
         url=url,
         data=login_data,
         headers={"Content-Type": "application/json"},
         verify=verify)
-    json_res = res.json()
     token = json_res.get('access_token')
     return token
   except Exception as e:
@@ -51,12 +61,11 @@ def upload_files_to_portal(portal_config_file, file_path, url_suffix, verify=Fal
   try:
     check_file_path(file_path)
     portal_config = read_json_data(portal_config_file)
+    if isinstance(portal_config, list):
+      portal_config = portal_config[0]
     base_url = portal_config.get('base_url')
     if base_url is None:
       raise KeyError("Missing base url")
-    login_data = portal_config.get('login_data')
-    if login_data is None:
-      raise KeyError("Missing logging info")
     url = urljoin(base_url, url_suffix)
     token = \
       get_login_token(
@@ -65,10 +74,11 @@ def upload_files_to_portal(portal_config_file, file_path, url_suffix, verify=Fal
     res = \
       post_request(
         url=url,
-        data=login_data,
-        headers={'accept': 'application/json', "Authorization": "Bearer {0}".format(token)},
+        data=None,
+        headers={"accept": "application/json", "Authorization": "Bearer {0}".format(token)},
         file_attachment=file_path,
         verify=verify)
+    return res
   except Exception as e:
     raise ValueError(
             "Failed to upload file {0} to portal, error: {1}".\
