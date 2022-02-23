4242b4c7060f0192950b6ace5bec9e6b8a342480
Avik Datta <avik.datta@imperial.ac.uk>
2022-02-23 00:13:23 +0000

    get method for portal REST api

--- igf_portal/api_utils.py
+++ igf_portal/api_utils.py
@@ -3,8 +3,21 @@ from urllib.parse import urljoin
 from igf_data.utils.dbutils import read_json_data
 from igf_data.utils.fileutils import check_file_path
 
-def get_request(portal_config_file, url_suffix):
-  pass
+def get_request(url, headers=None, verify=False):
+  try:
+    res = \
+      requests.get(
+        url=url,
+        headers=headers,
+        verify=verify)
+    if res.status_code != 200:
+      raise ValueError(
+              "Failed get request, got status: {0}".\
+                format(res.status_code))
+    res = res.json()
+    return res
+  except Exception as e:
+    raise ValueError(e)
 
 def post_request(url,  data, headers=None, verify=False, file_attachment=None):
   try:
@@ -55,6 +68,30 @@ def get_login_token(portal_config_file, verify=False, url_suffix='/api/v1/securi
     return token
   except Exception as e:
     raise ValueError("Failed to get token from portal, error: {0}".format(e))
+
+
+def get_data_from_portal(portal_config_file, url_suffix, verify=False):
+  try:
+    check_file_path(portal_config_file)
+    portal_config = read_json_data(portal_config_file)
+    if isinstance(portal_config, list):
+      portal_config = portal_config[0]
+    base_url = portal_config.get('base_url')
+    if base_url is None:
+      raise KeyError("Missing base url")
+    url = urljoin(base_url, url_suffix)
+    token = \
+      get_login_token(
+        portal_config_file=portal_config_file,
+        verify=verify)
+    res = \
+      get_request(
+        url=url,
+        headers={"accept": "application/json", "Authorization": "Bearer {0}".format(token)},
+        verify=verify)
+    return res
+  except Exception as e:
+    raise ValueError(e)
 
 
 def upload_files_to_portal(portal_config_file, file_path, url_suffix, verify=False):
