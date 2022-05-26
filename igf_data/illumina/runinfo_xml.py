3a758304d2e9ceb70ec37ce4e5cdffd7ff904494
Avik Datta <avik.datta@imperial.ac.uk>
2022-05-26 20:37:46 +0100

    updated runinfo for nextseq2k

--- igf_data/illumina/runinfo_xml.py
+++ igf_data/illumina/runinfo_xml.py
@@ -13,65 +13,76 @@ class RunInfo_xml:
     self._read_xml()
 
   def get_reads_stats(self, root_tag='read', number_tag='number',
-                      tags=('isindexedread','numcycles')):
+                      tags=('isindexedread','numcycles', 'isreversecomplement')):
     '''
     A method for getting read and index stats from the RunInfo.xml file
 
     :param root_tag: Root tag for xml file, default read
     :param number_tag: Number tag for xml file, default number
-    :param tags: List of tags for xml lookup, default ['isindexedread','numcycles']
+    :param tags: List of tags for xml lookup, default ['isindexedread', 'numcycles', 'isreversecomplement']
     :returns: A dictionary with the read number as the key
     '''
     try:
       tags = list(tags)
-      reads_stats=defaultdict(lambda: defaultdict(dict))
-      soup=self._soup
-      match_count=0
+      reads_stats = \
+        defaultdict(lambda: defaultdict(dict))
+      soup = self._soup
+      match_count = 0
       for r in soup.find_all(root_tag):
         for tag_name in tags:
-          reads_stats[r[number_tag]][tag_name]=r[tag_name]
-          match_count+=1
-
-      if match_count==0:
-        raise ValueError('no record found for {0} in file {1}'.format(root_tag, self.xml_file))
-
+          if tag_name in r.attrs:
+            reads_stats[r[number_tag]][tag_name] = r[tag_name]
+          else:
+            reads_stats[r[number_tag]][tag_name] = None
+          match_count += 1
+      if match_count == 0:
+        raise ValueError(f'no record found for {root_tag} in file {self.xml_file}')
       return reads_stats
-    except:
-      raise 
+    except Exception as e:
+      raise ValueError(f"Failed to get reads stats, error: {e}")
 
 
   def get_platform_number(self):
     '''
     Function for fetching the instrument series number
     '''
-    series_number=None
-    soup=self._soup
-    series_number=soup.instrument.contents[0]
+    try:
+      series_number = None
+      soup = self._soup
+      series_number = soup.instrument.contents[0]
+      if not series_number:
+        raise ValueError('could not find tag instrument')
+      return series_number
+    except Exception as e:
+      raise ValueError(f"Failed to get platform number, error: {e}")
 
-    if not series_number:
-      raise ValueError('couldn\'t find tag {0}'.format('instrument'))
-
-    return series_number
 
   def get_flowcell_name(self):
     '''
     A mthod for accessing flowcell name from the runinfo xml file
     '''
-    flowcell=None
-    flowcell=self._soup.flowcell.contents[0]
+    try:
+      flowcell = None
+      flowcell = self._soup.flowcell.contents[0]
+      if not flowcell:
+        raise ValueError('could not find tag flowcell')
+      return flowcell
+    except Exception as e:
+      raise ValueError(f"Failed to get flowcell name, error: {e}")
 
-    if not flowcell:
-      raise ValueError('couldn\'t find tag {0}'.format('flowcell'))
-    return flowcell
 
   def _read_xml(self):
     '''
     Internal function for reading the xml file using BS4
     '''
-    xml_file=self.xml_file
-    with open(xml_file, 'r') as fp:
-      soup = BeautifulSoup(fp, "html5lib")
-    self._soup = soup
+    try:
+      xml_file = self.xml_file
+      with open(xml_file, 'r') as fp:
+        soup = BeautifulSoup(fp, "html5lib")
+      self._soup = soup
+    except Exception as e:
+      raise ValueError(f"Failed to read xml file {xml_file}, error: {e}")
+
 
   def get_lane_count(self, lanecount_tag='lanecount'):
     try:
@@ -83,7 +94,8 @@ class RunInfo_xml:
         lane_counts = 0
       return lane_counts
     except Exception as e:
-      raise ValueError("Failed to get lane count, error: {0}".format(e))
+      raise ValueError(f"Failed to get lane count, error: {e}")
+
 
   def get_tiles_list(self, tag="tile"):
     try:
@@ -92,4 +104,4 @@ class RunInfo_xml:
         tiles.append(r.contents[0])
       return tiles
     except Exception as e:
-      raise ValueError("Failed to get tiles list, error: {0}".format(e))
+      raise ValueError(f"Failed to get tiles list, error: {e}")
