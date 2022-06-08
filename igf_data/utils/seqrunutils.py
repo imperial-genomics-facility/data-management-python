57418ecc2b25524104f7f9aef96e763be48cc1a2
Avik Datta <avik.datta@imperial.ac.uk>
2022-06-08 21:17:00 +0100

    minor changes

--- igf_data/utils/seqrunutils.py
+++ igf_data/utils/seqrunutils.py
@@ -2,33 +2,41 @@ import datetime
 from igf_data.igfdb.seqrunadaptor import SeqrunAdaptor
 from igf_data.utils.dbutils import read_dbconf_json, read_json_data
 
-def load_new_seqrun_data(data_file, dbconfig):
+def load_new_seqrun_data(
+      data_file: str,
+      dbconfig: str) -> None:
   '''
   A method for loading new data for seqrun table
   '''
   try:
-    formatted_data=read_json_data(data_file)
-    dbparam=read_dbconf_json(dbconfig)
-    sr=SeqrunAdaptor(**dbparam)
+    formatted_data = read_json_data(data_file)
+    dbparam = read_dbconf_json(dbconfig)
+    sr = SeqrunAdaptor(**dbparam)
     sr.start_session()
-    sr.store_seqrun_and_attribute_data(data=formatted_data)
+    sr.store_seqrun_and_attribute_data(
+      data=formatted_data)
     sr.close_session()
   except:
     raise
 
-def get_seqrun_date_from_igf_id(seqrun_igf_id):
+
+def get_seqrun_date_from_igf_id(
+      seqrun_igf_id: str) -> str:
   '''
   A utility method for fetching sequence run date from the igf id
-  
-  required params:
-  seqrun_igf_id: A seqrun igf id string
-  
-  returns a string value of the date
+
+  :params seqrun_igf_id: A seqrun igf id string
+  :returns: a string value of the date
   '''
   try:
-    seqrun_date=seqrun_igf_id.split('_')[0]                                     # collect partial seqrun date from igf id
-    seqrun_date=datetime.datetime.strptime(seqrun_date,'%y%m%d').date()         # identify actual date
-    seqrun_date=str(seqrun_date)                                                # convert object to string
+    seqrun_date = \
+      seqrun_igf_id.split('_')[0]                                     # collect partial seqrun date from igf id
+    seqrun_date = \
+      datetime.datetime.\
+        strptime(seqrun_date,'%y%m%d').date()                         # identify actual date
+    seqrun_date = \
+      str(seqrun_date)                                                # convert object to string
     return seqrun_date
-  except:
-    raise
+  except Exception as e:
+    raise ValueError(
+      f'Failed to get seqrun date from igf id {seqrun_igf_id}, error: {e}')
