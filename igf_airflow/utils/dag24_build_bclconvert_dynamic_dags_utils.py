2e03e0ec489f2ebd9d8e5925c15a5d8671f1a72f
Avik Datta <avik.datta@imperial.ac.uk>
2022-08-10 15:57:45 +0100

    dynamic dag builder dag

--- igf_airflow/utils/dag24_build_bclconvert_dynamic_dags_utils.py
+++ igf_airflow/utils/dag24_build_bclconvert_dynamic_dags_utils.py
@@ -16,8 +16,10 @@ from igf_data.illumina.runinfo_xml import RunInfo_xml
 from igf_data.utils.pipelineutils import check_and_load_pipeline
 from igf_airflow.utils.dag22_bclconvert_demult_utils import _get_formatted_samplesheets
 from igf_airflow.utils.dag22_bclconvert_demult_utils import _create_output_from_jinja_template
+from igf_airflow.utils.dag22_bclconvert_demult_utils import get_platform_name_and_flowcell_id_for_seqrun
 from igf_data.utils.dbutils import read_dbconf_json
 from igf_airflow.logging.upload_log_msg import send_log_to_channels
+
 
 log = logging.getLogger(__name__)
 
@@ -25,9 +27,13 @@ IGF_PORTAL_CONF = Variable.get('igf_portal_conf', default_var=None)
 SLACK_CONF = Variable.get('slack_conf',default_var=None)
 MS_TEAMS_CONF = Variable.get('ms_teams_conf',default_var=None)
 HPC_SEQRUN_BASE_PATH = Variable.get('hpc_seqrun_path', default_var=None)
+HPC_SSH_KEY_FILE = Variable.get('hpc_ssh_key_file', default_var=None)
 DATABASE_CONFIG_FILE = Variable.get('database_config_file', default_var=None)
 SINGLECELL_BARCODE_JSON = Variable.get('singlecell_barcode_json', default_var=None)
 SINGLECELL_DUAL_BARCODE_JSON = Variable.get('singlecell_dual_barcode_json', default_var=None)
+DYNAMIC_DEMULT_DAG_TEMPLATE = Variable.get('dynamic_demult_dag_template', default_var=None)
+HPC_DYNAMIC_DAG_DIR = Variable.get('hpc_dynamic_dag_dir', default_var=None)
+REMOTE_SERVER_USERNAME = Variable.get('seqrun_server_user', default_var=None)
 
 
 def _fetch_samplesheet_for_run(
@@ -269,3 +275,259 @@ def _get_sample_group_info_for_formatted_samplesheets(
   except Exception as e:
     raise ValueError(
       f'Failed to get sample group info: {e}')
+
+
+def generate_dynamic_dag_func(**context):
+  try:
+    ti = context.get('ti')
+    dag_run = context.get('dag_run')
+    formatted_samplesheets_key = \
+      context['params'].\
+        get('formatted_samplesheets_key', 'formatted_samplesheets')
+    formatted_samplesheets_task = \
+      context['params'].\
+        get('formatted_samplesheets_task', 'format_samplesheet')
+    sample_groups_key = \
+      context['params'].\
+        get('sample_groups_key', 'sample_groups')
+    sample_groups_task = \
+      context['params'].\
+        get('sample_groups_task', 'format_samplesheet')
+    samplesheet_info_key = \
+      context['params'].\
+        get('samplesheet_info_key', 'samplesheet_info')
+    samplesheet_info_task = \
+      context['params'].\
+        get('samplesheet_info_task', 'fetch_seqrun_data_from_portal')
+    samplesheet_id_key = \
+      context['params'].\
+        get('samplesheet_id_key', 'samplesheet_id')
+    temp_dag_file_key = \
+      context['params'].\
+        get('temp_dag_file_key', 'temp_dag_file')
+    dag_id_key = \
+      context['params'].\
+        get('dag_id_key', 'dag_id')
+    ## fetch samplesheet info
+    samplesheet_info = \
+      ti.xcom_pull(
+        task_ids=samplesheet_info_task,
+        key=samplesheet_info_key)
+    if samplesheet_info is None:
+      raise ValueError(
+        f'Failed to get samplesheet info from xcom')
+    if samplesheet_id_key not in samplesheet_info:
+      raise ValueError(
+        f'Failed to get samplesheet file from xcom')
+    samplesheet_id = \
+      samplesheet_info.get(samplesheet_id_key)
+    ## get seqrun id
+    seqrun_id = None
+    if dag_run is not None and \
+       dag_run.conf is not None and \
+       dag_run.conf.get('seqrun_id') is not None:
+      seqrun_id = dag_run.conf.get('seqrun_id')
+    if seqrun_id is None:
+      raise ValueError('seqrun_id is not provided')
+    ## get formatted samplesheets and sample groups
+    formatted_samplesheets = \
+      ti.xcom_pull(
+        task_ids=formatted_samplesheets_task,
+        key=formatted_samplesheets_key)
+    if formatted_samplesheets is None:
+      raise ValueError(
+        f'Failed to get formatted samplesheets from xcom')
+    sample_groups = \
+      ti.xcom_pull(
+        task_ids=sample_groups_task,
+        key=sample_groups_key)
+    if sample_groups is None:
+      raise ValueError(
+        f'Failed to get sample groups from xcom')
+    ## get flowcell ids
+    _, flowcell_id  = \
+      get_platform_name_and_flowcell_id_for_seqrun(
+        seqrun_igf_id=samplesheet_id,
+        db_config_file=DATABASE_CONFIG_FILE)
+    ## get dynamic dag
+    temp_dag_dir = \
+      get_temp_dir(use_ephemeral_space=True)
+    dag_id = f"demult_{flowcell_id}_{samplesheet_id}"
+    output_file = \
+      os.path.join(temp_dag_dir, f"{dag_id}.py")
+    _build_dynamic_demult_dag(
+      template_file=DYNAMIC_DEMULT_DAG_TEMPLATE,
+      output_file='',
+      formatted_samplesheets=formatted_samplesheets,
+      sample_groups=sample_groups,
+      seqrun_igf_id=seqrun_id)
+    ## Fix read permission for containers
+    os.chmod(
+      output_file,
+      stat.S_IRUSR |
+      stat.S_IWUSR |
+      stat.S_IRGRP |
+      stat.S_IWGRP |
+      stat.S_IROTH)
+    ## push temp dagfile to xcom
+    ti.xcom_push(
+      key=temp_dag_file_key,
+      value=output_file)
+    ti.xcom_push(
+      key=dag_id_key,
+      value=dag_id)
+  except Exception as e:
+    log.error(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=e,
+      reaction='fail')
+    raise
+
+
+def _build_dynamic_demult_dag(
+      template_file: str,
+      output_file: str,
+      formatted_samplesheets: list,
+      sample_groups: dict,
+      seqrun_igf_id: str) \
+        -> None:
+  try:
+    if os.path.exists(output_file):
+      raise IOError(f"File {output_file} already exists")
+    _create_output_from_jinja_template(
+      template_file=template_file,
+      output_file=output_file,
+      autoescape_list=['xml', 'html'],
+      data=dict(
+        SEQRUN_IGF_ID=seqrun_igf_id,
+        SAMPLE_GROUPS=sample_groups,
+        FORMATTED_SAMPLESHEETS=formatted_samplesheets))
+  except Exception as e:
+    raise ValueError(
+      f'Failed to build dynamic dag: {e}')
+
+
+def copy_dag_to_hpc_func(**context):
+  try:
+    ti = context.get('ti')
+    temp_dag_file_key = \
+      context['params'].\
+        get('temp_dag_file_key', 'temp_dag_file')
+    temp_dag_file_task = \
+      context['params'].\
+        get('temp_dag_file_task', 'generate_dynamic_dag')
+    ## get temp dagfile
+    temp_dag_file = \
+      ti.xcom_pull(
+        task_ids=temp_dag_file_task,
+        key=temp_dag_file_key)
+    if temp_dag_file is None:
+      raise ValueError(
+        f'Failed to get temp dagfile from xcom')
+    check_file_path(temp_dag_file)
+    ## copy dynamic dag to destination
+    dest_file = \
+      os.path.join(
+        HPC_DYNAMIC_DAG_DIR,
+        os.path.basename(temp_dag_file))
+    if os.path.exists(dest_file):
+      raise IOError(f"File {dest_file} already exists")
+    copy_local_file(temp_dag_file, dest_file)
+  except Exception as e:
+    log.error(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=e,
+      reaction='fail')
+    raise
+
+
+def copy_dag_to_remote_func(**context):
+  try:
+    ti = context.get('ti')
+    temp_dag_file_key = \
+      context['params'].\
+        get('temp_dag_file_key', 'temp_dag_file')
+    temp_dag_file_task = \
+      context['params'].\
+        get('temp_dag_file_task', 'generate_dynamic_dag')
+    remote_dynamic_dag_path = \
+      context['params'].\
+        get('remote_dynamic_dag_path')
+    remote_server_hostname = \
+      context['params'].\
+        get('remote_server_hostname')
+    ## get temp dagfile
+    temp_dag_file = \
+      ti.xcom_pull(
+        task_ids=temp_dag_file_task,
+        key=temp_dag_file_key)
+    if temp_dag_file is None:
+      raise ValueError(
+        f'Failed to get temp dagfile from xcom')
+    check_file_path(temp_dag_file)
+    ## copy dynamic dag to destination
+    dest_file = \
+      os.path.join(
+        remote_dynamic_dag_path,
+        os.path.basename(temp_dag_file))
+    copy_remote_file(
+      temp_dag_file,
+      dest_file,
+      force_update=True,
+      destination_address=f'{REMOTE_SERVER_USERNAME}@{remote_server_hostname}',
+      ssh_key_file=HPC_SSH_KEY_FILE)
+  except Exception as e:
+    log.error(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=e,
+      reaction='fail')
+    raise
+
+
+def register_pipeline_func(**context):
+  try:
+    ti = context.get('ti')
+    dag_id_key = \
+      context['params'].\
+        get('dag_id_key', 'dag_id')
+    dag_id_task = \
+      context['params'].\
+        get('dag_id_task', 'generate_dynamic_dag')
+    ## get dag id
+    dag_id = \
+      ti.xcom_pull(
+        task_ids=dag_id_task,
+        key=dag_id_key)
+    if dag_id is None:
+      raise ValueError(
+        f'Failed to get dag id from xcom')
+    ## register pipeline
+    pipeline_data = [{
+      "pipeline_name" : dag_id,
+      "pipeline_db" : "postgres",
+      "pipeline_type" : "AIRFLOW"}]
+    check_and_load_pipeline(
+      pipeline_data=pipeline_data,
+      dbconfig=DATABASE_CONFIG_FILE)
+  except Exception as e:
+    log.error(e)
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      comment=e,
+      reaction='fail')
+    raise
