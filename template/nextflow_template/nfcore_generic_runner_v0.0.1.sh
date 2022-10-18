8ace19869ddbd6bca658ddadf44d84684ba3c587
Avik Datta <avik.datta@imperial.ac.uk>
2022-10-18 15:54:09 +0100

    adding new generic nextflow runner

---
+++ template/nextflow_template/nfcore_generic_runner_v0.0.1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## THIS IS A TEMPLATE FOR RUNNING NF-CORE PIPELINE
+## VERSION: 0.0.1
+##
+## REQUIRED INPUTS:
+##
+##  * NEXTFLOW_PARAMS: Nextflow params
+##  * NEXTFLOW_CONF: Nextflow config file for the run
+##  * NEXTFLOW_VERSION: Nextflow version for the tool
+##  * NFCORE_PIPELINE_NAME: NF-Core pipeline name
+##
+
+## IMPORT ENV
+source /project/tgu/resources/pipeline_resource/nextflow/env.sh
+
+## SET NXF_VER FOR PIPELINE RUN
+export NXF_VER={{ NEXTFLOW_VERSION }}
+
+## NEXTFLOW RUN CMD
+$NEXTFLOW_EXE run {{ NFCORE_PIPELINE_NAME }} \
+  -with-tower $NEXTFLOW_TOWER \
+  -c {{ NEXTFLOW_CONF }} \
+  {{ NEXTFLOW_PARAMS }}
