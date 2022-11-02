7c7b596403eec0caa6d293a24ef91db92e9c62ef
Avik Datta <avik.datta@imperial.ac.uk>
2022-11-02 15:19:58 +0000

    added chipseq and atacseq input formatter

--- igf_nextflow/nextflow_utils/nextflow_input_formatter.py
+++ igf_nextflow/nextflow_utils/nextflow_input_formatter.py
@@ -1023,9 +1023,26 @@ def prepare_nfcore_ampliseq_input(
 def prepare_nfcore_rnafusion_input(
       runner_template_file: str,
       config_template_file: str,
+      project_name: str,
+      hpc_data_dir: str,
       dbconf_file: str,
       sample_metadata: dict,
-      analysis_metadata: dict) \
+      analysis_metadata: dict,
+      nfcore_pipeline_name: str = 'nf-core/rnafusion',
+      exclude_nf_param_list: list = [
+        '-resume',
+        '-profile',
+        '-c',
+        '-config',
+        '--input',
+        '--outdir',
+        '-with-report',
+        '-with-timeline',
+        '-with-dag',
+        '-with-tower',
+        '-w',
+        '-work-dir',
+        '-with-notification']) \
         -> Tuple[str, str]:
   """
   :param runner_template_file
@@ -1044,7 +1061,18 @@ def prepare_nfcore_rnafusion_input(
                "--genomes_base path"]
   """
   try:
-    pass
+    work_dir, runner_file = \
+      prepare_nfcore_rnaseq_input(
+      runner_template_file=runner_template_file,
+      config_template_file=config_template_file,
+      project_name=project_name,
+      hpc_data_dir=hpc_data_dir,
+      dbconf_file=dbconf_file,
+      sample_metadata=sample_metadata,
+      analysis_metadata=analysis_metadata,
+      nfcore_pipeline_name=nfcore_pipeline_name,
+      exclude_nf_param_list=exclude_nf_param_list)
+    return work_dir, runner_file
   except Exception as e:
     raise ValueError(
       f"Failed to create input for NFCore smrnaseq pipeline, error: {e}")
@@ -1053,9 +1081,26 @@ def prepare_nfcore_rnafusion_input(
 def prepare_nfcore_rnavar_input(
       runner_template_file: str,
       config_template_file: str,
+      project_name: str,
+      hpc_data_dir: str,
       dbconf_file: str,
       sample_metadata: dict,
-      analysis_metadata: dict) \
+      analysis_metadata: dict,
+      nfcore_pipeline_name: str = 'nf-core/rnavar',
+      exclude_nf_param_list: list = [
+        '-resume',
+        '-profile',
+        '-c',
+        '-config',
+        '--input',
+        '--outdir',
+        '-with-report',
+        '-with-timeline',
+        '-with-dag',
+        '-with-tower',
+        '-w',
+        '-work-dir',
+        '-with-notification']) \
         -> Tuple[str, str]:
   """
   :param runner_template_file
@@ -1076,18 +1121,116 @@ def prepare_nfcore_rnavar_input(
                "--seq_platform illumina"]}
   """
   try:
-    pass
+    work_dir, runner_file = \
+      prepare_nfcore_rnaseq_input(
+      runner_template_file=runner_template_file,
+      config_template_file=config_template_file,
+      project_name=project_name,
+      hpc_data_dir=hpc_data_dir,
+      dbconf_file=dbconf_file,
+      sample_metadata=sample_metadata,
+      analysis_metadata=analysis_metadata,
+      nfcore_pipeline_name=nfcore_pipeline_name,
+      exclude_nf_param_list=exclude_nf_param_list)
+    return work_dir, runner_file
   except Exception as e:
     raise ValueError(
       f"Failed to create input for NFCore smrnaseq pipeline, error: {e}")
+
+def _make_nfcore_atacseq_input(sample_metadata: dict,
+      fastq_df: pd.DataFrame,
+      output_dir: str,
+      output_file_path: str = 'input.csv',
+      sample_id_input_column: str = 'sample_igf_id',
+      run_id_input_column: str = 'run_igf_id',
+      fastq_file_input_column: str = 'file_path',
+      output_fastq1_column: str = 'fastq_1',
+      output_fastq2_column: str = 'fastq_2',
+      output_group_column: str = 'group',
+      output_replicate_column: str = 'replicate') -> \
+        str:
+  try:
+    fastq1_pattern = \
+      re.compile('\S+_R1_001.fastq.gz')
+    fastq2_pattern = \
+      re.compile('\S+_R2_001.fastq.gz')
+    input_csv_file_path = \
+      os.path.join(output_dir, output_file_path)
+    if os.path.exists(input_csv_file_path):
+      raise ValueError(
+        f"File {input_csv_file_path} is already present")
+    if sample_id_input_column not in fastq_df.columns or \
+       run_id_input_column not in fastq_df.columns or \
+       fastq_file_input_column not in fastq_df.columns:
+      raise KeyError(
+        "Missing required columns in fastq list")
+    final_csv_data = list()
+    for sample_entry in sample_metadata.keys():
+      sample_df = \
+        fastq_df[fastq_df[sample_id_input_column]==sample_entry]
+      if len(sample_df.index) == 0:
+        raise ValueError(
+          f"Missing fastqs for sample {sample_entry}")
+      for run_id, r_data in sample_df.groupby(run_id_input_column):
+        fastqs = \
+          r_data[fastq_file_input_column].values.tolist()
+        fastq1 = ''
+        fastq2 = ''
+        for f in fastqs:
+          if re.search(fastq1_pattern, f):
+            fastq1 = f
+          if re.search(fastq2_pattern, f):
+            fastq2 = f
+        if fastq1 == "":
+          raise ValueError(
+            f"Missing fastq1 for sample {sample_entry}")
+        row_data = {
+          output_fastq1_column: fastq1,
+          output_fastq2_column: fastq2,
+        }
+        extra_metadata = \
+          sample_metadata[sample_entry]
+        if isinstance(extra_metadata, dict):
+          row_data.\
+            update(**extra_metadata)
+        final_csv_data.\
+          append(row_data)
+    final_csv_df = \
+      pd.DataFrame(final_csv_data)
+    if output_group_column not in final_csv_df.columns or \
+       output_replicate_column not in final_csv_df.columns:
+      raise KeyError(
+        f"Missing {output_group_column} or {output_replicate_column} in sample_metadata")
+    final_csv_df.\
+      to_csv(input_csv_file_path, index=False)
+    return input_csv_file_path
+  except Exception as e:
+    raise ValueError(f"Failed to get input for atacseq, error: {e}")
 
 
 def prepare_nfcore_atacseq_input(
       runner_template_file: str,
       config_template_file: str,
+      project_name: str,
+      hpc_data_dir: str,
       dbconf_file: str,
       sample_metadata: dict,
-      analysis_metadata: dict) \
+      analysis_metadata: dict,
+      nfcore_pipeline_name: str = 'nf-core/atacseq',
+      exclude_nf_param_list: list = [
+        '-resume',
+        '-profile',
+        '-c',
+        '-config',
+        '--input',
+        '--outdir',
+        '-with-report',
+        '-with-timeline',
+        '-with-dag',
+        '-with-tower',
+        '-w',
+        '-work-dir',
+        '-with-notification']) \
         -> Tuple[str, str]:
   """
   :param runner_template_file
@@ -1107,18 +1250,175 @@ def prepare_nfcore_atacseq_input(
                "--genome GRCh38"]}
   """
   try:
-    pass
+    ## get fastq df
+    fastq_df = \
+      parse_sample_metadata_and_fetch_fastq(
+        sample_metadata=sample_metadata,
+        dbconf_file=dbconf_file)
+    ## get template and extend it and dump it as bash script
+    work_dir = \
+      get_temp_dir(use_ephemeral_space=True)                     ## get work dir
+    project_data_dir = \
+      os.path.join(
+        hpc_data_dir,
+        project_name)
+    check_file_path(project_data_dir)                            ## get and check project data dir on hpc
+    formatted_config_file = \
+      format_nextflow_conf(
+        config_template_file=config_template_file,
+        singularity_bind_dir_list=[project_data_dir, work_dir],
+        output_dir=work_dir)                                     ## get formatted config file for NextFlow run
+    nextflow_version = \
+      analysis_metadata.get('NXF_VER')                           ## get nextflow version from analysis design and its required
+    if nextflow_version is None:
+      raise KeyError(
+        "NXF_VER is missing from NextFlow analysis design")
+    nextflow_params = \
+      analysis_metadata.get('nextflow_params')                   ## get nextflow_params list and its optional
+    nextflow_params_list = list()
+    if nextflow_params is not None and \
+       isinstance(nextflow_params, list):
+      for i in nextflow_params:
+        param_flag = i.split(' ')                                ## only check param flag if its separate by space from param value
+        if param_flag not in exclude_nf_param_list:
+          nextflow_params_list.\
+            append(i)
+    ## add required params
+    ## prepare input file
+    input_file = \
+      _make_nfcore_atacseq_input(
+        sample_metadata=sample_metadata,
+        fastq_df=fastq_df,
+        output_dir=work_dir)
+    nextflow_params_list.\
+      append(f'--input {input_file}')
+    nextflow_params_list.\
+      append(f"--outdir {work_dir}")
+    nextflow_params_list.\
+      append(f"-with-report {os.path.join(work_dir, 'report.html')}")
+    nextflow_params_list.\
+      append(f"-with-dag {os.path.join(work_dir, 'dag.html')}")
+    nextflow_params_list.\
+      append(f"-with-timeline {os.path.join(work_dir, 'timeline.html')}")
+    nextflow_params_list = \
+      " ".join(nextflow_params_list)
+    ## dump command file
+    runner_file = \
+      os.path.join(
+        work_dir,
+        os.path.basename(runner_template_file))
+    _create_output_from_jinja_template(
+      template_file=runner_template_file,
+      output_file=runner_file,
+      autoescape_list=['xml',],
+      data={
+        "NEXTFLOW_VERSION": nextflow_version,
+        "NFCORE_PIPELINE_NAME": nfcore_pipeline_name,
+        "NEXTFLOW_CONF": formatted_config_file,
+        "NEXTFLOW_PARAMS": nextflow_params_list
+      })
+    return work_dir, runner_file
   except Exception as e:
     raise ValueError(
       f"Failed to create input for NFCore smrnaseq pipeline, error: {e}")
 
 
+def _make_nfcore_chipseq_input(sample_metadata: dict,
+      fastq_df: pd.DataFrame,
+      output_dir: str,
+      output_file_path: str = 'input.csv',
+      sample_id_input_column: str = 'sample_igf_id',
+      run_id_input_column: str = 'run_igf_id',
+      fastq_file_input_column: str = 'file_path',
+      output_sample_column: str = 'sample',
+      output_fastq1_column: str = 'fastq_1',
+      output_fastq2_column: str = 'fastq_2',
+      output_control_column: str = 'control',
+      output_antibody_column: str = 'antibody') -> \
+        str:
+  try:
+    fastq1_pattern = \
+      re.compile('\S+_R1_001.fastq.gz')
+    fastq2_pattern = \
+      re.compile('\S+_R2_001.fastq.gz')
+    input_csv_file_path = \
+      os.path.join(output_dir, output_file_path)
+    if os.path.exists(input_csv_file_path):
+      raise ValueError(
+        f"File {input_csv_file_path} is already present")
+    if sample_id_input_column not in fastq_df.columns or \
+       run_id_input_column not in fastq_df.columns or \
+       fastq_file_input_column not in fastq_df.columns:
+      raise KeyError(
+        "Missing required columns in fastq list")
+    final_csv_data = list()
+    for sample_entry in sample_metadata.keys():
+      sample_df = \
+        fastq_df[fastq_df[sample_id_input_column]==sample_entry]
+      if len(sample_df.index) == 0:
+        raise ValueError(
+          f"Missing fastqs for sample {sample_entry}")
+      for run_id, r_data in sample_df.groupby(run_id_input_column):
+        fastqs = \
+          r_data[fastq_file_input_column].values.tolist()
+        fastq1 = ''
+        fastq2 = ''
+        for f in fastqs:
+          if re.search(fastq1_pattern, f):
+            fastq1 = f
+          if re.search(fastq2_pattern, f):
+            fastq2 = f
+        if fastq1 == "":
+          raise ValueError(
+            f"Missing fastq1 for sample {sample_entry}")
+        row_data = {
+          output_sample_column: sample_entry,
+          output_fastq1_column: fastq1,
+          output_fastq2_column: fastq2,
+        }
+        extra_metadata = \
+          sample_metadata[sample_entry]
+        if isinstance(extra_metadata, dict):
+          row_data.\
+            update(**extra_metadata)
+        final_csv_data.\
+          append(row_data)
+    final_csv_df = \
+      pd.DataFrame(final_csv_data)
+    if output_control_column not in final_csv_df.columns or \
+       output_antibody_column not in final_csv_df.columns:
+      raise KeyError(
+        f"Missing {output_control_column} or {output_antibody_column} in sample_metadata")
+    final_csv_df.\
+      to_csv(input_csv_file_path, index=False)
+    return input_csv_file_path
+  except Exception as e:
+    raise ValueError(f"Failed to get input for chipseq, error: {e}")
+
+
 def prepare_nfcore_chipseq_input(
       runner_template_file: str,
       config_template_file: str,
+      project_name: str,
+      hpc_data_dir: str,
       dbconf_file: str,
       sample_metadata: dict,
-      analysis_metadata: dict) \
+      analysis_metadata: dict,
+      nfcore_pipeline_name: str = 'nf-core/chipseq',
+      exclude_nf_param_list: list = [
+        '-resume',
+        '-profile',
+        '-c',
+        '-config',
+        '--input',
+        '--outdir',
+        '-with-report',
+        '-with-timeline',
+        '-with-dag',
+        '-with-tower',
+        '-w',
+        '-work-dir',
+        '-with-notification']) \
         -> Tuple[str, str]:
   """
   :param runner_template_file
@@ -1138,7 +1438,74 @@ def prepare_nfcore_chipseq_input(
                "--genome GRCh38"]}
   """
   try:
-    pass
+    ## get fastq df
+    fastq_df = \
+      parse_sample_metadata_and_fetch_fastq(
+        sample_metadata=sample_metadata,
+        dbconf_file=dbconf_file)
+    ## get template and extend it and dump it as bash script
+    work_dir = \
+      get_temp_dir(use_ephemeral_space=True)                     ## get work dir
+    project_data_dir = \
+      os.path.join(
+        hpc_data_dir,
+        project_name)
+    check_file_path(project_data_dir)                            ## get and check project data dir on hpc
+    formatted_config_file = \
+      format_nextflow_conf(
+        config_template_file=config_template_file,
+        singularity_bind_dir_list=[project_data_dir, work_dir],
+        output_dir=work_dir)                                     ## get formatted config file for NextFlow run
+    nextflow_version = \
+      analysis_metadata.get('NXF_VER')                           ## get nextflow version from analysis design and its required
+    if nextflow_version is None:
+      raise KeyError(
+        "NXF_VER is missing from NextFlow analysis design")
+    nextflow_params = \
+      analysis_metadata.get('nextflow_params')                   ## get nextflow_params list and its optional
+    nextflow_params_list = list()
+    if nextflow_params is not None and \
+       isinstance(nextflow_params, list):
+      for i in nextflow_params:
+        param_flag = i.split(' ')                                ## only check param flag if its separate by space from param value
+        if param_flag not in exclude_nf_param_list:
+          nextflow_params_list.\
+            append(i)
+    ## add required params
+    ## prepare input file
+    input_file = \
+      _make_nfcore_chipseq_input(
+        sample_metadata=sample_metadata,
+        fastq_df=fastq_df,
+        output_dir=work_dir)
+    nextflow_params_list.\
+      append(f'--input {input_file}')
+    nextflow_params_list.\
+      append(f"--outdir {work_dir}")
+    nextflow_params_list.\
+      append(f"-with-report {os.path.join(work_dir, 'report.html')}")
+    nextflow_params_list.\
+      append(f"-with-dag {os.path.join(work_dir, 'dag.html')}")
+    nextflow_params_list.\
+      append(f"-with-timeline {os.path.join(work_dir, 'timeline.html')}")
+    nextflow_params_list = \
+      " ".join(nextflow_params_list)
+    ## dump command file
+    runner_file = \
+      os.path.join(
+        work_dir,
+        os.path.basename(runner_template_file))
+    _create_output_from_jinja_template(
+      template_file=runner_template_file,
+      output_file=runner_file,
+      autoescape_list=['xml',],
+      data={
+        "NEXTFLOW_VERSION": nextflow_version,
+        "NFCORE_PIPELINE_NAME": nfcore_pipeline_name,
+        "NEXTFLOW_CONF": formatted_config_file,
+        "NEXTFLOW_PARAMS": nextflow_params_list
+      })
+    return work_dir, runner_file
   except Exception as e:
     raise ValueError(
       f"Failed to create input for NFCore smrnaseq pipeline, error: {e}")
--- test/igf_nextflow/nextflow_input_formatter_test.py
+++ test/igf_nextflow/nextflow_input_formatter_test.py
@@ -30,7 +30,10 @@ from igf_nextflow.nextflow_utils.nextflow_input_formatter import prepare_nfcore_
 from igf_nextflow.nextflow_utils.nextflow_input_formatter import _make_nfcore_sarek_input
 from igf_nextflow.nextflow_utils.nextflow_input_formatter import _make_nfcore_ampliseq_input
 from igf_nextflow.nextflow_utils.nextflow_input_formatter import prepare_nfcore_ampliseq_input
-
+from igf_nextflow.nextflow_utils.nextflow_input_formatter import _make_nfcore_atacseq_input
+from igf_nextflow.nextflow_utils.nextflow_input_formatter import prepare_nfcore_atacseq_input
+from igf_nextflow.nextflow_utils.nextflow_input_formatter import _make_nfcore_chipseq_input
+from igf_nextflow.nextflow_utils.nextflow_input_formatter import prepare_nfcore_chipseq_input
 
 class Prepare_nfcore_input_testA(unittest.TestCase):
   def setUp(self):
@@ -740,6 +743,185 @@ class Prepare_nfcore_input_testA(unittest.TestCase):
     self.assertTrue(check_config_file)
     self.assertTrue(check_extra_params)
     self.assertTrue(check_metadata_file)
+
+
+  def test_make_nfcore_atacseq_input(self):
+    sample_metadata = {
+      "sampleA": {
+        "group": "control1",
+        "replicate": 1},
+      "sampleB": {
+        "group": "control2",
+        "replicate": 2}
+    }
+    fastq_df = \
+      parse_sample_metadata_and_fetch_fastq(
+        sample_metadata=sample_metadata,
+        dbconf_file=self.dbconfig)
+    input_file = \
+      _make_nfcore_atacseq_input(
+        sample_metadata=sample_metadata,
+        fastq_df=fastq_df,
+        output_dir=self.temp_dir)
+    self.assertTrue(os.path.exists(input_file))
+    csv_data = \
+      pd.read_csv(input_file, sep=",", header=0)
+    self.assertTrue('fastq_1' in csv_data)
+    self.assertTrue('fastq_2' in csv_data)
+    self.assertTrue('group' in csv_data)
+    self.assertTrue('replicate' in csv_data)
+    control1 = \
+      csv_data[csv_data['group']=='control1']
+    self.assertEqual(len(control1.index), 3)
+    self.assertTrue('/path/sampleA_S1_L001_R1_001.fastq.gz' in control1['fastq_1'].values.tolist())
+    self.assertTrue('/path/sampleA_S1_L001_R2_001.fastq.gz' in control1['fastq_2'].values.tolist())
+
+  def test_prepare_nfcore_atacseq_input(self):
+    sample_metadata = {
+      "sampleA": {
+        "group": "control1",
+        "replicate": 1},
+      "sampleB": {
+        "group": "control2",
+        "replicate": 2}
+    }
+    analysis_metadata = { 
+      "NXF_VER": "x.y.z",
+      "nextflow_params":  [
+        "-profile singularity",
+        "-r 1.2.2",
+        "--genome GRCh38"
+      ]}
+    hpc_data_dir = \
+      os.path.join(
+        self.temp_dir, 
+        'hpc_data_dir')
+    os.makedirs(
+      os.path.join(
+        self.temp_dir, 
+        'hpc_data_dir',
+        'projectA'))
+    work_dir, runner_file = \
+      prepare_nfcore_atacseq_input(
+        runner_template_file=self.runner_template_file,
+        config_template_file=self.config_template_file,
+        project_name='projectA',
+        hpc_data_dir=hpc_data_dir,
+        dbconf_file=self.dbconfig,
+        sample_metadata=sample_metadata,
+        analysis_metadata=analysis_metadata)
+    self.assertTrue(os.path.exists(runner_file))
+    self.assertTrue(os.path.exists(work_dir))
+    check_config_file = False
+    config_file_path = \
+      f'-c {work_dir}/{os.path.basename(self.config_template_file)}'
+    check_input_file = False
+    input_file_path = \
+      f'--input {os.path.join(work_dir, "input.csv")}'
+    check_extra_params = False
+    extra_params = \
+      "--genome GRCh38"
+    with open(runner_file, 'r') as fp:
+      for f in fp:
+        if config_file_path in f.strip():
+          check_config_file = True
+        if input_file_path in f.strip():
+          check_input_file = True
+        if extra_params in f.strip():
+          check_extra_params = True
+    self.assertTrue(check_input_file)
+    self.assertTrue(check_config_file)
+    self.assertTrue(check_extra_params)
+
+
+  def test_make_nfcore_chipseq_input(self):
+    sample_metadata = {
+      "sampleA": {
+        "antibody": "BCATENIN",
+        "control": "sampleB"},
+      "sampleB": {
+        "antibody": "",
+        "control": ""}
+    }
+    fastq_df = \
+      parse_sample_metadata_and_fetch_fastq(
+        sample_metadata=sample_metadata,
+        dbconf_file=self.dbconfig)
+    input_file = \
+      _make_nfcore_chipseq_input(
+        sample_metadata=sample_metadata,
+        fastq_df=fastq_df,
+        output_dir=self.temp_dir)
+    self.assertTrue(os.path.exists(input_file))
+    csv_data = \
+      pd.read_csv(input_file, sep=",", header=0)
+    self.assertTrue('fastq_1' in csv_data)
+    self.assertTrue('fastq_2' in csv_data)
+    self.assertTrue('control' in csv_data)
+    self.assertTrue('antibody' in csv_data)
+    self.assertTrue('sample' in csv_data)
+    control1 = \
+      csv_data[csv_data['sample']=='sampleA']
+    self.assertEqual(len(control1.index), 3)
+    self.assertTrue('/path/sampleA_S1_L001_R1_001.fastq.gz' in control1['fastq_1'].values.tolist())
+    self.assertTrue('/path/sampleA_S1_L001_R2_001.fastq.gz' in control1['fastq_2'].values.tolist())
+
+  def test_prepare_nfcore_chipseq_input(self):
+    sample_metadata = {
+      "sampleA": {
+        "antibody": "BCATENIN",
+        "control": "sampleB"},
+      "sampleB": {
+        "antibody": "",
+        "control": ""}
+    }
+    analysis_metadata = { 
+      "NXF_VER": "x.y.z",
+      "nextflow_params":  [
+        "-profile singularity",
+        "-r 2.0.0",
+        "--genome GRCh38"
+      ]}
+    hpc_data_dir = \
+      os.path.join(
+        self.temp_dir, 
+        'hpc_data_dir')
+    os.makedirs(
+      os.path.join(
+        self.temp_dir, 
+        'hpc_data_dir',
+        'projectA'))
+    work_dir, runner_file = \
+      prepare_nfcore_chipseq_input(
+        runner_template_file=self.runner_template_file,
+        config_template_file=self.config_template_file,
+        project_name='projectA',
+        hpc_data_dir=hpc_data_dir,
+        dbconf_file=self.dbconfig,
+        sample_metadata=sample_metadata,
+        analysis_metadata=analysis_metadata)
+    self.assertTrue(os.path.exists(runner_file))
+    self.assertTrue(os.path.exists(work_dir))
+    check_config_file = False
+    config_file_path = \
+      f'-c {work_dir}/{os.path.basename(self.config_template_file)}'
+    check_input_file = False
+    input_file_path = \
+      f'--input {os.path.join(work_dir, "input.csv")}'
+    check_extra_params = False
+    extra_params = \
+      "--genome GRCh38"
+    with open(runner_file, 'r') as fp:
+      for f in fp:
+        if config_file_path in f.strip():
+          check_config_file = True
+        if input_file_path in f.strip():
+          check_input_file = True
+        if extra_params in f.strip():
+          check_extra_params = True
+    self.assertTrue(check_input_file)
+    self.assertTrue(check_config_file)
+    self.assertTrue(check_extra_params)
 
 if __name__=='__main__':
   unittest.main()
