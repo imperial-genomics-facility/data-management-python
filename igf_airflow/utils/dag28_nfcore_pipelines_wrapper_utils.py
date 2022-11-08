055ed8f343c903d4e6701ae3279206b52405692c
Avik Datta <avik.datta@imperial.ac.uk>
2022-11-08 14:38:10 +0000

    added utils function for dag28

--- igf_airflow/utils/dag28_nfcore_pipelines_wrapper_utils.py
+++ igf_airflow/utils/dag28_nfcore_pipelines_wrapper_utils.py
@@ -54,22 +54,83 @@ GLOBUS_ROOT_DIR = Variable.get("globus_root_dir", default_var=None)
 
 def prepare_nfcore_pipeline_inputs(**context):
   try:
-    pass
-  except Exception as e:
-    log.error(e)
-    send_log_to_channels(
-      slack_conf=SLACK_CONF,
-      ms_teams_conf=MS_TEAMS_CONF,
-      task_id=context['task'].task_id,
-      dag_id=context['task'].dag_id,
-      comment=e,
-      reaction='fail')
-    raise
-
-
-def prepare_nfcore_pipeline_inputs(**context):
-  try:
-    pass
+    ti = context["ti"]
+    nextflow_command_key = \
+      context['params'].\
+      get("nextflow_command_key", "nextflow_command")
+    nextflow_workdir_key = \
+      context['params'].\
+      get("nextflow_workdir_key", "nextflow_workdir")
+    next_task = \
+      context['params'].\
+      get("next_task", "run_nfcore_pipeline")
+    last_task = \
+      context['params'].\
+      get("last_task", "mark_analysis_seed_as_failed")
+    required_analysis_metadata_key = \
+      context['params'].\
+      get("required_analysis_metadata_key",
+          ['NXF_VER', 'nextflow_params', 'nfcore_pipeline'])
+    ## dag_run.conf should have analysis_id
+    dag_run = context.get('dag_run')
+    analysis_id = None
+    if dag_run is not None and \
+       dag_run.conf is not None and \
+       dag_run.conf.get('analysis_id') is not None:
+      analysis_id = \
+        dag_run.conf.get('analysis_id')
+    if analysis_id is None:
+      raise ValueError('analysis_id not found in dag_run.conf')
+    ## pipeline_name is context['task'].dag_id
+    pipeline_name = context['task'].dag_id
+    ## get analysis design
+    input_design_yaml = \
+      fetch_analysis_design(
+        analysis_id=analysis_id,
+        pipeline_name=pipeline_name,
+        dbconfig_file=DATABASE_CONFIG_FILE)
+    ## get project name
+    project_igf_id = \
+      get_project_igf_id_for_analysis(
+        analysis_id=analysis_id,
+        dbconfig_file=DATABASE_CONFIG_FILE)
+    ## prepare nextflow input files
+    sample_metadata, analysis_metadata = \
+      parse_analysis_design_and_get_metadata(
+        input_design_yaml=input_design_yaml)
+    if sample_metadata is None or \
+       analysis_metadata is None:
+        raise KeyError("Missing sample or analysis metadata")
+    for key_name in required_analysis_metadata_key:
+      if key_name not in analysis_metadata or \
+         analysis_metadata.get(key_name) is None:
+        raise KeyError(
+          f"Missing required analysis metadata key {key_name}")
+    work_dir = \
+      get_temp_dir(use_ephemeral_space=True)
+    nfcore_pipeline_name = \
+      analysis_metadata.get('nfcore_pipeline')
+    try:
+      work_dir, runner_file = \
+        prepare_input_for_multiple_nfcore_pipeline(
+          runner_template_file=NEXTFLOW_RUNNER_TEMPLATE,
+          config_template_file=NEXTFLOW_CONF_TEMPLATE,
+          project_name=project_igf_id,
+          hpc_data_dir=HPC_BASE_RAW_DATA_PATH,
+          dbconf_file=DATABASE_CONFIG_FILE,
+          sample_metadata=sample_metadata,
+          analysis_metadata=analysis_metadata,
+          nfcore_pipeline_name=nfcore_pipeline_name)
+      ## push it ot xcom
+      ti.xcom_push(
+        key=nextflow_command_key,
+        value=runner_file)
+      ti.xcom_push(
+        key=nextflow_workdir_key,
+        value=work_dir)
+      return [next_task,]
+    except:
+      return [last_task,]
   except Exception as e:
     log.error(e)
     send_log_to_channels(
