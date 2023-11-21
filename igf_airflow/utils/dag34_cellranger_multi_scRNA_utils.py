daa1d28a9ca5135ce34229b29b7652862e1401e5
Avik Datta <avik.datta@imperial.ac.uk>
2023-11-21 01:06:58 +0000

    added unit test for library csv building

---
+++ igf_airflow/utils/dag34_cellranger_multi_scRNA_utils.py
@@ -0,0 +1,264 @@
+import os
+import re
+import json
+import yaml
+import shutil
+import zipfile
+import logging
+import pandas as pd
+from typing import (
+    Tuple,
+    Optional)
+from datetime import timedelta
+from airflow.models import Variable
+from igf_data.utils.bashutils import bash_script_wrapper
+from igf_data.utils.analysis_fastq_fetch_utils import get_fastq_and_run_for_samples
+from igf_data.utils.jupyter_nbconvert_wrapper import Notebook_runner
+from jinja2 import Template
+from yaml import (
+    Loader,
+    Dumper)
+from typing import (
+    Tuple,
+    Union)
+from igf_data.igfdb.igfTables import (
+    Pipeline,
+    Pipeline_seed,
+    Project,
+    Analysis)
+from igf_data.utils.fileutils import (
+    check_file_path,
+    copy_local_file,
+    get_temp_dir,
+    read_json_data,
+    get_date_stamp,
+    get_date_stamp_for_file_name)
+from igf_data.utils.dbutils import read_dbconf_json
+from igf_data.igfdb.baseadaptor import BaseAdaptor
+from igf_data.igfdb.projectadaptor import ProjectAdaptor
+from igf_data.igfdb.pipelineadaptor import PipelineAdaptor
+from igf_data.igfdb.analysisadaptor import AnalysisAdaptor
+from igf_data.igfdb.collectionadaptor import CollectionAdaptor
+from igf_data.igfdb.fileadaptor import FileAdaptor
+from igf_airflow.logging.upload_log_msg import send_log_to_channels
+from igf_airflow.utils.dag22_bclconvert_demult_utils import (
+    _create_output_from_jinja_template,
+    send_email_via_smtp)
+from igf_airflow.utils.dag26_snakemake_rnaseq_utils import (
+    fetch_analysis_design,
+    parse_analysis_design_and_get_metadata,
+    get_project_igf_id_for_analysis,
+    calculate_analysis_name,
+    load_analysis_and_build_collection,
+    copy_analysis_to_globus_dir,
+    check_and_seed_analysis_pipeline)
+from airflow.operators.python import get_current_context
+from airflow.decorators import task
+
+log = logging.getLogger(__name__)
+
+SLACK_CONF = Variable.get('analysis_slack_conf',default_var=None)
+MS_TEAMS_CONF = Variable.get('analysis_ms_teams_conf',default_var=None)
+HPC_SSH_KEY_FILE = Variable.get('hpc_ssh_key_file', default_var=None)
+DATABASE_CONFIG_FILE = Variable.get('database_config_file', default_var=None)
+HPC_BASE_RAW_DATA_PATH = Variable.get('hpc_base_raw_data_path', default_var=None)
+HPC_FILE_LOCATION = Variable.get("hpc_file_location", default_var="HPC_PROJECT")
+
+## EMAIL CONFIG
+EMAIL_CONFIG = Variable.get("email_config", default_var=None)
+EMAIL_TEMPLATE = Variable.get("seqrun_email_template", default_var=None)
+DEFAULT_EMAIL_USER = Variable.get("default_email_user", default_var=None)
+
+## GLOBUS
+GLOBUS_ROOT_DIR = Variable.get("globus_root_dir", default_var=None)
+
+## EMAIL CONFIG
+EMAIL_CONFIG = Variable.get("email_config", default_var=None)
+EMAIL_TEMPLATE = Variable.get("analysis_email_template", default_var=None)
+
+## TASK
+@task(
+  task_id="get_analysis_group_list",
+  retry_delay=timedelta(minutes=5),
+  retries=4,
+  queue='hpc_4G')
+def get_analysis_group_list(design_dict: dict) -> dict:
+  try:
+    design_file = design_dict.get('analysis_design')
+    check_file_path(design_file)
+    with open(design_file, 'r') as fp:
+      input_design_yaml = fp.read()
+      sample_metadata, analysis_metadata = \
+        parse_analysis_design_and_get_metadata(
+          input_design_yaml=input_design_yaml)
+    if sample_metadata is None or \
+       analysis_metadata is None:
+      raise KeyError("Missing sample or analysis metadata")
+    unique_sample_groups = set()
+    for _, group in sample_metadata.items():
+      grp_name = group.get('cellranger_group')
+      if grp_name is None:
+        raise KeyError("Missing cellranger_group in sample_metadata")
+      unique_sample_groups.add(grp_name)
+    if len(unique_sample_groups) == 0:
+      raise ValueError("No sample group found")
+    return list(unique_sample_groups)
+  except Exception as e:
+    context = get_current_context()
+    log.error(e)
+    log_file_path = [
+      os.environ.get('AIRFLOW__LOGGING__BASE_LOG_FOLDER'),
+      f"dag_id={context['ti'].dag_id}",
+      f"run_id={context['ti'].run_id}",
+      f"task_id={context['ti'].task_id}",
+      f"attempt={context['ti'].try_number}.log"]
+    message = \
+      f"Error: {e}, Log: {os.path.join(*log_file_path)}"
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      project_id=None,
+      comment=message,
+      reaction='fail')
+    raise ValueError(e)
+
+
+## TASK
+@task(
+  task_id="prepare_cellranger_script",
+  retry_delay=timedelta(minutes=5),
+  retries=4,
+  queue='hpc_4G')
+def prepare_cellranger_script(sample_group: str, design_dict: dict) -> dict:
+  try:
+    design_file = design_dict.get('analysis_design')
+    check_file_path(design_file)
+    with open(design_file, 'r') as fp:
+      input_design_yaml = fp.read()
+      sample_metadata, analysis_metadata = \
+        parse_analysis_design_and_get_metadata(
+          input_design_yaml=input_design_yaml)
+    if sample_metadata is None or \
+       analysis_metadata is None:
+      raise KeyError("Missing sample or analysis metadata")
+    return {"run_script": f'run_script_{sample_group}.sh', "run_dir": f'run_dir_{sample_group}'}
+  except Exception as e:
+    context = get_current_context()
+    log.error(e)
+    log_file_path = [
+      os.environ.get('AIRFLOW__LOGGING__BASE_LOG_FOLDER'),
+      f"dag_id={context['ti'].dag_id}",
+      f"run_id={context['ti'].run_id}",
+      f"task_id={context['ti'].task_id}",
+      f"attempt={context['ti'].try_number}.log"]
+    message = \
+      f"Error: {e}, Log: {os.path.join(*log_file_path)}"
+    send_log_to_channels(
+      slack_conf=SLACK_CONF,
+      ms_teams_conf=MS_TEAMS_CONF,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      project_id=None,
+      comment=message,
+      reaction='fail')
+    raise ValueError(e)
+
+def prepare_cellranger_run_dir_and_script_file(
+      sample_group: str,
+      work_dir: str,
+      design_file: str,
+      db_config_file: str,
+      library_csv_filename: str = 'library.csv') \
+        -> str:
+  try:
+    check_file_path(design_file)
+    check_file_path(work_dir)
+    with open(design_file, 'r') as fp:
+      input_design_yaml=fp.read()
+    sample_metadata, analysis_metadata = \
+      parse_analysis_design_and_get_metadata(
+        input_design_yaml=input_design_yaml)
+    if sample_metadata is None or \
+       analysis_metadata is None:
+      raise KeyError("Missing sample or analysis metadata")
+    ## library info
+    sample_library_list = \
+      create_library_information_for_sample_group(
+        sample_group=sample_group,
+        sample_metadata=sample_metadata,
+        db_config_file=db_config_file)
+    ## get cellranger conf
+    cellranger_multi_config = \
+      analysis_metadata.get("cellranger_multi_config")
+    if cellranger_multi_config is None:
+      raise KeyError("Missing cellranger_multi_config in analysis design")
+    ## create temp dir and dump script and library.csv
+    library_csv_file = \
+      os.path.join(
+        work_dir,
+        library_csv_filename)
+    sample_library_csv = \
+      pd.DataFrame(sample_library_list).\
+      to_csv(index=False)
+    with open(library_csv_file, 'w') as fp:
+      fp.write('\n'.join(cellranger_multi_config))
+      fp.write('\n') ## add an empty line
+      fp.write('[libraries]\n')
+      fp.write(sample_library_csv)
+    return library_csv_file
+  except Exception as e:
+    raise ValueError(
+      f"Failed to prepare cellranger script, error: {e}")
+
+
+def create_library_information_for_sample_group(
+      sample_group: str,
+      sample_metadata: dict,
+      db_config_file: str) -> list:
+  try:
+    ## get cellranger group
+    sample_group_dict = dict()
+    sample_igf_id_list = list()
+    for sample_igf_id, group in sample_metadata.items():
+      grp_name = group.get('cellranger_group')
+      feature_types = group.get('feature_types')
+      if grp_name is None or feature_types is None:
+        raise KeyError(
+          "Missing cellranger_group or feature_types in sample_metadata ")
+      if str(grp_name) == str(sample_group):
+        sample_igf_id_list.append(sample_igf_id)
+      sample_group_dict.update({ sample_igf_id: feature_types})
+    ## get sample ids from metadata
+    if len(sample_igf_id_list) == 0:
+      raise ValueError("No sample id found in the metadata")
+    ## get fastq files for all samples
+    fastq_list = \
+      get_fastq_and_run_for_samples(
+        dbconfig_file=db_config_file,
+        sample_igf_id_list=sample_igf_id_list)
+    if len(fastq_list) == 0:
+      raise ValueError(
+        "No fastq file found for samples")
+    ## create libraries section
+    df = pd.DataFrame(fastq_list)
+    sample_library_list = list()
+    for _, g_data in df.groupby(['sample_igf_id', 'run_igf_id', 'flowcell_id', 'lane_number']):
+      sample_igf_id = g_data['sample_igf_id'].values[0]
+      fastq_file_path = g_data['file_path'].values[0]
+      fastq_dir = os.path.dirname(fastq_file_path)
+      feature_types = sample_group_dict.get(sample_igf_id)
+      if feature_types is None:
+        raise KeyError(
+          f"No feature_types found for sample {sample_igf_id}")
+      fastq_id = \
+        os.path.basename(fastq_file_path).split("_")[0]
+      sample_library_list.append({
+        "fastq_id": fastq_id,
+        "fastqs": fastq_dir,
+        "feature_types": feature_types})
+    return sample_library_list
+  except Exception as e:
+    raise ValueError(
+      f"Failed to prepare cellranger script, error: {e}")
---
+++ test/igf_airflow/test_dag34_cellranger_multi_scRNA_utils.py
@@ -0,0 +1,296 @@
+import os
+import json
+import yaml
+import zipfile
+import unittest
+import pandas as pd
+from yaml import Loader, Dumper
+from igf_data.igfdb.igfTables import Base
+from igf_data.igfdb.baseadaptor import BaseAdaptor
+from igf_data.igfdb.pipelineadaptor import PipelineAdaptor
+from igf_data.igfdb.analysisadaptor import AnalysisAdaptor
+from igf_data.igfdb.projectadaptor import ProjectAdaptor
+from igf_data.igfdb.platformadaptor import PlatformAdaptor
+from igf_data.igfdb.seqrunadaptor import SeqrunAdaptor
+from igf_data.igfdb.sampleadaptor import SampleAdaptor
+from igf_data.igfdb.useradaptor import UserAdaptor
+from igf_data.igfdb.experimentadaptor import ExperimentAdaptor
+from igf_data.igfdb.runadaptor import RunAdaptor
+from igf_data.igfdb.collectionadaptor import CollectionAdaptor
+from igf_data.igfdb.fileadaptor import FileAdaptor
+from igf_data.utils.dbutils import read_dbconf_json
+from igf_data.utils.fileutils import (
+  get_temp_dir,
+  check_file_path,
+  remove_dir)
+from igf_airflow.utils.dag34_cellranger_multi_scRNA_utils import (
+  prepare_cellranger_run_dir_and_script_file,
+  create_library_information_for_sample_group)
+from igf_airflow.utils.dag26_snakemake_rnaseq_utils import (
+    parse_analysis_design_and_get_metadata)
+
+DESIGN_YAML = """sample_metadata:
+  IGFsampleA:
+    feature_types: Gene Expression
+    cellranger_group: grp1
+  IGFsampleB:
+    feature_types: VDJ-T
+    cellranger_group: grp2
+analysis_metadata:
+  cellranger_multi_config:
+    - "[gene-expression]"
+    - "reference,/REF"
+    - "r1-length,28"
+    - "r2-length,90"
+    - "chemistry,auto"
+    - "expect-cells,60000"
+    - "force-cells,6000"
+    - "include-introns,true"
+    - "no-secondary,false"
+    - "no-bam,false"
+    - "check-library-compatibility,true"
+    - "min-assignment-confidence,0.9"
+    - "cmo-set,/path/custom/cmo.csv"
+    - "[vdj]"
+    - "reference,/path"
+    - "r1-length,28"
+    - "r2-length,90"
+    - "[samples]"
+    - "sample_id,cmo_ids"
+    - "IGF3,CMO3"
+  scanpy:
+    active: true
+    mito_prefix: MT-
+    run_scrublet: true
+    run_cellcycle_score: true
+    cell_marker_list: /path/PangaloDB
+    cell_marker_species: HG38
+    s_genes: ''
+    g2m_genes: ''
+    cell_marker_mode: NON-VDJ
+  scvelo:
+    active: true"""
+
+class TestDag34_cellranger_multi_scRNA_utilA(unittest.TestCase):
+  def setUp(self):
+    self.temp_dir = get_temp_dir()
+    self.dbconfig = 'data/dbconfig.json'
+    dbparam = read_dbconf_json(self.dbconfig)
+    base = BaseAdaptor(**dbparam)
+    self.engine = base.engine
+    self.dbname = dbparam['dbname']
+    Base.metadata.create_all(self.engine)
+    self.session_class = base.get_session_class()
+    base.start_session()
+    platform_data = [{
+      "platform_igf_id" : "M03291",
+      "model_name" : "MISEQ",
+      "vendor_name" : "ILLUMINA",
+      "software_name" : "RTA",
+      "software_version" : "RTA1.18.54"}]
+    flowcell_rule_data = [{
+      "platform_igf_id": "M03291",
+      "flowcell_type": "MISEQ",
+      "index_1": "NO_CHANGE",
+      "index_2": "NO_CHANGE"}]
+    pl = PlatformAdaptor(**{'session':base.session})
+    pl.store_platform_data(data=platform_data)
+    pl.store_flowcell_barcode_rule(data=flowcell_rule_data)
+    seqrun_data = [{
+      'seqrun_igf_id': '180416_M03291_0139_000000000-BRN47',
+      'flowcell_id': '000000000-BRN47',
+      'platform_igf_id': 'M03291',
+      'flowcell': 'MISEQ'}]
+    sra = SeqrunAdaptor(**{'session':base.session})
+    sra.store_seqrun_and_attribute_data(data=seqrun_data)
+    project_data = [{'project_igf_id':'IGFQprojectA'}]
+    pa = ProjectAdaptor(**{'session':base.session})
+    pa.store_project_and_attribute_data(data=project_data)
+    sample_data = [{
+      'sample_igf_id': 'IGFsampleA',
+      'project_igf_id': 'IGFQprojectA',
+      'species_name': 'HG38'
+    },{
+      'sample_igf_id': 'IGFsampleB',
+      'project_igf_id': 'IGFQprojectA',
+      'species_name': 'UNKNOWN'
+    }]
+    sa = SampleAdaptor(**{'session':base.session})
+    sa.store_sample_and_attribute_data(data=sample_data)
+    experiment_data = [{
+      'project_igf_id': 'IGFQprojectA',
+      'sample_igf_id': 'IGFsampleA',
+      'experiment_igf_id': 'IGFsampleA_MISEQ',
+      'library_name': 'IGFsampleA',
+      'library_source': 'TRANSCRIPTOMIC',
+      'library_strategy': 'RNA-SEQ',
+      'experiment_type': 'POLYA-RNA',
+      'library_layout': 'PAIRED',
+      'platform_name': 'MISEQ',
+    },{
+      'project_igf_id': 'IGFQprojectA',
+      'sample_igf_id': 'IGFsampleB',
+      'experiment_igf_id': 'IGFsampleB_MISEQ',
+      'library_name': 'IGFsampleB',
+      'library_source': 'UNKNOWN',
+      'library_strategy': 'UNKNOWN',
+      'experiment_type': 'UNKNOWN',
+      'library_layout': 'UNKNOWN',
+      'platform_name': 'MISEQ',
+    }]
+    ea = ExperimentAdaptor(**{'session':base.session})
+    ea.store_project_and_attribute_data(data=experiment_data)
+    run_data = [{
+      'experiment_igf_id': 'IGFsampleA_MISEQ',
+      'seqrun_igf_id': '180416_M03291_0139_000000000-BRN47',
+      'run_igf_id': 'IGFsampleA_MISEQ_000000000-BRN47_1',
+      'lane_number': '1'
+    },{
+      'experiment_igf_id': 'IGFsampleB_MISEQ',
+      'seqrun_igf_id': '180416_M03291_0139_000000000-BRN47',
+      'run_igf_id': 'IGFsampleB_MISEQ_000000000-BRN47_1',
+      'lane_number': '1'
+    }]
+    ra = RunAdaptor(**{'session':base.session})
+    ra.store_run_and_attribute_data(data=run_data)
+    file_data = [
+      {'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_R1_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_R2_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_I1_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_I2_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleB/IGFsampleB_S2_L001_R1_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleB/IGFsampleA_S1_L001_R2_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleB/IGFsampleB_S2_L001_I1_001.fastq.gz'},
+      {'file_path': '/path/IGFSampleB/IGFsampleB_S2_L001_I2_001.fastq.gz'}]
+    fa = FileAdaptor(**{'session':base.session})
+    fa.store_file_and_attribute_data(data=file_data)
+    collection_data = [{
+      'name': 'IGFsampleA_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'table': 'run'
+    }, {
+      'name': 'IGFsampleB_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'table': 'run'
+    }]
+    collection_files_data = [{
+      'name': 'IGFsampleA_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_R1_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleA_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_R2_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleA_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_I1_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleA_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleA/IGFsampleA_S1_L001_I2_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleB_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleB/IGFsampleB_S2_L001_R1_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleB_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleB/IGFsampleA_S1_L001_R2_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleB_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleB/IGFsampleB_S2_L001_I1_001.fastq.gz'
+    }, {
+      'name': 'IGFsampleB_MISEQ_000000000-BRN47_1',
+      'type': 'demultiplexed_fastq',
+      'file_path': '/path/IGFSampleB/IGFsampleB_S2_L001_I2_001.fastq.gz'
+    }]
+    ca = CollectionAdaptor(**{'session':base.session})
+    ca.store_collection_and_attribute_data(data=collection_data)
+    ca.create_collection_group(data=collection_files_data)
+    base.close_session()
+    self.yaml_data = DESIGN_YAML
+    self.yaml_file = \
+      os.path.join(
+        self.temp_dir,
+        'analysis_design.yaml')
+    with open(self.yaml_file, 'w') as fp:
+      fp.write(self.yaml_data)
+
+  def tearDown(self):
+    remove_dir(self.temp_dir)
+    Base.metadata.drop_all(self.engine)
+    if os.path.exists(self.dbname):
+      os.remove(self.dbname)
+
+  def test_create_library_information_for_sample_group(self):
+    sample_metadata, analysis_metadata = \
+      parse_analysis_design_and_get_metadata(
+        input_design_yaml=self.yaml_data)
+    sample_library_list = \
+        create_library_information_for_sample_group(
+          sample_group='grp1',
+          sample_metadata=sample_metadata,
+          db_config_file=self.dbconfig)
+    self.assertEqual(len(sample_library_list), 1)
+    self.assertTrue(isinstance(sample_library_list, list))
+    self.assertTrue(isinstance(sample_library_list[0], dict))
+    self.assertEqual(sample_library_list[0].get('fastq_id'), 'IGFsampleA')
+    self.assertEqual(sample_library_list[0].get('fastqs'), '/path/IGFSampleA')
+    self.assertEqual(sample_library_list[0].get('feature_types'), 'Gene Expression')
+    sample_library_list = \
+        create_library_information_for_sample_group(
+          sample_group='grp2',
+          sample_metadata=sample_metadata,
+          db_config_file=self.dbconfig)
+    self.assertEqual(len(sample_library_list), 1)
+    self.assertTrue(isinstance(sample_library_list, list))
+    self.assertTrue(isinstance(sample_library_list[0], dict))
+    self.assertEqual(sample_library_list[0].get('fastq_id'), 'IGFsampleB')
+    self.assertEqual(sample_library_list[0].get('fastqs'), '/path/IGFSampleB')
+    self.assertEqual(sample_library_list[0].get('feature_types'), 'VDJ-T')
+    with self.assertRaises(Exception):
+      sample_library_list = \
+        create_library_information_for_sample_group(
+          sample_group='grp5',
+          sample_metadata=sample_metadata,
+          db_config_file=self.dbconfig)
+
+  def test_prepare_cellranger_run_dir_and_script_file(self):
+    temp_dir = get_temp_dir()
+    library_csv_file = \
+      prepare_cellranger_run_dir_and_script_file(
+        sample_group="grp1",
+        work_dir=temp_dir,
+        design_file=self.yaml_file,
+        db_config_file=self.dbconfig)
+    self.assertTrue(os.path.exists(library_csv_file))
+    gene_expression_list = list()
+    libraries_list = list()
+    with open(library_csv_file, 'r') as fp:
+      for i in fp:
+        if i.startswith('['):
+          data_list = list()
+          ge_start = False
+          lib_start = False
+        if ge_start:
+          gene_expression_list.\
+            append(i.strip())
+        if lib_start:
+          libraries_list.\
+            append(i.strip())
+        if i.startswith('[gene-expression]'):
+          ge_start = True
+        if i.startswith('[libraries]'):
+          lib_start = True
+    self.assertEqual(len(gene_expression_list), 12)
+    self.assertEqual(len(libraries_list), 2)
+    (sample_id, fastq_dir, feature) = \
+      libraries_list[1].split(',')
+    self.assertEqual(sample_id, 'IGFsampleA')
+    self.assertEqual(fastq_dir, '/path/IGFSampleA')
+    self.assertEqual(feature, 'Gene Expression')
+
+if __name__=='__main__':
+  unittest.main()
