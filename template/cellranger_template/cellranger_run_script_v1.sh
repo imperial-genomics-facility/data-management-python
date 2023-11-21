6a4bad7c8a9d38cac8ff13339fb17012f19f0f32
Avik Datta <avik.datta@imperial.ac.uk>
2023-11-21 01:06:33 +0000

    added wrapper script

---
+++ template/cellranger_template/cellranger_run_script_v1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## THIS IS A TEMPLATE FOR RUNNING CELLRANGER PIPELINE
+## VERSION: 0.0.1
+##
+## REQUIRED INPUTS:
+##
+##  * CELLRANGER_MULTI_ID: Cellranger ID
+##  * CELLRANGER_MULTI_CSV: Cellranger multi csv
+##  * CELLRANGER_MULTI_OUTPUT: Cellranger multi output dir
+##  * WORKDIR: Work dir path
+
+## IMPORT ENV
+source /project/tgu/resources/pipeline_resource/cellranger/env.sh
+
+cd {{ WORKDIR }}
+
+## CELLRANGER MULTI RUN CMD
+$CELLRANGER_EXE multi \
+  --id={{ CELLRANGER_MULTI_ID }} \
+  --csv={{ CELLRANGER_MULTI_CSV }} \
+  --output-dir={{ CELLRANGER_MULTI_OUTPUT }} \
+  --maxjobs=${CELLRANGER_MULTI_JOB_COUNTS} \
+  --localcores=4 \
+  --localmem=8 \
+  --jobmode=pbspro \
+  --disable-ui
