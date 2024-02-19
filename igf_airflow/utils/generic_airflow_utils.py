3de5e08f67bb0fabdf7da72baa5d582fa079e14e
Avik Datta <avik.datta@imperial.ac.uk>
2024-02-19 15:29:52 +0000

    new generic method for sending logs to channels

--- igf_airflow/utils/generic_airflow_utils.py
+++ igf_airflow/utils/generic_airflow_utils.py
@@ -22,7 +22,7 @@ DATABASE_CONFIG_FILE = Variable.get('database_config_file', default_var=None)
 def send_airflow_failed_logs_to_channels(
     slack_conf: str,
     ms_teams_conf: str,
-    message_prefix: str = 'Error') -> None:
+    message_prefix: str) -> None:
   try:
     context = get_current_context()
     log_file_path = [
@@ -41,6 +41,32 @@ def send_airflow_failed_logs_to_channels(
       project_id=None,
       comment=message,
       reaction='fail')
+  except Exception as e:
+    log.error(e)
+
+
+def send_airflow_pipeline_logs_to_channels(
+    slack_conf: str,
+    ms_teams_conf: str,
+    message_prefix: str) -> None:
+  try:
+    context = get_current_context()
+    log_file_path = [
+      os.environ.get('AIRFLOW__LOGGING__BASE_LOG_FOLDER'),
+      f"dag_id={context['ti'].dag_id}",
+      f"run_id={context['ti'].run_id}",
+      f"task_id={context['ti'].task_id}",
+      f"attempt={context['ti'].try_number}.log"]
+    message = \
+      f"Error: {message_prefix}, Log: {os.path.join(*log_file_path)}"
+    send_log_to_channels(
+      slack_conf=slack_conf,
+      ms_teams_conf=ms_teams_conf,
+      task_id=context['task'].task_id,
+      dag_id=context['task'].dag_id,
+      project_id=None,
+      comment=message,
+      reaction='pass')
   except Exception as e:
     log.error(e)
 
