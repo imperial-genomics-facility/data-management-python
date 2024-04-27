2a9cc03441cad12698a028c8f8f1db0eb68147e4
Avik Datta <avik.datta@imperial.ac.uk>
2024-04-27 22:48:57 +0100

    changing pid dir for airflow worker and scheduler

--- scripts/hpc/airflow_worker.sh
+++ scripts/hpc/airflow_worker.sh
@@ -4,9 +4,11 @@ job_queue=${1:?'Missing job queue'}
 job_name=${2:?'Missing job name'}
 
 source /rds/general/user/igf/home/data2/airflow_v3/secrets/hpc_env.sh
+cd /rds/general/user/igf/ephemeral
+mkdir -p /rds/general/user/igf/ephemeral/${PBS_JOBID}
 
 airflow celery worker \
-  --pid $TMPDIR/pid \
+  --pid /rds/general/user/igf/ephemeral/${PBS_JOBID}/pid \
   --celery-hostname ${PBS_JOBID}-${job_name} \
   --queues ${job_queue} \
   --skip-serve-logs
