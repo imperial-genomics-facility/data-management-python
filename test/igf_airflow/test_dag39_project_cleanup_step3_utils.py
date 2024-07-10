7be5a65caf367004678fad77ba52e20ee2248d0e
Avik Datta <avik.datta@imperial.ac.uk>
2024-07-10 22:34:34 +0100

    fixed dag39 tests

--- test/igf_airflow/test_dag39_project_cleanup_step3_utils.py
+++ test/igf_airflow/test_dag39_project_cleanup_step3_utils.py
@@ -40,5 +40,38 @@ class TestDag39_project_cleanup_step3_utilsA(unittest.TestCase):
     send_airflow_pipeline_logs_to_channels.\
       assert_called_once()
 
+  @patch("igf_airflow.utils.dag39_project_cleanup_step3_utils.get_current_context")
+  @patch("igf_airflow.utils.dag39_project_cleanup_step3_utils.get_data_from_portal")
+  def test_mark_project_deleted_on_portal(
+        self,
+        get_current_context,
+        get_data_from_portal):
+    mark_project_deleted_on_portal.\
+      function()
+    get_current_context.\
+      assert_called_once()
+    get_data_from_portal.\
+      assert_called_once()
+
+
+  @patch("igf_airflow.utils.dag39_project_cleanup_step3_utils.format_and_send_generic_email_to_user")
+  def test_notify_user_about_project_cleanup_finished(
+        self,
+        format_and_send_generic_email_to_user):
+    json_data = {
+      "user_name": "A",
+      "user_email": "B",
+      "projects": ["AA", "BB"],
+      "deletion_date": "2023-09-01"}
+    json_file = \
+        os.path.join(self.temp_dir, "data.json")
+    with open(json_file, "w") as fp:
+      json.dump(json_data, fp)
+    notify_user_about_project_cleanup_finished.\
+      function(
+        project_cleanup_data_file=json_file)
+    format_and_send_generic_email_to_user.\
+      assert_called_once()
+
 if __name__=='__main__':
   unittest.main()
