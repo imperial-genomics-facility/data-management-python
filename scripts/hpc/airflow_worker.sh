3ba70d0bdf2878c61b959f6b8d05862236b37f8c
Avik Datta <avik.datta@imperial.ac.uk>
2025-02-27 12:10:10 +0000

    changed worker runner path

--- scripts/hpc/airflow_worker.sh
+++ scripts/hpc/airflow_worker.sh
@@ -3,7 +3,7 @@
 job_queue=${1:?'Missing job queue'}
 job_name=${2:?'Missing job name'}
 
-source /rds/general/user/igf/home/data2/airflow_v3/secrets/hpc_env.sh
+source /rds/general/user/igf/home/data2/airflow_v4/secrets/hpc_env.sh
 cd /rds/general/user/igf/ephemeral
 mkdir -p /rds/general/user/igf/ephemeral/${PBS_JOBID}
 
