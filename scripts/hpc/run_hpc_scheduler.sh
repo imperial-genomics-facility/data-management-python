51826b708517838dc435b245feda02fd9c7f1d39
Avik Datta <avik.datta@imperial.ac.uk>
2025-02-27 12:13:44 +0000

    fix for dag7

--- scripts/hpc/run_hpc_scheduler.sh
+++ scripts/hpc/run_hpc_scheduler.sh
@@ -5,7 +5,7 @@
 #PBS -l walltime=01:00:00
 #PBS -l select=1:ncpus=4:mem=4gb
 
-source /rds/general/user/igf/home/data2/airflow_v3/secrets/hpc_env.sh
+source /rds/general/project/genomics-facility-archive-2019/live/AIRFLOW/airflow_v4/secrets/hpc_env.sh
 cd /rds/general/user/igf/ephemeral
 mkdir -p /rds/general/user/igf/ephemeral/${PBS_JOBID}
 
