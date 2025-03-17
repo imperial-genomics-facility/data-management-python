8746e4d8d8acbd0fef27c1426486618093b7ffc1
Avik Datta <avik.datta@imperial.ac.uk>
2025-03-17 16:04:47 +0000

    added function for scaling workers

--- igf_airflow/utils/dag1_calculate_hpc_worker_utils.py
+++ igf_airflow/utils/dag1_calculate_hpc_worker_utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+from io import StringIO
 import requests, json, redis
 from typing import List
 from igf_data.utils.dbutils import read_json_data
@@ -78,7 +81,239 @@ def get_redis_queue_tasks(redis_conf_file: str) -> List[dict]:
     queue_list = fetch_queue_list_from_redis_server(url=url)
     return queue_list
   except Exception as e:
-    raise ValueError(f'Failed to get redis queue tasks, error: {e}')
+    raise ValueError(
+      f'Failed to get redis queue tasks, error: {e}')
+
+
+def merge_worker_info_and_scale_workers(
+    hpc_worker_info: str,
+    celery_flower_worker_info: List[str],
+    redis_queue_info: List[dict],
+    max_items_in_queue: int = 3,
+    total_hpc_jobs: int = 30) -> List[dict]:
+  try:
+    pass
+  except Exception as e:
+    raise ValueError(f'Failed to merge worker info and scale workers, error: {e}')
+
+
+def combine_celery_and_hpc_worker_info(
+    hpc_worker_info: str,
+    celery_flower_worker_info: List[dict],
+    redis_queue_info: List[dict],
+    generic_queue_name: str = 'generic',
+    max_items_in_queue: int = 3,
+    total_hpc_jobs: int = 30)-> List[dict]:
+    try:
+      ## process hpc output
+      hpc_jobs_df = pd.DataFrame()
+      if hpc_worker_info != "":
+        hpc_jobs_df = pd.read_csv(StringIO(hpc_worker_info), header=None)
+        if len(hpc_jobs_df.columns) != 3:
+          raise ValueError(
+            f'Expected 3 columns in the hpc wortker info, got {len(hpc_jobs_df.columns)}')
+        hpc_jobs_df.columns = [
+          'job_id',
+          'queue_name',
+          'job_status']
+      ## process celery flower output
+      celery_worker_df = pd.DataFrame(celery_flower_worker_info)
+      required_celery_columns = [
+        'worker_id',
+        'active_jobs',
+        'queue_lists']
+      for c in required_celery_columns:
+        if c not in celery_worker_df.columns:
+          raise KeyError(
+            f'Columns {required_celery_columns} not found in the celery worker info')
+      celery_worker_df["queue_lists"] = \
+        celery_worker_df["queue_lists"].map(lambda x: ','.join(x))
+      filt_worker_df = \
+        celery_worker_df[celery_worker_df['queue_lists'] != generic_queue_name].copy()
+      ## using last queue name as the main queue name
+      filt_worker_df["queue_name"] = \
+        filt_worker_df["queue_lists"].\
+          map(lambda x: x.split(",")[-1])
+      filt_worker_df["worker_info"] = \
+        filt_worker_df["worker_id"].\
+          map(lambda x: x.replace("celery@", "").split("-"))
+      filt_worker_df[["job_id", "queue_name_tag"]] = \
+        pd.DataFrame(
+          filt_worker_df['worker_info'].to_list(),
+          index=filt_worker_df.index)
+      filt_worker_df = \
+        filt_worker_df[[
+          "job_id",
+          "worker_id",
+          "queue_name",
+          "active_jobs"]]
+      ## merge data
+      merged_data = pd.DataFrame()
+      if len(hpc_jobs_df.index) > 0 and len(filt_worker_df.index) > 0:
+        merged_data = \
+          hpc_jobs_df.set_index(['job_id','queue_name']).\
+            join(
+              filt_worker_df.set_index(['job_id','queue_name']),
+              how='outer').\
+            reset_index()
+        merged_data = \
+          merged_data.fillna({"worker_id":"U"})
+        merged_data['job_status'] = \
+          merged_data['job_status'].fillna('U')
+        merged_data['active_jobs'] = \
+          merged_data['active_jobs'].fillna(0)
+        merged_data = \
+          merged_data.astype({'active_jobs':int})
+        merged_data = \
+          merged_data[merged_data['job_status'] !='U']
+        ## create a new column to store the job count per worker status
+        ## for hpc active workers
+        merged_data['hpc_r'] = \
+          np.select([
+            merged_data["job_status"] == "R",],[
+            1,],
+            default=0)
+        ## for hpc queued workers
+        merged_data['hpc_q'] = \
+          np.select([
+            merged_data["job_status"] == "Q",],[
+            1,],
+            default=0)
+        ## for celery active workers
+        merged_data['task_r'] = \
+          np.select([
+            (merged_data["job_status"] == "R")&(merged_data["active_jobs"] == 1),],[
+            1,],
+            default=0)
+        ## for celery inactive workers
+        merged_data['task_i'] = \
+          np.select([
+            (merged_data["job_status"] == "R") & (merged_data["active_jobs"] == 0),],[
+            1,],
+            default=0)
+        ## keep subset of columns
+        filt_merged_data = \
+          merged_data[[
+            'queue_name',
+            'hpc_r',
+            'hpc_q',
+            'task_r',
+            'task_i']].copy()
+        ## count the number of jobs per queue
+        agg_df = \
+          filt_merged_data.\
+            groupby('queue_name').agg("sum")
+        ## add the queue info from redis
+        if len(redis_queue_info) > 0:
+          queue_data = [
+            {'queue_name': queue_name, 'queued': queued_jobs}
+              for entry in redis_queue_info
+                for queue_name, queued_jobs in entry.items()]
+          queue_df = pd.DataFrame(queue_data)
+          final_agg_df = \
+            agg_df.join(
+              queue_df.set_index("queue_name"),
+              how="left")
+        else:
+          final_agg_df = agg_df.copy()
+          final_agg_df['queued'] = 0
+        ## format the final dataframe
+        final_agg_df = \
+          final_agg_df.\
+            fillna(0).\
+            astype({
+              'hpc_r': int,
+              'hpc_q': int,
+              'task_r': int,
+              'task_i': int,
+              'queued': int}).\
+            reset_index()
+        ## scale out and scale in operations
+        scaled_df = \
+          calculate_scale_out_scale_in_ops(
+            input_df=final_agg_df,
+            max_items_in_queue=max_items_in_queue,
+            total_hpc_jobs=total_hpc_jobs)
+        return scaled_df.to_dict(orient='records')
+    except Exception as e:
+      raise ValueError(
+        f'Failed to combine celery and hpc worker info, error: {e}')
+
+
+def calculate_scale_out_scale_in_ops(
+    input_df: pd.DataFrame,
+    max_items_in_queue : int = 3,
+    total_hpc_jobs : int = 30) \
+        -> pd.DataFrame:
+  """
+  A function for calculating scale out and scale in operations
+
+  :param input_df: A pandas dataframe with columns queue_name, hpc_r, hpc_q, task_r, task_i, queued
+  :param max_items_in_queue: Maximum number of items allowed in the queue
+  :param total_hpc_jobs: Total number of hpc jobs allowed
+  :returns: A pandas dataframe with scale_out_ops and scale_in_ops columns
+  """
+  try:
+    required_columns = [
+      "queue_name",
+      "hpc_r",
+      "hpc_q",
+      "task_r",
+      "task_i",
+      "queued"]
+    # check if all required columns are present
+    # copy the input dataframe
+    df = input_df.copy()
+    for c in required_columns:
+      if c not in df.columns:
+        raise KeyError(f'Column {c} is missing from input dataframe')
+    df['scale_out_ops'] = 0 
+    df['scale_in_ops'] = 0
+    allowed_tasks = \
+      total_hpc_jobs - (df['hpc_r'].sum() + df['hpc_q'].sum())
+    ## scale-in - full
+    df['scale_in_ops'] = \
+      np.where(
+        ((df.hpc_r > 0) & (df.task_r == 0) & (df.queued == 0)),
+        df.hpc_r,
+        df['scale_in_ops'])
+    ## scale-in - partial
+    df['scale_in_ops'] = \
+      np.where(
+        ((df.hpc_r > 0) & (df.task_r < df.hpc_r) & (df.queued == 0)),
+        df.hpc_r - df.task_r,
+        df['scale_in_ops'])
+    ## no new task can be added
+    if allowed_tasks < 1:
+      return df
+    ## adding new tasks
+    ## scale-out - partial
+    df['scale_out_ops'] = \
+      np.where(
+        ((df.hpc_r > 0) & (df.hpc_q == 0) & (df.queued > max_items_in_queue)),
+        df.queued,
+        df['scale_out_ops'])
+    ## scale-out - full
+    df['scale_out_ops'] = \
+      np.where(
+        ((df.hpc_r == 0) & (df.hpc_q == 0) & (df.queued > 0)),
+        df.queued,
+        df['scale_out_ops'])
+    ## limit new tasks
+    if df['scale_out_ops'].sum() > allowed_tasks:
+      requested_tasks = df['scale_out_ops'].values.tolist()
+      new_requested_tasks = [0 for i in range(0, len(requested_tasks))]
+      while allowed_tasks:
+        for i in range(0, len(new_requested_tasks)):
+          if new_requested_tasks[i] < requested_tasks[i] and allowed_tasks:
+            new_requested_tasks[i] += 1
+            allowed_tasks -= 1
+      df['scale_out_ops'] = \
+        pd.Series(new_requested_tasks, index=df.index)
+    return df
+  except Exception as e:
+    raise ValueError(
+      f'Failed to calculate scale out and scale in ops, error: {e}')
 
 def scale_hpc_workers():
     pass
--- test/igf_airflow/test_dag1_calculate_hpc_worker_utils.py
+++ test/igf_airflow/test_dag1_calculate_hpc_worker_utils.py
@@ -6,6 +6,7 @@ import responses
 import requests
 import unittest
 import pandas as pd
+from io import StringIO
 from unittest.mock import patch
 from yaml import load, dump, SafeLoader, Dumper
 from igf_data.utils.fileutils import (
@@ -18,6 +19,8 @@ from igf_airflow.utils.dag1_calculate_hpc_worker_utils import (
   filter_scale_in_workers,
   check_celery_worker_status,
   prepare_scale_out_workers,
+  combine_celery_and_hpc_worker_info,
+  calculate_scale_out_scale_in_ops,
   fetch_queue_list_from_redis_server)
 
 class Test_dag1_calculate_hpc_worker_utils(unittest.TestCase):
@@ -79,6 +82,35 @@ class Test_dag1_calculate_hpc_worker_utils(unittest.TestCase):
     queue_list = get_redis_queue_tasks('A')
     self.assertEqual(len(queue_list), 1)
     self.assertEqual(queue_list[0], {'A':1})
+
+
+  def test_calculate_scale_out_scale_in_ops(self):
+    input_data = [ {'queue_name':'hpc_4G','hpc_r':9,'hpc_q':0,'task_r':9,'task_i':0,'queued':7},
+                   {'queue_name':'hpc_64G16t','hpc_r':2,'hpc_q':0,'task_r':2,'task_i':0,'queued':0},
+                   {'queue_name':'hpc_8G8t','hpc_r':1,'hpc_q':0,'task_r':0,'task_i':1,'queued':0} ]
+    input_df = pd.DataFrame(input_data)
+    scaled_df = \
+      calculate_scale_out_scale_in_ops(
+        input_df=input_df,
+        max_items_in_queue=3,
+        total_hpc_jobs=30)
+    self.assertTrue('scale_out_ops' in scaled_df.columns)
+    self.assertTrue('scale_in_ops' in scaled_df.columns)
+    self.assertTrue('hpc_4G' in scaled_df['queue_name'].values.tolist())
+    self.assertTrue('hpc_64G16t' in scaled_df['queue_name'].values.tolist())
+    self.assertTrue('hpc_8G8t' in scaled_df['queue_name'].values.tolist())
+    self.assertEqual(scaled_df[scaled_df["queue_name"] == "hpc_4G"]["scale_out_ops"].values[0], 7)
+    self.assertEqual(scaled_df[scaled_df["queue_name"] == "hpc_64G16t"]["scale_out_ops"].values[0], 0)
+    self.assertEqual(scaled_df[scaled_df["queue_name"] == "hpc_8G8t"]["scale_in_ops"].values[0], 1)
+    input_data = [ {'queue_name':'hpc_4G','hpc_r':10,'hpc_q':0,'task_r':10,'task_i':0,'queued':7}]
+    input_df = pd.DataFrame(input_data)
+    scaled_df = \
+      calculate_scale_out_scale_in_ops(
+        input_df=input_df,
+        max_items_in_queue=3,
+        total_hpc_jobs=10)
+    self.assertTrue('hpc_4G' in scaled_df['queue_name'].values.tolist())
+    self.assertEqual(scaled_df[scaled_df["queue_name"] == "hpc_4G"]["scale_out_ops"].values[0], 0)
 
 if __name__=='__main__':
   unittest.main()
