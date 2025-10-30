e68623ba21f433f36d98ef49d707ee5bed6b0aac
Avik Datta <avik.datta@imperial.ac.uk>
2025-10-30 19:04:21 +0000

    added initial tasks

---
+++ test/igf_airflow/test_dag45_metadata_registration_utils.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch
+
+class Test_dag45_metadata_registration_utilsA(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+  
+  def test_register_metadata_from_portal(self):
+    pass
