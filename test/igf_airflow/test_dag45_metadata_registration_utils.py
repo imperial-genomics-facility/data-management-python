82669dd67b7836aa485a260b28298d33490b8464
Avik Datta <avik.datta@imperial.ac.uk>
2025-11-07 23:53:22 +0000

    added tests for metadata registration

--- test/igf_airflow/test_dag45_metadata_registration_utils.py
+++ test/igf_airflow/test_dag45_metadata_registration_utils.py
@@ -24,6 +24,15 @@ class Test_dag45_metadata_registration_utilsA(unittest.TestCase):
     raw_metadata_id = find_raw_metadata_id.function()
     assert raw_metadata_id == 1
 
+  @patch('igf_airflow.utils.dag45_metadata_registration_utils.Find_and_register_new_project_data_from_portal_db')
+  @patch('igf_airflow.utils.dag45_metadata_registration_utils._parse_default_user_email_from_email_config')
+  def test_register_metadata_from_portal(self, mock_parse, mock_class):
+    # Create instance of the mocked class
+    mock_instance = MagicMock()
+    mock_instance.process_project_data_and_account.return_value = None
+    mock_class.return_value = mock_instance
+    register_metadata_from_portal.function(raw_metadata_id=1)
+    mock_instance.process_project_data_and_account.assert_called_once()
 
 if __name__=='__main__':
   unittest.main()
